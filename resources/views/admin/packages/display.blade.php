e3fd5078501134b88354de665a01ffe3cc7c54d0
Kashif Sohail <kashif.sohail@redsignal.biz>
2022-02-02 19:10:18 +0500

    CUE-89

---
+++ resources/views/admin/packages/display.blade.php
@@ -0,0 +1,129 @@
+<!--begin::Modal content-->
+<div class="modal-content">
+    <!--begin::Modal header-->
+    <div class="modal-header" id="kt_modal_password_header">
+        <!--begin::Modal title-->
+        <h2 class="fw-bolder rota-title">Display</h2>
+        <!--end::Modal title-->
+        <!--begin::Close-->
+        <div class="btn btn-icon btn-sm btn-active-icon-primary popup-close" data-kt-users-modal-action="close">
+            <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
+            <span class="svg-icon svg-icon-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="black" />
+                    <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black" />
+                </svg>
+            </span>
+            <!--end::Svg Icon-->
+        </div>
+        <!--end::Close-->
+    </div>
+    <!--end::Modal header-->
+    <!--begin::Modal body-->
+    <div class="modal-body scroll-y mx-5 mx-xl-15">
+
+        <!--begin::Form-->
+        <div class="d-flex flex-column scroll-y me-n7 pe-7 mt-10" id="kt_modal_plans_scroll">
+
+            <div class="form-group">
+
+                <div class="row">
+                    <div class="form-group col-md-4">
+                        <label style="font-size: 14px;">Patient</label>
+                        <strong style="font-size:18px;display: block;" id="user_name"></strong>
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label style="font-size: 14px;">Patient</label>
+                        <strong style="font-size:18px;display: block;" id="location_name"></strong>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group">
+
+                <div class="row">
+                    <div class="table-responsive">
+                        <table id="plans_service" class="table table-bordered table-advance">
+
+                            <thead>
+                            <tr>
+                                <th>Service Name</th>
+                                <th>Service Price</th>
+                                <th>Discount Name</th>
+                                <th>Discount Type</th>
+                                <th>Discount Price</th>
+                                <th>Subtotal</th>
+                                <th>Tax %</th>
+                                <th>Tax Price</th>
+                                <th>Total</th>
+                            </tr>
+                            </thead>
+
+                            <tbody class="display_plans">
+
+                                {{--<tr>
+                                    <td id="service_name"></td>
+                                    <td id="service_price"></td>
+                                    <td id="discount_name"></td>
+                                    <td id="discount_type"></td>
+                                    <td id="discount_price"></td>
+                                    <td id="invoice_subtotal"></td>
+                                    <td id="invoice_tax"></td>
+                                    <td id="invoice_tax_price"></td>
+                                    <td id="total_price"></td>
+                                </tr>--}}
+
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="table-responsive">
+                        <h4>History</h4>
+                        <table id="plan_history" class="table table-bordered table-advance">
+
+                            <thead>
+                            <tr>
+                                <th>Payment Mode</th>
+                                <th>Cash Flow</th>
+                                <th>Cash Amount</th>
+                                <th>Created At</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            <tr>
+                                <td id="payment_mode"></td>
+                                <td id="cash_flow"></td>
+                                <td id="cash_amount"></td>
+                                <td id="Created At"></td>
+                            </tr>
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+
+                <div class="row float-right">
+                    <div class="col-md-12 col-sm-12 col-xs-12 mt-10">
+                        <a class="btn btn-lg btn-primary blue hidden-print margin-bottom-5" target="_blank"
+                           href="{{ route('admin.invoices.invoice_pdf',1) }}">Print
+                            <i class="fa fa-print"></i>
+                        </a>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+        <!--end::Scroll-->
+    </div>
+    <!--end::Modal body-->
+</div>
+<!--end::Modal content-->
+
+
+
