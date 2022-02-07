3c0adb02bb4b961885b7caefc9f4b510ea2357f7
Kashif Sohail <kashif.sohail@redsignal.biz>
2022-02-07 17:47:38 +0500

    Non Plan Refunds

--- resources/views/admin/nonplansrefunds/actions.blade.php
+++
@@ -1,3 +0,0 @@
-@if(Gate::allows('refunds_refund'))
-    <a class="btn btn-xs btn-info" href="{{ route('admin.nonprefunds.refund_create',[$nonplansrefunds['id']]) }}" data-target="#ajax_refunds_create" data-toggle="modal">@lang('global.app_refund')</a>
-@endif
