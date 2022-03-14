7e9e296ea50d7fb195fe0adb204fd3a9e403404f
Hasan Redsignal <hasan@redsignal.biz>
2022-03-14 19:01:58 +0500

    Inventory migration created

---
+++ database/migrations/2022_03_14_110839_create_order_details_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id');
+            $table->foreignId('product_id');
+            $table->foreignId('discount_id')->nullable();
+            $table->integer('refund_order_id')->nullable();
+            $table->integer('quantity');
+            $table->float('sale_price', 8, 2);
+            $table->float('discount_price', 8, 2)->nullable();
+            $table->float('sale_price_after_discount', 8, 2);
+            $table->enum('order_type', ['sale', 'refund']);
+            $table->text('reason')->nullable();
+            $table->timestamps();
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+}
