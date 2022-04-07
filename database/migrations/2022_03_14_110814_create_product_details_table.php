5e68374dfb4edcfdc063ec7d646ad473fe81b675
Hasan Redsignal <hasan@redsignal.biz>
2022-04-07 11:23:19 +0500

    Made changes in migration script

--- database/migrations/2022_03_14_110814_create_product_details_table.php
+++ database/migrations/2022_03_14_110814_create_product_details_table.php
@@ -15,6 +15,7 @@ class CreateProductDetailsTable extends Migration
     {
         Schema::create('product_details', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('account_id');
             $table->foreignId('product_id');
             $table->float('purchase_price', 8, 2);
             $table->float('total_purchase_price', 8, 2);
