94ebbd4be98c956692ad177e14a36807b8b82ab6
Kashif Sohail <kashif.sohail@redsignal.biz>
2022-05-11 11:44:19 +0500

    dashboard

--- app/Reports/dashboardreport.php
+++ app/Reports/dashboardreport.php
@@ -196,6 +196,12 @@ class dashboardreport
             );
         }
         $counter = 0;
+
+        $report_data[0] = [
+            'Task',
+            'Hours per Day'
+        ];
+
         foreach ($location_information as $key => $location_infomation) {
             if ($where == 'today') {
                 $packagesadvances = PackageAdvances::whereDate('created_at', '=', Carbon::now()->format('Y-m-d'))
@@ -324,21 +330,16 @@ class dashboardreport
             $total_revenue = $total_revenue_cash_in + $total_revenue_card_in;
             $In_hand_balance = $total_revenue - $total_refund_out;
 
-            if ($counter === 0) {
-                $report_data[$location_single_info->id] = [
-                    'Task',
-                    'Hours per Day'
-                ];
-            }
 
-            if ($In_hand_balance > 0) {
-                $report_data[$location_single_info->id] = array(
-                    $location_single_info->city->name . ' - ' . $location_single_info->name,
-                    $In_hand_balance,
-                );
+                if ($In_hand_balance > 0) {
+                    $report_data[$location_single_info->id] = array(
+                        $location_single_info->city->name . ' - ' . $location_single_info->name,
+                        $In_hand_balance,
+                    );
 
-                $total += $In_hand_balance;
-            }
+                    $total += $In_hand_balance;
+                }
+
 
             $counter++;
         }
