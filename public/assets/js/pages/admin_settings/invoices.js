ff37d133bb793e9b3d9e67c9c7724293cac57258
Kashif Sohail <kashif.sohail@redsignal.biz>
2022-05-11 12:45:31 +0500

    fix

--- public/assets/js/pages/admin_settings/invoices.js
+++ public/assets/js/pages/admin_settings/invoices.js
@@ -519,6 +519,8 @@ function setFilters(filter_values, active_filters) {
 
         hideShowAdvanceFilters(active_filters);
 
+        getUserCentre();
+
     } catch (error) {
         showException(error);
     }
