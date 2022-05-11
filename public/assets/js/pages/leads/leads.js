69ebe3c901a9c51281eacddd0e63aa757759d442
Kashif Sohail <kashif.sohail@redsignal.biz>
2022-05-11 13:14:30 +0500

    fix

--- public/assets/js/pages/leads/leads.js
+++ public/assets/js/pages/leads/leads.js
@@ -806,6 +806,8 @@ function setFilters(filter_values, active_filters) {
 
         hideShowAdvanceFilters(active_filters);
 
+        getUserCity();
+
     } catch (error) {
         showException(error);
     }
