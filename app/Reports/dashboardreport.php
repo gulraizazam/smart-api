25a424599837e605e98511f2a219f2d9bbe61729
Kashif Sohail <kashif.sohail@redsignal.biz>
2022-05-11 12:06:48 +0500

    dashboard

--- app/Reports/dashboardreport.php
+++ app/Reports/dashboardreport.php
@@ -192,14 +192,19 @@ class dashboardreport
         $total = 0;
         $report_data = array();
         $wherecondtion = array();
-        if($request->performance){
-            $wherecondtion[] = array(
-                'created_by',
-                '=',
-                Auth::User()->id
-            );
-        }
+
+        $wherecondtion[] = array(
+            'created_by',
+            '=',
+            Auth::User()->id
+        );
         $counter = 0;
+
+        $report_data[] = [
+            'Task',
+            'Hours per Day'
+        ];
+
         foreach ($location_information as $key => $location_infomation) {
             if ($where == 'today') {
                 $packagesadvances = PackageAdvances::whereDate('created_at', '=', Carbon::now()->format('Y-m-d'))
@@ -328,20 +333,13 @@ class dashboardreport
             $total_revenue = $total_revenue_cash_in + $total_revenue_card_in;
             $In_hand_balance = $total_revenue - $total_refund_out;
 
-            if ($counter == 0) {
-                $report_data[$location_single_info->id] = [
-                    'Task',
-                    'Hours per Day'
-                ];
-            } else {
-                if ($In_hand_balance > 0) {
-                    $report_data[$location_single_info->id] = array(
-                        $location_single_info->city->name . ' - ' . $location_single_info->name,
-                        $In_hand_balance,
-                    );
+            if ($In_hand_balance > 0) {
+                $report_data[] = array(
+                    $location_single_info->city->name . ' - ' . $location_single_info->name,
+                    $In_hand_balance,
+                );
 
-                    $total += $In_hand_balance;
-                }
+                $total += $In_hand_balance;
             }
 
             $counter++;
