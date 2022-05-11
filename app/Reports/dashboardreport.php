fb66a4b5559090bba60e9661d0d441b7fd1eca36
Kashif Sohail <kashif.sohail@redsignal.biz>
2022-05-11 12:04:04 +0500

    dashboard

--- app/Reports/dashboardreport.php
+++ app/Reports/dashboardreport.php
@@ -32,11 +32,173 @@ class dashboardreport
         $total = 0;
         $report_data = array();
         $wherecondtion = array();
-        $wherecondtion[] = array(
-            'created_by',
-            '=',
-            Auth::User()->id
-        );
+
+        $counter = 0;
+        foreach ($location_information as $key => $location_infomation) {
+            if ($where == 'today') {
+                $packagesadvances = PackageAdvances::whereDate('created_at', '=', Carbon::now()->format('Y-m-d'))
+                    ->where([
+                        ['account_id', '=', $account_id],
+                        ['location_id', '=', $key],
+                    ])->get();
+            }
+            if ($where == 'yesterday') {
+                $packagesadvances = PackageAdvances::whereDate('created_at', '=', Carbon::now()->subDay(1)->format('Y-m-d'))
+                    ->where([
+                        ['account_id', '=', $account_id],
+                        ['location_id', '=', $key],
+                    ])->get();
+            }
+            if ($where == 'last7day') {
+                $packagesadvances = PackageAdvances::whereDate('created_at', '>=', Carbon::now()->subDay(6)->format('Y-m-d'))
+                    ->whereDate('created_at', '<=', Carbon::now()->format('Y-m-d'))
+                    ->where([
+                        ['account_id', '=', $account_id],
+                        ['location_id', '=', $key],
+                    ])->get();
+            }
+            if ($where == 'thisMonth') {
+                $packagesadvances = PackageAdvances::whereDate('created_at', '>=', Carbon::now()->startOfMonth()->format('Y-m-d'))
+                    ->whereDate('created_at', '<=', Carbon::now()->endOfMonth()->format('Y-m-d'))
+                    ->where([
+                        ['account_id', '=', $account_id],
+                        ['location_id', '=', $key],
+                    ])->get();
+            }
+
+            $location_single_info = Locations::find($key);
+
+            if ($packagesadvances) {
+                $balance = 0;
+                $total_balance = 0;
+                $total_revenue_cash_in = 0;
+                $total_revenue_card_in = 0;
+                $total_refund_out = 0;
+
+                foreach ($packagesadvances as $packagesadvance) {
+                    if (
+                        (
+                            $packagesadvance->cash_flow == 'in' &&
+                            $packagesadvance->is_adjustment == '0' &&
+                            $packagesadvance->is_tax == '0' &&
+                            $packagesadvance->is_cancel == '0'
+                        )
+                    ) {
+                        switch ($packagesadvance->cash_flow) {
+                            case 'in':
+                                $balance = $balance + $packagesadvance->cash_amount;
+                                break;
+                            case 'out':
+                                $balance = $balance - $packagesadvance->cash_amount;
+                                break;
+                            default:
+                                break;
+                        }
+                        $total_balance = $balance;
+                        if ($packagesadvance->cash_amount != 0) {
+                            if ($packagesadvance->package_id) {
+                                $transtype = Config::get('constants.trans_type.advance_in');
+                            }
+                            if ($packagesadvance->invoice_id && $packagesadvance->cash_flow == 'in') {
+                                $transtype = Config::get('constants.trans_type.advance_in');
+                            }
+                            if ($packagesadvance->is_adjustment == '1') {
+                                $transtype = Config::get('constants.trans_type.adjustment');
+                            }
+                            if ($packagesadvance->is_cancel == '1') {
+                                $transtype = Config::get('constants.trans_type.invoice_cancel');
+                            }
+                            if ($packagesadvance->invoice_id && $packagesadvance->cash_flow == 'out') {
+                                $transtype = Config::get('constants.trans_type.invoice_create');
+                            }
+                            if ($packagesadvance->is_refund == '1') {
+                                $transtype = Config::get('constants.trans_type.refund_in');
+                            }
+                            if ($packagesadvance->is_tax == '1') {
+                                $transtype = Config::get('constants.trans_type.tax_out');
+                            }
+                            if ($packagesadvance->cash_flow == 'in') {
+                                if ($packagesadvance->paymentmode->name == 'Cash') {
+                                    $revenue_cash_in = $packagesadvance->cash_amount;
+                                    $revenue_card_in = '';
+                                    $revenue_bank_in = '';
+                                    $refund_out = '';
+                                }
+                                if ($packagesadvance->paymentmode->name == 'Card') {
+                                    $revenue_cash_in = '';
+                                    $revenue_card_in = $packagesadvance->cash_amount;
+                                    $revenue_bank_in = '';
+                                    $refund_out = '';
+                                }
+                                if ($packagesadvance->paymentmode->name == 'Bank/Wire Transfer') {
+                                    $revenue_cash_in = '';
+                                    $revenue_card_in = '';
+                                    $revenue_bank_in = $packagesadvance->cash_amount;
+                                    $refund_out = '';
+                                }
+                            } else {
+                                $revenue_cash_in = '';
+                                $revenue_card_in = '';
+                                $revenue_bank_in = '';
+                                $refund_out = $packagesadvance->cash_amount;
+                            }
+
+                            if ($revenue_cash_in) {
+                                $total_revenue_cash_in += $revenue_cash_in;
+                            }
+                            if ($revenue_card_in) {
+                                $total_revenue_card_in += $revenue_card_in;
+                            }
+                            if ($revenue_bank_in) {
+                                $total_revenue_card_in += $revenue_bank_in;
+                            }
+                            if ($refund_out) {
+                                $total_refund_out += $refund_out;
+                            }
+                        }
+                    }
+                }
+            }
+            $total_revenue = $total_revenue_cash_in + $total_revenue_card_in;
+            $In_hand_balance = $total_revenue - $total_refund_out;
+
+            if ($counter == 0) {
+                $report_data[$location_single_info->id] = [
+                    'Task',
+                    'Hours per Day'
+                ];
+            } else {
+                if ($In_hand_balance > 0) {
+                    $report_data[$location_single_info->id] = array(
+                        $location_single_info->city->name . ' - ' . $location_single_info->name,
+                        $In_hand_balance,
+                    );
+
+                    $total += $In_hand_balance;
+                }
+            }
+
+            $counter++;
+        }
+
+        return [
+            $report_data,
+            $total
+        ];
+    }
+
+    public static function myCollectionbyrevenuewidgets($location_information, $account_id, $where,$request)
+    {
+        $total = 0;
+        $report_data = array();
+        $wherecondtion = array();
+        if($request->performance){
+            $wherecondtion[] = array(
+                'created_by',
+                '=',
+                Auth::User()->id
+            );
+        }
         $counter = 0;
         foreach ($location_information as $key => $location_infomation) {
             if ($where == 'today') {
