fe26158605e2c0033e6aee81209cf0f285e303d9
Kashif Sohail <kashif.sohail@redsignal.biz>
2022-05-14 11:03:39 +0500

    invoices

--- resources/views/admin/invoices/InvoiceMedicalHistorypdf.blade.php
+++ resources/views/admin/invoices/InvoiceMedicalHistorypdf.blade.php
@@ -103,6 +103,21 @@
         .client-info td{
             padding-top: 8px
         }
+
+        @media not print {
+            .invoice-pdf {
+                width: 50%;
+                margin-left: 25%;
+                margin-top: 50px;
+                height: 100%;
+            }
+        }
+        @page {
+            size: auto;
+            margin-top: 0;
+            margin-bottom: 0;
+        }
+
     </style>
 </head>
 <body>
@@ -329,10 +344,10 @@
         </tr>
     </table>
 
-    <table class="mt-0" style="width: 100%;position: fixed;bottom: 40px;">
+    <table class="mt-0">
         <tr>
             <td>&nbsp;</td>
-            <td style="text-align: right;">{{$location_info->address}}</td>
+            <td style="text-align: right;padding-top: 50px;">{{$location_info->address}}</td>
         </tr>
         <tr>
             <td><strong>Consultant Signature: </strong><span>__________________________</span></td>
@@ -341,11 +356,14 @@
     </table>
 </div>
 
-{{--<table style="width: 100%;" class="mt-0">
-    <tr>
-        <td><div class="danger-alert">Invoice is not Refundable</div></td>
-    </tr>
-</table>--}}
+
+<script>
+
+   window.print();
+   setTimeout(function () { window.close(); }, 100);
+
+</script>
+
 </body>
 
 </html>
