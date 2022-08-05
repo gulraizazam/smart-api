a7709cb7cda69b904c8dae8519e25c3bf1c7105f
Hasan Redsignal <hasan@redsignal.biz>
2022-08-05 15:49:40 +0500

    worked on cue-154

--- app/Helpers/Widgets/LocationsWidget.php
+++ app/Helpers/Widgets/LocationsWidget.php
@@ -83,6 +83,7 @@ class LocationsWidget
                 'account_id' => $account_id,
                 'region_id' => $region->id,
                 'slug' => 'custom',
+                'active' => 1,
             ))->orderBy('name', 'asc')->get();
 
             if ($other_childrens) {
