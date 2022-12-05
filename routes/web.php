63d32664606c7fc746e18e70adaf7b22018e26c3
gulraizazam <gulraiz@redsignal.biz>
2022-12-05 23:53:33 +0500

    update roles

--- routes/web.php
+++ routes/web.php
@@ -87,8 +87,8 @@ use App\Http\Controllers\Admin\Reports\AppointmentsController as ReportAppointme
     Route::group(['middleware' => ['auth.common','checkAccount'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
 
         Route::get('attchrole',function(){
-            $user = \App\Models\User::whereEmail("dha@cutera.pk")->first();
-            dd($user->assignRole(4));
+            $user = \App\Models\User::whereEmail("dr.nida@cutera.pk")->first();
+            dd($user->assignRole(5));
         });
 
 
