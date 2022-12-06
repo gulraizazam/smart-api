2d58eddd4ed82a1b9be24ea60a7a9caa53c760d9
gulraizazam <gulraiz@redsignal.biz>
2022-12-06 18:25:06 +0500

    debugging sms

--- app/Helpers/GeneralFunctions.php
+++ app/Helpers/GeneralFunctions.php
@@ -202,7 +202,48 @@ class GeneralFunctions
                 }
             }
         }
-
+        if (hasFilter($filters, 'name')) {
+            $query = Services::with('children')
+            ->where('name',
+            'like',
+            '%' . $filters['name'] . '%');
+            $services = $query->get();
+        
+            $mergedServices = [];
+            foreach ($services as $key => $service) {
+                $children = collect($service->children)->flatten();
+                unset($service->children);
+                $mergedServices[] = $service->toArray();
+                $children = $children->toArray();
+                foreach ($children as $child) {
+                    $mergedServices[] = $child;
+                }
+    
+            }
+    
+            return $mergedServices; 
+        }
+        if(hasFilter($filters, 'status')){
+            $query = Services::with('children')
+            ->where('active',
+            '=',
+            $filters['status']);
+            $services = $query->get();
+        
+            $mergedServices = [];
+            foreach ($services as $key => $service) {
+                $children = collect($service->children)->flatten();
+                unset($service->children);
+                $mergedServices[] = $service->toArray();
+                $children = $children->toArray();
+                foreach ($children as $child) {
+                    $mergedServices[] = $child;
+                }
+    
+            }
+    
+            return $mergedServices; 
+        }
         $allService = Services::where('parent_id', 0)
             ->where('slug', 'all')
             ->first();
