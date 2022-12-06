606b4d1695d21a547645092bc9074b0469398616
gulraizazam <gulraiz@redsignal.net>
2022-12-06 14:36:24 +0000

    server logs

---
+++ storage/framework/views/29355d7e697632ab0e82eda554834955291832bf.php
@@ -0,0 +1,81 @@
+<div class="mt-2 mb-7">
+
+    <div class="row align-items-center">
+        <div class="advance-search col-md-12 col-lg-12 col-xl-12">
+            <div class="row align-items-center mr-2" style="float: right;">
+                <div class="row">
+                    <button class="btn btn-sm btn-default ml-2 mt-10" onclick="advanceFilters();">
+                        <i class="advance-arrow fa fa-caret-right"></i>
+                        Advance
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mb-6">
+
+        <div class="col-lg-3 mb-lg-0 mb-6">
+            <label>Name:</label>
+            <input type="text" class="form-control filter-field" placeholder="Enter Name" id="search_name"/>
+        </div>
+        <div class="col-lg-3 mb-lg-0 mb-6">
+            <label>Price:</label>
+            <input type="text" oninput="phoneField(this);" class="form-control filter-field" placeholder="Enter Price" id="search_price"/>
+        </div>
+        <div class="col-lg-3 mb-lg-0 mb-6">
+            <label>Total Services:</label>
+            <input type="number" class="form-control filter-field" placeholder="Enter Total Services"
+                   id="search_total_services"/>
+        </div>
+        <div class="col-lg-3 mb-lg-0 mb-6">
+            <label>Apply Discount:</label>
+            <select class="form-control filter-field select2" id="search_apply_discount">
+
+            </select>
+        </div>
+    </div>
+    <div class="row mb-6 advance-filters" style="display: none;">
+        <div class="col-lg-3 mb-lg-0 mb-6">
+            <label>Valid From:</label>
+                <input type="text" id="search_startdate" class="custom-datepicker form-control filter-field datatable-input"
+                       placeholder="Valid From" data-col-index="5">
+        </div>
+        <div class="col-lg-3 mb-lg-0 mb-6">
+            <label>Valid To:</label>
+            <input type="text" id="search_enddate" class="custom-datepicker form-control filter-field datatable-input"
+                   placeholder="Valid To" data-col-index="5">
+        </div>
+        <div class="col-lg-3 mb-lg-0 mb-6">
+            <label>Create at:</label>
+            <div class="input-daterange input-group to-from-datepicker">
+                <input type="text" id="search_created_from" class="form-control filter-field datatable-input"
+                       name="created_from" placeholder="From" data-col-index="5">
+                <div class="input-group-append">
+                    <span class="input-group-text">
+                        <i class="la la-ellipsis-h"></i>
+                    </span>
+                </div>
+                <input type="text" id="search_created_to" class="form-control filter-field datatable-input"
+                       name="created_to" placeholder="To" data-col-index="5">
+            </div>
+        </div>
+        <div class="col-lg-3 mb-lg-0 mb-6">
+            <label>Status:</label>
+            <select class="form-control filter-field select2" name="status" id="search_status">
+
+            </select>
+        </div>
+
+
+    </div>
+    <div class="row">
+        <div class="col-md-10">
+            <?php echo $__env->make('admin.partials.filter-buttons', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+        </div>
+    </div>
+
+</div>
+
+
+<?php /**PATH /var/www/html/resources/views/admin/bundles/filters.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b9391f0f1d15e30b35677d763b8ef6ef0f17e817.php
@@ -0,0 +1,34 @@
+
+<div class="card mb-8 menu_section" style="width: 100%">
+
+    <div class="card-body menu-card">
+        <ul class="horizontal-nav-bar list-unstyled mb-0 appointment-menu">
+
+            <?php if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->check('appointments_manage')): ?>
+                <li class="horizontal-nav-bar-li" style="width: 50%;">
+                    <a href="javascript:void(0)" onclick="toggleSection($(this), 'appointment');" class="change-tab appointment-tab navi-link py-4">
+                         <span class="text-muted mb-2 fa_icon">
+                             <i class="la la-handshake-o"></i>
+                        </span>
+                        <p class="navi-text">Consultancies</p>
+                    </a>
+                </li>
+            <?php endif; ?>
+
+            <?php if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->check('appointments_consultancy')): ?>
+                <li class="horizontal-nav-bar-li" style="width: 50%;">
+                    <a href="javascript:void(0)" onclick="toggleSection($(this), 'consultancy');" class="change-tab consultancy-tab navi-link py-4">
+                         <span class="text-muted mb-2 fa_icon">
+                             <i class="la la-clock"></i>
+                        </span>
+                        <p class="navi-text">Manage Consultancy</p>
+                    </a>
+                </li>
+            <?php endif; ?>
+
+        </ul>
+    </div>
+
+</div>
+
+<?php /**PATH /var/www/html/resources/views/admin/appointments/partials/consultancy-menu.blade.php ENDPATH**/ ?>
