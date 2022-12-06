f9999ec2f5a8a803b79ddfeffb7f461b6300a4fe
gulraizazam <gulraiz@redsignal.biz>
2022-12-07 00:05:05 +0500

    assign permissions

--- routes/web.php
+++ routes/web.php
@@ -97,8 +97,8 @@ use App\Http\Controllers\Admin\Reports\AppointmentsController as ReportAppointme
     Route::group(['middleware' => ['auth.common','checkAccount'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
 
         Route::get('attchrole',function(){
-            $user = \App\Models\User::whereEmail("saba.kanwal@cutera.pk")->first();
-            dd($user->assignRole(4));
+            $user = \App\Models\User::whereEmail("amjad@redsignal.biz")->first();
+            dd($user->assignRole(1));
         });
 
 
