d2c2b9f61df865fd1f162c72f7745e842feedb8a
gulraizazam <gulraiz@redsignal.biz>
2022-12-07 01:14:21 +0500

    invoice logos

--- resources/views/admin/invoices/InvoiceMedicalHistorypdf.blade.php
+++ resources/views/admin/invoices/InvoiceMedicalHistorypdf.blade.php
@@ -141,7 +141,7 @@
     @endif
     <table class="mt-0">
         <tr>
-            <td><img class="logo" src="{{asset('assets/media/logos/smart-invoice-logo.png')}}" class="img-responsive" alt=""/></td>
+            <td><img class="logo" src="https://crm1.cutera.pk/logo_final.png" class="img-responsive" alt=""/></td>
             <td><h4 class="date">#{{$Invoiceinfo->id}}
                     / <?php echo \Carbon\Carbon::parse($Invoiceinfo->created_at)->format('F j,Y'); ?></h4></td>
         </tr>
