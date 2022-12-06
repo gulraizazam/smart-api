1c6dec5398b197427124b46ef6d917c297fd45ec
gulraizazam <gulraiz@redsignal.biz>
2022-12-07 01:15:33 +0500

    invoice logos

--- resources/views/admin/invoices/invoice_pdf.blade.php
+++ resources/views/admin/invoices/invoice_pdf.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Smart</title>
+    <title>Cutera Aesthetics</title>
     <meta content="Cutera Aesthetics is a Medical Spa offering more than 60 treatment for skin rejuvenation and body contouring" name="description" />
     <meta content="Red Signal" name="author"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
