050bafa44806722746dbefb177d93ea145130a0d
gulraizazam <gulraiz@redsignal.biz>
2022-12-07 13:04:41 +0500

    export issue

--- app/Http/Controllers/Admin/LeadsController.php
+++ app/Http/Controllers/Admin/LeadsController.php
@@ -2785,8 +2785,8 @@ class LeadsController extends Controller
     }
 
     public function exportDocs(Request $request) {
-        set_time_limit(0);
-        ini_set('memory_limit', '-1');
+        // set_time_limit(0);
+        // ini_set('memory_limit', '-1');
         return Excel::download(new ExportLead($request), 'leads.'.$request->ext);
     }
 }
