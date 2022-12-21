2e4250f062e4367cabf9fd9d3bd593b982aec08d
gulraizazam <gulraiz@redsignal.biz>
2022-12-21 13:19:17 +0500

    dashboard stats

--- app/Reports/dashboardreport.php
+++ app/Reports/dashboardreport.php
@@ -356,5 +356,163 @@ class dashboardreport
             $total
         ];
     }
+    public static function collectionbycenter($location_informations, $account_id, $where,$request)
+    {
+       
+        $total = 0;
+        
+        $wherecondtion = array();
+        foreach ($location_informations as $key => $location_infomation) {
+            if ($where == 'today') {
+                $packagesadvances = PackageAdvances::whereDate('created_at', '=', Carbon::now()->format('Y-m-d'))
+                    ->where([
+                        ['account_id', '=', $account_id],
+                        ['location_id', '=', $key],
+                    ])->get();
+                   
+            }
+            if ($where == 'yesterday') {
+                
+                $packagesadvances = PackageAdvances::whereDate('created_at', '=', Carbon::now()->subDay(1)->format('Y-m-d'))
+                    ->where([
+                        ['account_id', '=', $account_id],
+                        ['location_id', '=', $key],
+                    ])->get();
+            }
+            if ($where == 'last7day') {
+                $packagesadvances = PackageAdvances::whereDate('created_at', '>=', Carbon::now()->subDay(6)->format('Y-m-d'))
+                    ->whereDate('created_at', '<=', Carbon::now()->format('Y-m-d'))
+                    ->where([
+                        ['account_id', '=', $account_id],
+                        ['location_id', '=', $key],
+                    ])->get();
+            }
+            if ($where == 'thisMonth') {
+                $packagesadvances = PackageAdvances::whereDate('created_at', '>=', Carbon::now()->startOfMonth()->format('Y-m-d'))
+                    ->whereDate('created_at', '<=', Carbon::now()->endOfMonth()->format('Y-m-d'))
+                    ->where([
+                        ['account_id', '=', $account_id],
+                        ['location_id', '=', $key],
+                    ])->get();
+            }
+            if ($where == 'lastmonth') {
+                
+                $packagesadvances = PackageAdvances::whereDate('created_at','>=', Carbon::now()->subMonth()->StartOfMonth()->format('Y-m-d') )
+                    ->whereDate('created_at', '<=', Carbon::now()->subMonth()->endOfMonth()->format('Y-m-d'))
+                    ->where([
+                        ['account_id', '=', $account_id],
+                        ['location_id', '=', $key],
+                    ])->get();
+                    
+            }
+            $location_single_info = Locations::find($key);
+            
+            if ($packagesadvances) {
+                $balance = 0;
+                $total_balance = 0;
+                $total_revenue_cash_in = 0;
+                $total_revenue_card_in = 0;
+                $total_refund_out = 0;
+
+                foreach ($packagesadvances as $packagesadvance) {
+                    
+                    if (
+                        (
+                            $packagesadvance->cash_flow == 'in' &&
+                            $packagesadvance->is_adjustment == '0' &&
+                            $packagesadvance->is_tax == '0' &&
+                            $packagesadvance->is_cancel == '0'
+                        )
+                    ) {
+                        switch ($packagesadvance->cash_flow) {
+                            case 'in':
+                                $balance = $balance + $packagesadvance->cash_amount;
+                                
+                                break;
+                            case 'out':
+                                $balance = $balance - $packagesadvance->cash_amount;
+                                
+                                break;
+                            default:
+                                break;
+                        }
+                        $total_balance = $balance;
+                       
+                        if ($packagesadvance->cash_amount != 0) {
+                            if ($packagesadvance->package_id) {
+                                $transtype = Config::get('constants.trans_type.advance_in');
+                            }
+                            if ($packagesadvance->invoice_id && $packagesadvance->cash_flow == 'in') {
+                                $transtype = Config::get('constants.trans_type.advance_in');
+                            }
+                            if ($packagesadvance->is_adjustment == '1') {
+                                $transtype = Config::get('constants.trans_type.adjustment');
+                            }
+                            if ($packagesadvance->is_cancel == '1') {
+                                $transtype = Config::get('constants.trans_type.invoice_cancel');
+                            }
+                            if ($packagesadvance->invoice_id && $packagesadvance->cash_flow == 'out') {
+                                $transtype = Config::get('constants.trans_type.invoice_create');
+                            }
+                            if ($packagesadvance->is_refund == '1') {
+                                $transtype = Config::get('constants.trans_type.refund_in');
+                            }
+                            if ($packagesadvance->is_tax == '1') {
+                                $transtype = Config::get('constants.trans_type.tax_out');
+                            }
+                            if ($packagesadvance->cash_flow == 'in') {
+                                if ($packagesadvance->paymentmode->name == 'Cash') {
+                                    $revenue_cash_in = $packagesadvance->cash_amount;
+                                    $revenue_card_in = '';
+                                    $revenue_bank_in = '';
+                                    $refund_out = '';
+                                }
+                                if ($packagesadvance->paymentmode->name == 'Card') {
+                                    $revenue_cash_in = '';
+                                    $revenue_card_in = $packagesadvance->cash_amount;
+                                    $revenue_bank_in = '';
+                                    $refund_out = '';
+                                }
+                                if ($packagesadvance->paymentmode->name == 'Bank/Wire Transfer') {
+                                    $revenue_cash_in = '';
+                                    $revenue_card_in = '';
+                                    $revenue_bank_in = $packagesadvance->cash_amount;
+                                    $refund_out = '';
+                                }
+                            } else {
+                                $revenue_cash_in = '';
+                                $revenue_card_in = '';
+                                $revenue_bank_in = '';
+                                $refund_out = $packagesadvance->cash_amount;
+                            }
+
+                            if ($revenue_cash_in) {
+                                $total_revenue_cash_in += $revenue_cash_in;
+                            }
+                            if ($revenue_card_in) {
+                                $total_revenue_card_in += $revenue_card_in;
+                            }
+                            if ($revenue_bank_in) {
+                                $total_revenue_card_in += $revenue_bank_in;
+                            }
+                            if ($refund_out) {
+                                $total_refund_out += $refund_out;
+                            }
+                        }
+                    }
+                }
+            }
+            $total_revenue = $total_revenue_cash_in + $total_revenue_card_in;
+            
+            $In_hand_balance = $total_revenue - $total_refund_out;
+            $total += $In_hand_balance;
+              
+               
+        }
+        
+        return [
+           $total
+        ];
+    }
 
 }
