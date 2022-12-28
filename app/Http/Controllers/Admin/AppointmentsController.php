63a054ef6a289c575ac4e7241e789cf6ef450557
gulraizazam <gulraiz@redsignal.biz>
2022-12-28 12:10:11 +0500

    changed treatment time

--- app/Http/Controllers/Admin/AppointmentsController.php
+++ app/Http/Controllers/Admin/AppointmentsController.php
@@ -4942,7 +4942,7 @@ class AppointmentsController extends Controller
 
             if ($doctor_checking && $room_check_availability) {
                 $appointmentData['scheduled_date'] = Carbon::parse($request->get("start"))->format("Y-m-d");
-                $appointmentData['scheduled_time'] = Carbon::parse($request->get("start"))->format("h:i:s ");
+                $appointmentData['scheduled_time'] = Carbon::parse($request->get("start"))->format("H:i:s ");
 
                 $appointmentData['first_scheduled_date'] = Carbon::parse($request->get("start"))->format("Y-m-d");
                 $appointmentData['first_scheduled_time'] = Carbon::parse($request->get("start"))->format("H:i:s");
--- resources/views/admin/invoices/invoice_pdf.blade.php
+++ resources/views/admin/invoices/invoice_pdf.blade.php
@@ -194,7 +194,7 @@
             <td>
                 <img class="img-responsive logo" src="{{asset('assets/media/new_logo.png')}}" style="width:235px;" alt=""/>
                 <p class="logo_caption">{{$location_info->address}}.</p>
-                <p class="logo_caption logo_caption2">Phone. {{$location_info->fdo_phone}}  &nbsp; |  &nbsp; Email. {{$account->email}}  &nbsp; | &nbsp;  www.cutera.pk  &nbsp; | &nbsp; NTN. {{$location_info->ntn}} &nbsp; | &nbsp; STN. {{$location_info->stn}}</p>
+                <p class="logo_caption logo_caption2">Phone. {{$location_info->fdo_phone}}  &nbsp; |  &nbsp; Email. {{$account->email}}  &nbsp; | &nbsp;  www.cuteraesthetics.com  &nbsp; | &nbsp; NTN. {{$location_info->ntn}} &nbsp; | &nbsp; STN. {{$location_info->stn}}</p>
             </td>
             <td style="padding:0px !important; float:right; width:120px; text-align:right;">
                 <div class="invoice_btn" style="width:120px; float:right; text-align:right;">
