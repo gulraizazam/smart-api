e07177d8a82274d2160fb2151502cb0cb23558b9
gulraizazam <gulraiz@redsignal.biz>
2022-12-30 13:09:11 +0500

    rota issue

--- app/Models/ResourceHasRotaDays.php
+++ app/Models/ResourceHasRotaDays.php
@@ -333,7 +333,7 @@ class ResourceHasRotaDays extends Model
             } else {
                 $appointments = [];
             }
-            dd( $appointments);
+           
         }
 
         return $appointments;
