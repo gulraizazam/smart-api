bb67dacc4340dbc8c6b65efa0f28ee2842fca357
gulraizazam <gulraiz@redsignal.biz>
2022-12-30 12:06:45 +0500

    rota issue

--- app/Http/Controllers/Admin/ResourceRotasController.php
+++ app/Http/Controllers/Admin/ResourceRotasController.php
@@ -679,8 +679,8 @@ class ResourceRotasController extends Controller
         if ($resourcerota->end <= $request->end) {
 
             $response = ResourceHasRota::updateRecord($id, $request, Auth::User()->account_id);
-
-            return ApiHelper::apiResponse($this->success, 'Record updated', $response['status']);
+dd($response);
+            return ApiHelper::apiResponse($this->success, $response['message'], $response['status']);
 
         }
 
