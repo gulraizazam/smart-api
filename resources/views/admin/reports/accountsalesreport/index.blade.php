06c7de3cdba5743bd95feda6fa73b61cfd8036c1
gulraizazam <gulraiz@redsignal.biz>
2022-12-30 18:04:09 +0500

    reports issue resolved

--- resources/views/admin/reports/accountsalesreport/index.blade.php
+++ resources/views/admin/reports/accountsalesreport/index.blade.php
@@ -199,6 +199,7 @@
                                             <div class="form-group col-md-3 sn-select @if($errors->has('location_id')) has-error @endif"
                                                  style="display: none;" id="location_id_D">
                                                 {!! Form::label('location_id_com', 'Centres', ['class' => 'control-label']) !!}
+                                                
                                                 {!! Form::select('location_id_com[]', $locations_com, null, ['id' => 'location_id_com','class' => 'form-control select2', 'multiple' => 'multiple']) !!}
                                                 <span id="location_id_handler"></span>
                                             </div>
@@ -286,6 +287,9 @@
     @push('js')
 
         <script>
+            $("#location_id_com").on('change',function(){
+                $("#location_id_com-report").val($("#location_id_com").val());
+            });
             $('#date_range').daterangepicker({
                 locale: {
                 },
