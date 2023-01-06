8d4ecae2ab3107e4457e44dde015c616d6e6becb
gulraizazam <gulraiz@redsignal.biz>
2023-01-06 17:57:38 +0500

    sms issue

--- app/Http/Controllers/Admin/AppointmentsController.php
+++ app/Http/Controllers/Admin/AppointmentsController.php
@@ -5410,6 +5410,7 @@ class AppointmentsController extends Controller
                 $screen = $appointment->appointment_type_id == 1 ? 'Consultancy' : 'Treatment';
                 GeneralFunctions::saveAppointmentLogs('rescheduled', $screen, $appointment);
                 $log_type = 'sms';
+                dd($appointment);
                 $this->sendRescheduleSMS($request->appointment_id, $appointment->phone, $log_type, $appointment->account_id);
                 return ApiHelper::apiResponse($this->success, 'Record updated successfully!');
             }
