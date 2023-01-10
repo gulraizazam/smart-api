b7392295c12d330b6dbea7e2b40af781870d576f
gulraizazam <gulraiz@redsignal.biz>
2023-01-10 13:25:03 +0500

    logo edited

--- public/assets/js/pages/admin_settings/locations.js
+++ public/assets/js/pages/admin_settings/locations.js
@@ -1,5 +1,4 @@
 var table_url = route('admin.locations.datatable');
-
 var table_columns = [
     {
         field: 'id',
@@ -64,57 +63,48 @@ var table_columns = [
             return actions(data);
         }
     }];
-
-
 function actions(data) {
     if (typeof data.id !== 'undefined') {
         let id = data.id;
-
         let csrf = $('meta[name="csrf-token"]').attr('content');
         let url = route('admin.locations.edit', {id: id});
         let delete_url = route('admin.locations.destroy', {id: id});
-
         if (permissions.edit || permissions.delete) {
             let actions = '<div class="dropdown dropdown-inline action-dots">\
-        <a href="javascript:void(0);" class="btn btn-sm btn-clean btn-icon mr-2" data-toggle="dropdown">\
-            <i class="ki ki-bold-more-hor" aria-hidden="true"></i>\
-        </a>\
-        <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">\
-            <ul class="navi flex-column navi-hover py-2">\
-                <li class="navi-header font-weight-bolder text-uppercase font-size-xs text-primary pb-2">\
-                    Choose an action: \
-                    </li>';
-            if (permissions.edit) {
-                actions += '<li class="navi-item">\
-                    <a href="javascript:void(0);" onclick="editRow(`' + url + '`);" class="navi-link">\
-                        <span class="navi-icon"><i class="la la-pencil"></i></span>\
-                        <span class="navi-text">Edit</span>\
-                    </a>\
-                </li>';
-            }
-            if (permissions.delete) {
-                actions += '<li class="navi-item">\
-                        <a href="javascript:void(0);" onclick="deleteRow(`' + delete_url + '`);" class="navi-link">\
-                        <span class="navi-icon"><i class="la la-trash"></i></span>\
-                        <span class="navi-text">Delete</span>\
-                        </a>\
-                     </li>';
-            }
-
-            actions += '</ul>\
-        </div>\
-    </div>';
-
+                <a href="javascript:void(0);" class="btn btn-sm btn-clean btn-icon mr-2" data-toggle="dropdown">\
+                    <i class="ki ki-bold-more-hor" aria-hidden="true"></i>\
+                </a>\
+                <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">\
+                    <ul class="navi flex-column navi-hover py-2">\
+                        <li class="navi-header font-weight-bolder text-uppercase font-size-xs text-primary pb-2">\
+                            Choose an action: \
+                        </li>';
+                        if (permissions.edit) {
+                            actions += '<li class="navi-item">\
+                                <a href="javascript:void(0);" onclick="editRow(`' + url + '`);" class="navi-link">\
+                                    <span class="navi-icon"><i class="la la-pencil"></i></span>\
+                                    <span class="navi-text">Edit</span>\
+                                </a>\
+                            </li>';
+                        }
+                        if (permissions.delete) {
+                            actions += '<li class="navi-item">\
+                                    <a href="javascript:void(0);" onclick="deleteRow(`' + delete_url + '`);" class="navi-link">\
+                                    <span class="navi-icon"><i class="la la-trash"></i></span>\
+                                    <span class="navi-text">Delete</span>\
+                                    </a>\
+                                </li>';
+                        }
+                    actions += '</ul>\
+                </div>\
+            </div>';
             return actions;
         }
     }
     return '';
 }
-
 function editRow(url) {
-
     $("#modal_edit_locations").modal("show");
-
     $.ajax({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -123,43 +113,27 @@ function editRow(url) {
         type: "GET",
         cache: false,
         success: function (response) {
-
             setEditData(response);
-
             reInitSelect2(".select2", "");
-
         },
         error: function (xhr, ajaxOptions, thrownError) {
             errorMessage(xhr);
-
             reInitValidation(TownValidation);
         }
     });
-
-
 }
-
 function setEditData(response) {
-
     let location = response.data.location;
-
     $("#modal_edit_location_form").attr("action", route('admin.locations.update', {id: location.id}));
-
     let service_location = response.data.service_location;
-
     let cities = response.data.cities;
     let cities_options = '<option value="">Select A City</option>';
-
     Object.entries(cities).forEach(function(value, index) {
         cities_options += '<option value="'+value[0]+'">'+value[1]+'</option>';
     });
-
     $("#edit_location_cities").html(cities_options);
-
     let service_options = makeServiceOptions(response);
-
     $("#edit_location_services").html(service_options);
-
     $("#edit_name").val(location.name);
     $("#edit_fdo_name").val(location.fdo_name);
     $("#edit_fdo_phone").val(location.fdo_phone);
@@ -168,20 +142,14 @@ function setEditData(response) {
     $("#edit_tax_percentage").val(location.tax_percentage);
     $("#edit_ntn").val(location.ntn);
     $("#edit_stn").val(location.stn);
-
-    let image = asset_url +'assets/media/logos/smart.svg';
+    let image = asset_url +'assets/media/new_logo.png';
     if (location.image_src != '') {
         image = asset_url +'storage/centre_logo/'+ location.image_src;
     }
     $("#edit-image").css('background-image', "url(" + image + ")");
     $("#edit_location_cities").val(location.city_id).change();
     $("#edit_location_services").val(service_location).change();
-
-
-
-
 }
-
 function applyFilters(datatable) {
 
     $('#apply-filters').on('click', function() {
@@ -205,7 +173,6 @@ function applyFilters(datatable) {
     });
 
 }
-
 function resetAllFilters(datatable) {
 
     $('#reset-filters').on('click', function() {
@@ -226,7 +193,6 @@ function resetAllFilters(datatable) {
     });
 
 }
-
 function setFilters(filter_values, active_filters) {
 
     let cities = filter_values.cities;
@@ -278,7 +244,6 @@ function setFilters(filter_values, active_filters) {
 
     getUserCity();
 }
-
 function createCentre($route) {
 
     $.ajax({
@@ -300,7 +265,6 @@ function createCentre($route) {
         }
     });
 }
-
 function setCreateData(response) {
 
 
@@ -317,11 +281,10 @@ function setCreateData(response) {
 
     $("#add_location_services").html(service_options);
 
-    let image = asset_url +'assets/media/logos/smart.svg';
+    let image = asset_url +'assets/media/new_logo.png';
 
     $("#add_center_image").css('background-image', "url(" + image + ")");
 }
-
 function makeServiceOptions(response) {
 
     let services = response.data.services;
@@ -354,7 +317,6 @@ function makeServiceOptions(response) {
 
     return service_options;
 }
-
 function hideShowAdvanceFilters(active_filters) {
     if ((typeof active_filters.city_id !== 'undefined' && active_filters.city_id != '')
         || (typeof active_filters.region_id !== 'undefined' && active_filters.region_id != '')
