e0b26764375d6e8a7f87e2a952c62d49ff6b2005
gulraizazam <gulraiz@redsignal.biz>
2023-01-17 13:12:52 +0500

    filters

--- resources/views/admin/appointments/treatment.blade.php
+++ resources/views/admin/appointments/treatment.blade.php
@@ -270,7 +270,7 @@
                       
                         $("#treatment_city_filter").val(result.city_id).change();
                         $("#treatment_location_filter").val(result.location_id).change();
-                        $("#treatment_doctor_filter").val(result.doctor_id);
+                        $("#treatment_doctor_filter").val(result.doctor_id).change();
                         $("#treatment_resource_filter").val(result.machine_id);
                         setDashboardFilters();
                     }, 900);
