3ff8febdfbe12d59a1e9b3f43b406c6079d91072
gulraizazam <gulraiz@redsignal.biz>
2023-01-18 17:28:38 +0500

    filters reload issue

--- resources/views/admin/appointments/treatment.blade.php
+++ resources/views/admin/appointments/treatment.blade.php
@@ -169,55 +169,36 @@
     @push('js')
         <script>
             $(document).ready(function () {
-                // var result = get_query();
-                // console.log(result);
-                // if (typeof result.tab !== 'undefined') {
-                //     $("." + result.tab+ '-tab').click();
-                //     if (typeof result.city_id !== "undefined"
-                //     && typeof result.location_id !== "undefined"
-                //     && typeof result.doctor_id !== "undefined"
-                //     && typeof result.machine_id !== "undefined"
-                //     && typeof result.tab !== 'undefined' && result.tab == "treatment") {
-                    var geturl = window.location.href;
-                    if(geturl.includes('city_id')){
-                        geturl = geturl.split('city_id=')[1].split('&location_id')[0];
-                        if(geturl !== "undefined"){
-                            setTimeout(function(){
-                                $("#treatment_city_filter").val(geturl).change();
-                            },500);
-                        }
+                var result = get_query();
+                console.log(result);
+                if (typeof result.tab !== 'undefined') {
+                    $("." + result.tab+ '-tab').click();
+                } else {
+                    $(".appointment-tab").addClass("nav-bar-active")
+                }
+                if (typeof result.city_id !== "undefined"
+                    && typeof result.location_id !== "undefined"
+                    && typeof result.doctor_id !== "undefined"
+                    && typeof result.machine_id !== "undefined"
+                    && typeof result.tab !== 'undefined') {
+                    loadDoctors(result.location_id, result.tab);
+                    setTimeout( function () {
+                        // alert('test');?
+                        $("#treatment_city_filter").val(result.city_id).change();
+                        $("#treatment_location_filter").val(result.location_id).change();
+                        // if($("#treatment_city_filter"))
+                        $("#treatment_doctor_filter").val(result.doctor_id);
+                        $("#treatment_resource_filter").val(result.machine_id);
 
-                    }
-                    // setTimeout( function () {
+                        console.log('result.city_id', result.city_id);
+                        console.log('result.location_id', result.location_id);
+                        console.log('result.doctor_id', result.doctor_id);
+                        console.log('result.machine_id', result.machine_id);
                         
-                    //     loadDoctors(result.location_id, result.tab);
-                    //     $("#treatment_city_filter").val(result.city_id).change();
-                    //     $("#treatment_location_filter").val(result.location_id).change();
-
-                    //     if($("#treatment_city_filter").length && $("#treatment_location_filter").length && $("#treatment_doctor_filter").length && $("#treatment_resource_filter").length){
-                    //         // alert('test');
-                    //         $("#treatment_doctor_filter").val(result.doctor_id);
-                    //         $("#treatment_doctor_filter").val(result.doctor_id).attr('selected', true);
-                    //         $("#treatment_doctor_filter").val(result.doctor_id).prop('selected', true);
-                    //         $("#treatment_resource_filter").val(result.machine_id);
-                    //     }
-
-                    //     // console.log('result.city_id', result.city_id);
-                    //     // console.log('result.location_id', result.location_id);
-                    //     // console.log('result.doctor_id', result.doctor_id);
-                    //     // console.log('result.machine_id', result.machine_id);
-                        
-                    //     setDashboardFilters();
-                    // }, 1000);
-                // }
-                // } else {
-                //     $(".appointment-tab").addClass("nav-bar-active")
-                // }
-                
+                        setDashboardFilters();
+                    }, 1000);
+                }
             });
-            // $(document).ajaxComplete(function(){
-            
-            // });
             let appointment_limit = '{{config('constants.export-appointment-limit')}}';
             var limit = '{{config('constants.export-appointment-limit')}}';
             var offset = 0;
