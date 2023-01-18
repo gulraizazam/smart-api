9271cfbd7875ea1796d283e9fbbb689e2d48752c
gulraizazam <gulraiz@redsignal.biz>
2023-01-18 17:28:38 +0500

    filters reload issue

--- resources/views/admin/appointments/services/filters.blade.php
+++ resources/views/admin/appointments/services/filters.blade.php
@@ -4,12 +4,12 @@
 
         <div class="col-lg-3 mb-lg-0 mb-6">
             <label>City:</label>
-            <select onchange="loadLocations($(this).val(), 'treatment');" class="form-control select2" id="treatment_city_filter"></select>
+            <select onchange="loadLocations($(this).val(), 'treatment');" class="form-control" id="treatment_city_filter"></select>
         </div>
 
         <div class="col-lg-3 mb-lg-0 mb-6">
             <label>Centre:</label>
-            <select onchange="loadDoctors($(this).val(), 'treatment');" class="form-control select2" id="treatment_location_filter"></select>
+            <select onchange="loadDoctors($(this).val(), 'treatment');" class="form-control" id="treatment_location_filter"></select>
         </div>
 
         <div class="col-lg-3 mb-lg-0 mb-6">
