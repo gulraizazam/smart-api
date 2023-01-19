43397aae1a73efb74a1383b029bbd568043ffc11
gulraizazam <gulraiz@redsignal.biz>
2023-01-19 19:31:23 +0500

    records test

---
+++ resources/views/admin/records.blade.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
+    </head>
+    <body>
+    <table id="example" class="display" style="width:100%">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011-04-25</td>
+                <td>$320,800</td>
+            </tr>
+            
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </tfoot>
+    </table>
+    </body>
+    <script href="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script href="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+    <script>$$(document).ready(function () {
+            $('#example').DataTable();
+        });
+</script>
+</html>
