fe1f15db46d0fe0d3cac64a4c1c3d263e8f53326
gulraizazam <gulraiz@redsignal.biz>
2023-01-19 19:52:11 +0500

    records test

--- resources/views/admin/records.blade.php
+++ resources/views/admin/records.blade.php
@@ -8,11 +8,12 @@
             <tr>
                 <th>Name</th>
                 <th>scheduled_date</th>
-                <th>first_scheduled_date</th>
+                <th>Invoice Creation Date</th>
+               
                 <th>scheduled_time</th>
                 <th>doctor_id</th>
                 <th>location_id</th>
-                <th>appointment_id</th>
+              
                 <th>service_id</th>
                 <th>total_price(invoice)</th>
                 <th>cash_amount</th>
@@ -24,17 +25,18 @@
             <?php 
                 $service = \App\Models\Services::where('id',$rec->service_id)->first();
                 $doc = \App\Models\Doctors::where('id',$rec->doctor_id)->first();
-                $location = \App\Models\Locations::where('id',$rec->slocation_id)->first();
+                $location = \App\Models\Locations::where('id',$rec->location_id)->first();
                 
             ?>
             <tr>
                 <td>{{$rec->name}}</td>
                 <td>{{$rec->scheduled_date}}</td>
-                <td>{{$rec->first_scheduled_date}}</td>
+                <td>{{$rec->scheduled_date}}</td>
+              
                 <td>{{$rec->scheduled_time}}</td>
-                <td>{{$doc->name}}</td>
-                <td>{{$rec->location_id}}</td>
-                <td>{{$rec->appointment_id}}</td>
+                <td>{{$doc->name ?? ''}}</td>
+                <td>{{$location->name ?? ''}}</td>
+               
                 <td>{{$service->name}}</td>
                 <td>{{$rec->total_price}}</td>
                 <td>{{$rec->cash_amount}}</td>
