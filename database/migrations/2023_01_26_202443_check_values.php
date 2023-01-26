2852a9a79f4f0df56b4654c5e3041eb502786d92
gulraizazam <gulraiz@redsignal.biz>
2023-01-26 20:49:05 +0500

    trigger

---
+++ database/migrations/2023_01_26_202443_check_values.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CheckValues extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('CREATE TRIGGER check_update_time AFTER UPDATE ON `package_advances` FOR EACH ROW
+        BEGIN
+           INSERT INTO `track_values` (`update_time`) VALUES (`1`);
+        END');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER `add_Item_city`');
+    }
+}
