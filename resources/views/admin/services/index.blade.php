611c71c8b9374fe5aac834e6d6354e2707d2f79d
gulraizazam <gulraiz@redsignal.biz>
2023-02-08 15:22:02 +0500

    resolved cue-324

--- resources/views/admin/services/index.blade.php
+++ resources/views/admin/services/index.blade.php
@@ -129,6 +129,15 @@
                             $("#service_color").val(data.color);
                         }
                     });
+                    $('#service_duration').css('display','block');
+                    $('#service_price').css('display','block');
+                    $('#service_tax').css('display','block');
+                    $('#service_node').css('display','block');
+                }else{
+                    $('#service_duration').css('display','none');
+                    $('#service_price').css('display','none');
+                    $('#service_tax').css('display','none');
+                    $('#service_node').css('display','none');
                 }
                 
             }
@@ -144,7 +153,19 @@
                             $("#edit_color").val(data.color);
                         }
                     });
+                    $('#service_duration').css('display','block');
+                    $('#service_price').css('display','block');
+                    $('#service_tax').css('display','block');
+                    $('#service_node').css('display','block');
+                    $('#main_edit_complimentary').css('display','block');
+                }else{
+                    $('#service_duration').css('display','none');
+                    $('#service_price').css('display','none');
+                    $('#service_tax').css('display','none');
+                    $('#service_node').css('display','none');
+                    $('#main_edit_complimentary').css('display','none');
                 }
+                
                 
             }
         </script>
