094942d7d76abedb3fc540481baff61491aad472
gulraizazam <gulraiz@redsignal.biz>
2023-02-28 18:54:49 +0500

    resolved cue-355

--- resources/views/admin/patients/filters.blade.php
+++ resources/views/admin/patients/filters.blade.php
@@ -1,5 +1,4 @@
 <div class="mt-2 mb-7">
-
     <div class="row align-items-center">
         <div class="advance-search col-md-12 col-lg-12 col-xl-12">
             <div class="row align-items-center mr-2" style="float: right;">
@@ -12,43 +11,31 @@
             </div>
         </div>
     </div>
-
-
-
     <div class="row mb-6">
-
-        <div class="col-lg-3 mb-lg-0 mb-6">
-            <label>Patient ID:</label>
-            <input type="text" class="form-control filter-field" placeholder="Enter Patient ID" id="search_patient_id" />
+        <div class="col-lg-2 mb-lg-0 mb-6" id="patient_id">
+            <label>Patient Search:</label>
+            <input style="width: 110%;" class="form-control filter-field patient_id">
+            <input type="hidden" class="filter-field search_field" id="search_patient_id">
+            <span onclick="addUsers()" class="croxcli" style="position:absolute; padding-left: 0% !important; top:37px; right:3px;"><i class="fa fa-times" aria-hidden="true"></i></span>
+            <div class="suggesstion-box" style="display: none;">
+                <ul class="suggestion-list"></ul>
+            </div>
         </div>
-
         <div class="col-lg-3 mb-lg-0 mb-6">
             <label>Name:</label>
             <input class="form-control filter-field" id="search_name" placeholder="Enter Name">
         </div>
-
         <div class="col-lg-3 mb-lg-0 mb-6">
             <label>Email:</label>
             <input class="form-control filter-field" id="search_email" placeholder="Enter Email">
         </div>
-
-        <div class="col-lg-3 mb-lg-0 mb-6">
-            <label>Phone:</label>
-            <input type="text" class="form-control filter-field" placeholder="e.g: 0300XXXXXXX" id="search_phone" />
-        </div>
-
-
-    </div>
-
-    <div class="row mb-8 advance-filters" style="display: none;">
-
-
         <div class="col-lg-3 mb-lg-0 mb-6">
             <label>Gender:</label>
             <select class="form-control filter-field select2" id="search_gender">
             </select>
         </div>
-
+    </div>
+    <div class="row mb-8 advance-filters" style="display: none;">
         <div class="col-lg-3 mb-lg-0 mb-6">
             <label>Create at:</label>
             <div class="input-daterange input-group to-from-datepicker" >
@@ -69,12 +56,9 @@
             </div>
         @endif
     </div>
-
     <div class="row">
         <div class="col-md-10">
-
             @include('admin.partials.filter-buttons')
-
         </div>
     </div>
 </div>
