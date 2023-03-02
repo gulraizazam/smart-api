497fb2fa712d05f2ced11df84c0003c8ad09c127
gulraizazam <gulraiz@redsignal.biz>
2023-03-02 12:59:06 +0500

    resolved conflict

--- public/assets/js/pages/appointment/datatable.js
+++ public/assets/js/pages/appointment/datatable.js
@@ -30,7 +30,7 @@ var table_columns = [
             if (data.appointment_status_id == "Arrived" || data.appointment_status_id == "Cancelled") {
                 return '<span>'+data.scheduled_date+'</span>';
             } else {
-                return '<a href="javascript:void(0);" onclick="editSchedule(' + data.id + ','+ data.doctorId+','+ data.locationId+');"><br> ' + data.scheduled_date + ' <i style="color: #cc8600; font-size: large" class="la la-pencil"></i></a>';
+                return '<a href="javascript:void(0);" onclick="editSchedule(' + data.id + ');"><br> ' + data.scheduled_date + ' <i style="color: #cc8600; font-size: large" class="la la-pencil"></i></a>';
             }
         }
     },{
@@ -217,7 +217,7 @@ function setStatusData(response, id) {
     }
 }
 
-function editSchedule(id,doc_id,loc_id) {
+function editSchedule(id) {
 
     $("#modal_change_appointment_schedule").modal("show");
     $("#schedule_appointment_id").val(id)
