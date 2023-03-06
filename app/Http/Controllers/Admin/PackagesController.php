bd83afc541c5037667a5dd8b7d5771e42fc8b1ae
gulraizazam <gulraiz@redsignal.biz>
2023-03-06 19:38:52 +0500

    resolved cue-367

--- app/Http/Controllers/Admin/PackagesController.php
+++ app/Http/Controllers/Admin/PackagesController.php
@@ -397,6 +397,7 @@ class PackagesController extends Controller
      */
     public function getdiscountinfocustom(Request $request)
     {
+        
         $status = true;
         $service_id = $request->service_id;
         $service_data = Bundles::find($service_id);
@@ -409,39 +410,54 @@ class PackagesController extends Controller
         }else{
             $request->discount_value = $discount_data->amount;
         }
-
-        if ($request->discount_type == Config::get('constants.Fixed')) {
-
-            $discount_type = Config::get('constants.Fixed');
-
-            $discount_price = $request->discount_value;
-
-            $discount_price_in_percentage = ($discount_price / $service_data->price) * 100;
-
-            if ($discount_data->amount >= $discount_price_in_percentage) {
-
+        
+        if($discount_data->type=='Fixed'){
+            if ($request->discount_type == Config::get('constants.Fixed')) {
+                if($request->discount_value > $discount_data->amount){
+                    return false;
+                }
+                $discount_type = Config::get('constants.Fixed');
+    
+                $discount_price = $request->discount_value;
+    
+                $discount_price_in_percentage = ($discount_price / $service_data->price) * 100;
                 $net_amount = ($service_data->price) - ($discount_price);
-
+                
+    
             } else {
-                $status = false;
+    
+                $discount_type = Config::get('constants.Percentage');
+    
+                $discount_price = $request->discount_value;
+    
+                if ($discount_data->amount >= $discount_price) {
+                    $discount_price_cal =  $discount_data->amount * (($discount_price) / 100);
+                    $net_amount = ($service_data->price) - ($discount_price_cal);
+    
+                } else {
+                    $status = false;
+                }
+                
             }
-
-        } else {
-
-            $discount_type = Config::get('constants.Percentage');
-
-            $discount_price = $request->discount_value;
-
-            if ($discount_data->amount >= $discount_price) {
-
-                $discount_price_cal = $service_data->price * (($discount_price) / 100);
-
-                $net_amount = ($service_data->price) - ($discount_price_cal);
-
+        }else{
+            if ($request->discount_type == Config::get('constants.Fixed')) {
+                $discount_price = $request->discount_value;
+                $discount_price_in_percentage = ($discount_data->amount/100)*$service_data->price;
+                if($request->discount_value > $discount_price_in_percentage){
+                    return false;
+                }
+                $net_amount = ($service_data->price) - ($request->discount_value);
             } else {
-                $status = false;
+                if($request->discount_value > $discount_data->amount){
+                    return false;
+                }
+                $discount_price = $request->discount_value;
+                $discount_price_in_percentage = ($discount_data->amount/100)*$service_data->price;
+                
+                $net_amount = ($service_data->price) - ($discount_price_in_percentage);
             }
         }
+        
         if ($status == true) {
 
             return ApiHelper::apiResponse($this->success, 'Net Amount', true, [
