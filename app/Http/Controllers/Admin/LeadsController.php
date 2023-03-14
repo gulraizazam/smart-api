7b748597ee2060f81b26d7212a1385e267e00acd
gulraizazam <gulraiz@redsignal.biz>
2023-03-14 18:06:19 +0500

    added centre

--- app/Http/Controllers/Admin/LeadsController.php
+++ app/Http/Controllers/Admin/LeadsController.php
@@ -2749,7 +2749,10 @@ class LeadsController extends Controller
     }
     public function leadupdate()
     {
-        $apts = Appointments::select('location_id','lead_id')->where('appointment_type_id',1)->where('lead_id','!=',Null)->get();
-        dd($apts);
+        $apts = Appointments::select('location_id','lead_id')->where('appointment_type_id',1)->where('lead_id','!=',Null)
+        ->orderBy('lead_id')->latest()->get();
+       foreach($apts as $apt){
+        Leads::where('lead_status_id',4)->where('id',$apt->lead_id)->update(['location_id',$apt->location_id]);
+       }
     }
 }
