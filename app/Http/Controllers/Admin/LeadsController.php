cd0eb6ea1132489728d48acef3e44d95ff05285d
gulraizazam <gulraiz@redsignal.biz>
2023-03-15 17:08:29 +0500

    added filters

--- app/Http/Controllers/Admin/LeadsController.php
+++ app/Http/Controllers/Admin/LeadsController.php
@@ -1900,6 +1900,7 @@ class LeadsController extends Controller
                                         $find_parent = Services::whereId($child_service_id)->first();
                                         $find_parent2 = Services::whereId($service_id)->first();
                                         if($find_parent2->id != $find_parent->parent_id ){
+                                            dd($find_parent);
                                             $service_id= $find_parent->parent_id;
                                         }
                                     }
