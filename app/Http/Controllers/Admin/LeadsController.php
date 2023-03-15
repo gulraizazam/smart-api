3ceeeb22ff81cb8f8a51b337be1f587883a3b90a
gulraizazam <gulraiz@redsignal.biz>
2023-03-15 15:45:55 +0500

    added filters

--- app/Http/Controllers/Admin/LeadsController.php
+++ app/Http/Controllers/Admin/LeadsController.php
@@ -2715,7 +2715,6 @@ class LeadsController extends Controller
         $leads = $resultQuery->select('*', 'leads.created_by as lead_created_by', 'leads.id as lead_id', 'leads.created_at as lead_created_at', 'users.id as PatientId')
         ->get();
         $customPaper = array(0,0,720,1440);
-        dd($leads);
         $pdf = PDF::loadView('admin.leads.lead-pdf', compact('leads'))->setPaper($customPaper, 'portrait');;
         return $pdf->download('leads.pdf');
     }
