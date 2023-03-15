bb8dce101f6bd33c546ae93eef7dff1b40bd8591
gulraizazam <gulraiz@redsignal.biz>
2023-03-15 16:13:07 +0500

    added filters

--- resources/views/admin/leads/view.blade.php
+++ resources/views/admin/leads/view.blade.php
@@ -47,8 +47,6 @@
                 <td id="lead_status"></td>
                 <th>Service</th>
                 <td id="treatment"></td>
-                <th>Child Service</th>
-                <td id="child_service"></td>
                 <th>SMS Status</th>
                 <td id="sms_status"></td>
             </tr>
