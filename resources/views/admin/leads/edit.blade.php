3604b9eaf1ee4d182d6c4fd358cf421693ebfb53
gulraizazam <gulraiz@redsignal.biz>
2023-03-15 13:31:27 +0500

    added filters

--- resources/views/admin/leads/edit.blade.php
+++ resources/views/admin/leads/edit.blade.php
@@ -99,7 +99,7 @@
                             </select>
                         </div>
                         <div class="fv-row col-md-6 mt-5">
-                            <label class="required fw-bold fs-6 mb-2 pl-0">Centre <span class="text text-danger">*</span></label>
+                            <label class="required fw-bold fs-6 mb-2 pl-0">Centre </label>
                             <select id="edit_location_id" class="form-control form-control-solid mb-3 mb-lg-0 select2" name="location_id">
                             </select>
                         </div>
