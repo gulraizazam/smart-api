fe76b0e78fe6f0d713fedc5cbaff11dec3a1536e
gulraizazam <gulraiz@redsignal.biz>
2023-03-16 19:22:52 +0500

    resolved leads

--- app/Http/Controllers/Admin/LeadsController.php
+++ app/Http/Controllers/Admin/LeadsController.php
@@ -2744,15 +2744,14 @@ class LeadsController extends Controller
        ini_set('memory_limit', '-1');
        try{
         $leads = Leads::select('patient_id','id')->where('lead_status_id',4)->get();
-        $tt = Leads::join('appointments','leads.id','appointments.lead_id')->where('appointment_type_id',1)->get();
-        dd($tt);
-        foreach($leads as $lead){
-         $apts = Appointments::select('location_id','lead_id')->where('appointment_type_id',1)->where('lead_id',$lead->id)
-            ->where('patient_id', $lead->patient_id)
-            ->latest()->first();
-            if($apts){
-               Leads::where('lead_status_id',4)->where('patient_id',$apts->patient_id)->update(['location_id'=>$apts->location_id]);
-           }
+        $tt = Leads::join('appointments','leads.id','appointments.lead_id')->where('leads.lead_status_id',4)->where('appointment_type_id',1)
+        ->distinct('appointments.lead_id')
+        ->get();
+dd($tt);
+        foreach($tt as $lead){
+         
+               Leads::where('lead_status_id',4)->where('patient_id',$lead->patient_id)->update(['location_id'=>$lead->location_id]);
+           
            
         }
        }catch(Exception $e){
