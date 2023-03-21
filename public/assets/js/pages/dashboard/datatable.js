8be1a8f2c6cefb531c2d24ed41471f6aa267907c
muhammadatifch <matif@redsignal.biz>
2023-03-21 15:34:01 +0500

    error resolve cue-341

--- public/assets/js/pages/dashboard/datatable.js
+++ public/assets/js/pages/dashboard/datatable.js
@@ -239,8 +239,8 @@ function changeDate() {
             var collection = response.data.todaycollection;
             var sales = response.data.revenue.toFixed();
             let locationId = "<?php implode(',', " + response.data.location_id + "); ?>";
-            let urlconsultant = route('admin.consultancy.index') + "?type=1&from=" + response.data.start_date + "&to=" + response.data.end_date + "&center_id=";
-            let urltreatment = route('admin.treatment.index') + "?type=2&from=" + response.data.start_date + "&to=" + response.data.end_date + "&center_id=";
+            let urlconsultant = route('admin.consultancy.index') + "?type=1&from=" + response.data.start_date + "&to=" + response.data.end_date;
+            let urltreatment = route('admin.treatment.index') + "?type=2&from=" + response.data.start_date + "&to=" + response.data.end_date;
             $("#allrevenue").text('PKR: ' + sales);
             $("#allconsult").text(response.data.done_consultancies+'/'+response.data.all_consultancies);
             $("#allconsultantdate").attr("href", urlconsultant);
