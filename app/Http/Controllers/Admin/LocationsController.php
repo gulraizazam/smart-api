530bb3a1aa3cc3bd14f17f6144d0b19141f78c8a
muhammadatifch <matif@redsignal.biz>
2023-03-22 16:49:34 +0500

    error resolved cue-361

--- app/Http/Controllers/Admin/LocationsController.php
+++ app/Http/Controllers/Admin/LocationsController.php
@@ -216,10 +216,7 @@ class LocationsController extends Controller
             ['active', '=', '1'],
             ['is_featured', '=', '1']
         ])->get()->pluck('full_name', 'id');
-
-
         $ServiceLocations = array();
-
         return ApiHelper::apiResponse($this->success, 'Record found', true, [
             'services' => $Services,
             'service_location' => $ServiceLocations,
@@ -245,16 +242,11 @@ class LocationsController extends Controller
             return ApiHelper::apiResponse($this->success, $validator->messages()->first(), false);
         }
         if ($location = Locations::createRecord($request, Auth::User()->account_id)) {
-
             $locatUser = array();
-
             $location_slug_all = Locations::where('slug', '=', 'all')->first();
-
             $user_has_location_data = UserHasLocations::where('location_id', '=', $location_slug_all->id ?? 0)->groupby('user_id')->get();
-
             if (count($user_has_location_data) > 0) {
                 foreach ($user_has_location_data as $user) {
-
                     $user_has_locations = array(
                         'user_id' => $user->user_id,
                         'region_id' => $location->region_id,
@@ -313,9 +305,7 @@ class LocationsController extends Controller
                     ServiceHasLocations::createRecord($servicesData, $location);
                 }
             }
-
             return ApiHelper::apiResponse($this->success, 'Record has been created successfully.');
-
         } else {
             return ApiHelper::apiResponse($this->success, 'Something went wrong, please try again later.', false);
         }
@@ -356,15 +346,11 @@ class LocationsController extends Controller
         if (!Gate::allows('locations_edit')) {
             return ApiHelper::apiResponse($this->unauthorized, 'You are not authorized to access this resource.', false);
         }
-
         $location = Locations::getData($id);
-
         if (!$location) {
             return view('error', compact('lead_statuse'));
         }
-
         $ServiceLocations = $location->service_has_locations()->pluck('service_id')->toArray();
-
         $cities = Cities::where([
             ['account_id', '=', Auth::User()->account_id],
             ['slug', '=', 'custom'],
