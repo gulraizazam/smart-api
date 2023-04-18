6166a5fae04601248073bb40503e737d47cc8be1
gulraizazam <gulraiz@redsignal.biz>
2023-04-18 12:27:12 +0500

    resolved cue-398

--- app/Http/Controllers/ConversionReportController.php
+++ app/Http/Controllers/ConversionReportController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ACL;
+use App\Models\Appointments;
 use App\Models\Cities;
+use App\Models\Invoices;
 use App\Models\Locations;
+use App\Models\Packages;
+use App\Models\PackageService;
 use App\Models\Services;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -20,6 +25,20 @@ class ConversionReportController extends Controller
     }
     public function LoadConversionReport(Request $request)
     {
-        dd($request->all());
+        $packagebundleIds = self::where([
+            'package_id' => $package->id,
+            'is_allocate' => '1'
+        ])->pluck('id');
+        $GetPackage = Packages::findOrFail($packagebundle[0]->package_id);
+        $GetAppointment = Appointments::findOrFail($GetPackage->appointment_id);
+        $GetInvoiceInfo = Invoices::where(['appointment_id' => $GetAppointment->id])->first();
+        $package_services = PackageService::with('service')->whereIn('package_bundle_id',$packagebundleIds)
+        ->where('created_at','>',Carbon::parse($GetInvoiceInfo->created_at))
+        ->get();
+        foreach ($package_services as $packagebundl) {
+            if($packagebundl->service->parent_id != $GetAppointment->service_id){
+                $GetAppointment->update(['service_id'=> $package_services[0]->service->parent_id]);
+            }
+        }
     }
 }
