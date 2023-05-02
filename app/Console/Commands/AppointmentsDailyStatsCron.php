ecc0d871c0bd05d725d29d725f3299399244348c
gulraizazam <gulraiz@redsignal.biz>
2023-05-02 10:33:31 +0500

    resolved cron issue

--- app/Console/Commands/AppointmentsDailyStatsCron.php
+++ app/Console/Commands/AppointmentsDailyStatsCron.php
@@ -43,15 +43,9 @@ class AppointmentsDailyStatsCron extends Command
     public function handle()
     {
         $consultancyslug = AppointmentTypes::where(['slug' => 'consultancy'])->first()->id;
-        $treatmentslug = AppointmentTypes::where(['slug' => 'treatment'])->first()->id;
-        $appointmentpending = AppointmentStatuses::where(['name' => 'Pending'])->first()->id;
-        $appointmentarrived = AppointmentStatuses::where(['name' => 'Arrived'])->first()->id;
-        $consultationscheduled = Appointments::where(['scheduled_date' => Carbon::now()->format("Y-m-d"), 'appointment_status_id' => $appointmentpending, 'appointment_type_id' => $consultancyslug])->count();
-        $consultationarrived = Appointments::where(['scheduled_date' => Carbon::now()->format("Y-m-d"), 'appointment_status_id' => $appointmentarrived, 'appointment_type_id' => $consultancyslug])->count();
-        // Treatment
-        $treatmentscheduled = Appointments::where(['scheduled_date' => Carbon::now()->format("Y-m-d"), 'appointment_status_id' => $appointmentpending, 'appointment_type_id' => $treatmentslug])->count();
-        $treatmentarrived = Appointments::where(['scheduled_date' => Carbon::now()->format("Y-m-d"), 'appointment_status_id' => $appointmentarrived, 'appointment_type_id' => $treatmentslug])->count();
-        $appointmentdailystats = AppointmentsDailyStats::create(['consultation_scheduled_count' => $consultationscheduled, 'consultation_arrived_count' => $consultationarrived, 'treatment_scheduled_count' => $treatmentscheduled, 'treatment_arrived_count' => $treatmentarrived, 'cron_current_date' => Carbon::now()]);
+        $consultationscheduled = Appointments::where(['scheduled_date' => Carbon::now()->format("Y-m-d"), 'base_appointment_status_id' => 1, 'appointment_type_id' => $consultancyslug])->count();
+        $consultationarrived = Appointments::where(['scheduled_date' => Carbon::now()->format("Y-m-d"), 'base_appointment_status_id' => 2, 'appointment_type_id' => $consultancyslug])->count();
+        $appointmentdailystats = AppointmentsDailyStats::create(['consultation_scheduled_count' => $consultationscheduled, 'consultation_arrived_count' => $consultationarrived, 'cron_current_date' => Carbon::now()]);
         return 0;
     }
 }
