24614ca3bd85c1f1a5c81f2d8434cb37437d04c8
gulraizazam <gulraiz@redsignal.biz>
2023-05-03 16:47:56 +0500

    resolved command issue

--- app/Models/AppointmentsDailyStats.php
+++ app/Models/AppointmentsDailyStats.php
@@ -9,4 +9,14 @@ class AppointmentsDailyStats extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function appointment(){
+        return $this->belongsTo(Appointments::class, 'appointment_id');
+    }
+    public function location(){
+        return $this->belongsTo(Locations::class, 'centre_id');
+    }
 }
