d6957010a0595d1b21abddb5c0dc69eb2a202efb
gulraizazam <gulraiz@redsignal.biz>
2023-05-03 16:47:56 +0500

    resolved command issue

--- app/Http/Controllers/Admin/Reports/FinanceReportController.php
+++ app/Http/Controllers/Admin/Reports/FinanceReportController.php
@@ -30,6 +30,7 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use App\Helpers\Explode_Multi_select;
 use App\Helpers\GeneralFunctions;
 use App\Models\Appointments;
+use App\Models\AppointmentsDailyStats;
 use Illuminate\Support\Facades\DB;
 
 class FinanceReportController extends Controller
@@ -2825,5 +2826,47 @@ class FinanceReportController extends Controller
         ->get();
         $arrived = $resultQuery->where(['base_appointment_status_id' => 2])->count();
         return view('admin.reports.daily_arrived',get_defined_vars());
+    }
+    public function staffWiseArrival()
+    {
+        $locations = Locations::getActiveRecordsByCity('',ACL::getUserCentres(), Auth::User()->account_id);
+        $Users = User::getAllRecords(Auth::User()->account_id)->getDictionary();
+        return view('admin.reports.staffwisearrival',get_defined_vars());
+    }
+    public function staffWiseArrivalReport(Request $request)
+    {
+        $where = array();
+        if ($request->location_id  && $request->location_id ) {
+            $where[] = array(['centre_id' => $request->location_id]);
+        }
+        if ($request->created_by && $request->created_by != '') {
+            $where[] = array(['user_id' => $request->created_by]);
+        }
+        if ($request->date_from) {
+            $where[] = array('cron_current_date', '>=', $request->date_from);
+        }
+        if ($request->date_to) {
+            $where[] = array('cron_current_date', '<=', $request->date_to);
+        }
+        $records = array();
+        $records["data"] = array();
+        if (Gate::allows('appointments_consultancy')) {
+            $resultQuery = AppointmentsDailyStats::whereIn('centre_id', ACL::getUserCentres());
+        }
+        if (Gate::allows('appointments_consultancy') && Gate::allows('appointments_services')) {
+            $resultQuery = AppointmentsDailyStats::whereIn('centre_id', ACL::getUserCentres());
+        }
+        if (count($where)) {
+            $resultQuery->where($where);
+        }
+        $Appointments = $resultQuery->with(['user', 'appointment' => function($q){
+            $q->select('*', 'appointments.name as patient_name', 'appointments.id as app_id', 'appointments.created_by as app_created_by', 'appointments.updated_by as app_updated_by', 'appointments.created_at as app_created_at')
+            ->orderBy("appointments.created_at", "DESC");
+        }])
+        ->get();
+        $arrived = $resultQuery->where(['appointment_status_id' => 2])->count();
+        $user = User::where(['id' => $request->created_by])->first()->name ?? '';
+        $centre = Locations::where(['id' => $request->location_id])->first()->name ?? '';
+        return view('admin.reports.staff_wise_arrived',get_defined_vars());
     }
 }
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -995,6 +995,19 @@
  
                                      </ul>
                                  </div>
+                                 <div class="menu-submenu">
+                                    <i class="menu-arrow"></i>
+                                    <ul class="menu-subnav">
+                                        <li class="menu-item {{activeMenu('admin.reports.staff_wise_arrival')}}" aria-haspopup="true">
+                                            <a href="{{route('admin.reports.staff_wise_arrival')}}" class="menu-link">
+                                                <i class="menu-bullet menu-bullet-dot">
+                                                    <span></span>
+                                                </i>
+                                                <span class="menu-text">Staff Wise Arrival Report </span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
                             @endcan
 
                         </li>
