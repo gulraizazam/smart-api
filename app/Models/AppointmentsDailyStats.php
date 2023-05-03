67e9bdab748bdf624049b6cc21edb4c00355e5c7
Muhammad Atif <matif@redsignal.biz>
2023-05-03 13:33:30 +0500

    staff wise daily report

--- app/Models/AppointmentsDailyStats.php
+++ app/Models/AppointmentsDailyStats.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Appointments;
 
 class AppointmentsDailyStats extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function appointment(){
+        return $this->belongsTo(Appointments::class, 'appointment_id');
+    }
+
+    public function location(){
+        return $this->belongsTo(Locations::class, 'centre_id');
+    }
 }
