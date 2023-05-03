8fb7da11ca5dc88317621bf4584b3cfa14b256ba
gulraizazam <gulraiz@redsignal.biz>
2023-05-03 13:13:56 +0500

    check data

--- app/Reports/Finanaces.php
+++ app/Reports/Finanaces.php
@@ -2669,11 +2669,11 @@ class Finanaces
             ->whereDate('package_advances.created_at', '>=', $start_date)
             ->whereDate('package_advances.created_at', '<=', $end_date)
             ->where('package_advances.cash_amount','>',0)
-             //->where($where)
+             ->where($where)
             ->select('appointments.*')
             ->orderBy('appointments.created_at', 'desc')
             ->get();
-            dd(FacadesDB::getQueryLog());
+           // dd(FacadesDB::getQueryLog());
         $total = 0;
         $count = array();
         $arrived_count = array();
