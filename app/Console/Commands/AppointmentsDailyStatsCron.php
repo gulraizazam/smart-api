d67515c37dbc9c855acaad441ca50fcb54c58253
gulraizazam <gulraiz@redsignal.biz>
2023-05-05 11:50:48 +0500

    resolved command issue

--- app/Console/Commands/AppointmentsDailyStatsCron.php
+++ app/Console/Commands/AppointmentsDailyStatsCron.php
@@ -49,7 +49,12 @@ class AppointmentsDailyStatsCron extends Command
             $appointments = Appointments::where(['location_id' => $location, 'scheduled_date' => Carbon::now()->format("Y-m-d"), 'appointment_type_id' => $consultancyslug])->select('id', 'location_id', 'base_appointment_status_id', 'created_by')->get();
             if(!$appointments->isEmpty()){
                 foreach ($appointments as $appointment){
-                    AppointmentsDailyStats::create(['centre_id' => $appointment->location_id, 'user_id' => $appointment->created_by, 'appointment_id' => $appointment->id, 'appointment_status_id' => $appointment->base_appointment_status_id, 'cron_current_date' => Carbon::now()]);
+                    AppointmentsDailyStats::updateOrCreate(['appointment_id' => $appointment->id, 'created_at' => Carbon::now()->format("Y-m-d")],
+                    ['centre_id' => $appointment->location_id,
+                    'user_id' => $appointment->created_by,
+                    'appointment_id' => $appointment->id,
+                    'appointment_status_id' => $appointment->base_appointment_status_id,
+                    'cron_current_date' => Carbon::now()]);
                 }
             }
         }
