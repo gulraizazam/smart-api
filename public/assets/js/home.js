22f4911ecea1b473c19b268ca84198abc848c4fe
gulraizazam <gulraiz@redsignal.biz>
2023-05-18 18:57:18 +0500

    changed chart

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -586,15 +586,19 @@ function initCentreWiseArrival(period){
                 var barLenght = response.data.bar;
                 for(var i = 0; i < barLenght.length; i++){
                     var walkin = "";
+                    var arrived = "";
+                    var total = "";
                     if(response.data.walkin[i] === undefined) {
                         walkin = "0";
+                        arrived = response.data.arrived[i]-walkin;
                     } else {
                         walkin = response.data.walkin[i];
+                        arrived = response.data.arrived[i]-walkin;
                     }
                     let str = barLenght[i];
                     let wordToRemove = "CUTERA ";
                     let centre_name = str.replace(new RegExp('\\b' + wordToRemove + '\\b', 'gi'), '');
-                    TABLE_HTML += "<tr><td style='color: #2b7bc1;font-weight: bold;'>"+centre_name+"</td><td>"+response.data.arrived[i]+"/"+response.data.total[i]+"</td><td>"+walkin+"</td><td>"+((response.data.arrived[i] / response.data.total[i]) * 100).toFixed(2)+"%</td></tr>";  
+                    TABLE_HTML += "<tr><td style='color: #2b7bc1;font-weight: bold;'>"+centre_name+"</td><td>"+arrived+"/"+response.data.total[i]+"</td><td>"+walkin+"</td><td>"+((response.data.arrived[i] / response.data.total[i]) * 100).toFixed(2)+"%</td></tr>";  
                 }
                 jQuery('#table-body').append(TABLE_HTML);
             }, 2000); 
