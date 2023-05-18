1378674a4958da0c23c489005c607e693b9fab34
gulraizazam <gulraiz@redsignal.biz>
2023-05-18 18:54:44 +0500

    changed chart

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -594,7 +594,7 @@ function initCentreWiseArrival(period){
                     let str = barLenght[i];
                     let wordToRemove = "CUTERA ";
                     let centre_name = str.replace(new RegExp('\\b' + wordToRemove + '\\b', 'gi'), '');
-                    TABLE_HTML += "<tr><td style='color: #2b7bc1;font-weight: bold;'>"+centre_name+"</td><td>"+(response.data.arrived[i])-(walkin)+"/"+(response.data.total[i])-(walkin)+"</td><td>"+walkin+"</td><td>"+((response.data.arrived[i]-walkin / response.data.total[i]-walkin) * 100).toFixed(2)+"%</td></tr>";  
+                    TABLE_HTML += "<tr><td style='color: #2b7bc1;font-weight: bold;'>"+centre_name+"</td><td>"+response.data.arrived[i]+"/"+response.data.total[i]+"</td><td>"+walkin+"</td><td>"+((response.data.arrived[i] / response.data.total[i]) * 100).toFixed(2)+"%</td></tr>";  
                 }
                 jQuery('#table-body').append(TABLE_HTML);
             }, 2000); 
