7d928ee8ec5c1ef59abec49779b258d79e27fb88
gulraizazam <gulraiz@redsignal.biz>
2023-05-18 13:57:11 +0500

    design changes

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -980,7 +980,7 @@
                         let str = barLenght[i];
                         let wordToRemove = "CUTERA ";
                         let centre_name = str.replace(new RegExp('\\b' + wordToRemove + '\\b', 'gi'), '');
-                        TABLE_HTML += "<tr><td>"+centre_name+"</td><td>"+response.data.arrived[i]/response.data.total[i]+"</td><td>"+walkin+"</td><td>"+((response.data.arrived[i] / response.data.total[i]) * 100).toFixed(2)+"%</td></tr>";  
+                        TABLE_HTML += "<tr><td>"+centre_name+"</td><td>"+response.data.arrived[i]+"/"+response.data.total[i]+"</td><td>"+walkin+"</td><td>"+((response.data.arrived[i] / response.data.total[i]) * 100).toFixed(2)+"%</td></tr>";  
                     }
                     jQuery('#table-body').append(TABLE_HTML);
                     BarChartCentre(response);
