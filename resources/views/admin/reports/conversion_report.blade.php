67813ce38616be88606bb86c6e58bf8e35b1222f
Muhammad Atif <matif@redsignal.biz>
2023-05-24 16:09:52 +0500

    error resolved

--- resources/views/admin/reports/conversion_report.blade.php
+++ resources/views/admin/reports/conversion_report.blade.php
@@ -114,6 +114,14 @@
                                 <td class="bg-light">Arrival to Conversion Ratio</td>
                                 <td class="bg-light" style="text-align:right;">{{ number_format($arrival_to_conversion_ratio,2) }} %</td>
                             </tr>
+                            <tr class="">
+                                <td class="bg-light">Total Conversion</td>
+                                <td class="bg-light" style="text-align:right;">{{ $total_conversion }}</td>
+                            </tr>
+                            <tr class="">
+                                <td class="bg-light">Total Arrival</td>
+                                <td class="bg-light" style="text-align:right;">{{ $total_arrival }}</td>
+                            </tr>
 
                         </thead>
                     </table>
@@ -127,7 +135,7 @@
                         <thead>
                             <tr class="">
                                 <td class="bg-light">{{$conversion['service']}}</td>
-                                <td class="bg-light" style="text-align:right;"></td>
+                                <td class="bg-light" style="text-align:right;">Conversion: {{$conversion['total_conversion']}}/Total: {{$conversion['total_arrival']}}</td>
                             </tr>
                             <tr class="">
                                 <td class="bg-light" style="text-align:right;">Total Conversion Value</td>
