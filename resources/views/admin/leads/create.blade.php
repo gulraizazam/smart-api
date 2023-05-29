a3464af021afa36b616da6a5e47fff25e690cd86
Muhammad Atif <matif@redsignal.biz>
2023-05-29 13:48:18 +0500

    error resolved

--- resources/views/admin/leads/create.blade.php
+++ resources/views/admin/leads/create.blade.php
@@ -64,7 +64,7 @@
                             </select>
                         </div>
                         <div class="fv-row col-md-6 mt-5">
-                            <label class="required fw-bold fs-6 mb-2 pl-0">Child Service  </label>
+                            <label class="required fw-bold fs-6 mb-2 pl-0">Child Service  <span class="text text-danger">*</span></label>
                             <select id="add_child_service_id" class="form-control form-control-solid mb-3 mb-lg-0 select2" name="child_service_id">
                             </select>
                         </div>
