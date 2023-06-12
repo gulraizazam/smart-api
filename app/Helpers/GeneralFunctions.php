3075a20d1fed5984ae20f5ee053196cd49089aa3
Muhammad Atif <matif@redsignal.biz>
2023-06-12 11:50:37 +0500

    error resolved

--- app/Helpers/GeneralFunctions.php
+++ app/Helpers/GeneralFunctions.php
@@ -153,6 +153,63 @@ class GeneralFunctions
         return $appointment->appointment_type_id ?? 0;
     }
 
+    public static function servicesList($request = null, $total = 0)
+    {
+        $where = [];
+        if ($total >= 0) {
+            $filename = 'services';
+            if(isset($request)){
+                $filters = getFilters($request->all());
+                //dd($filters, hasFilter($filters, 'status'), hasFilter($filters, 'name'), $filters['status'], $filters['status'] == 1);
+                $apply_filter = checkFilters($filters, $filename);
+                if (hasFilter($filters, 'name')) {
+                    $where[] = ['name','like','%' . $filters['name'] . '%'];
+                    Filters::put(Auth::user()->id, $filename, 'name', $filters['name']);
+                } else {
+                    if ($apply_filter) {
+                        Filters::forget(Auth::User()->id, $filename, 'name');
+                    } else {
+                        if (Filters::get(Auth::User()->id, $filename, 'name')) {
+                            $where[] = [ 'name','like','%' . Filters::get(Auth::user()->id, $filename, 'name') . '%'];
+                        }
+                    }
+                }
+                if (hasFilter($filters, 'status')) {
+                    $where[] = ['active', '=', $filters['status']];
+                    Filters::put(Auth::user()->id, $filename, 'status', $filters['status']);
+                } else {
+                    if ($apply_filter) {
+                        Filters::forget(Auth::user()->id, $filename, 'status');
+                    } else {
+                        if (Filters::get(Auth::user()->id, $filename, 'status') != null) {
+                            $where[] = ['active', '=', $filters['status']];
+                        }
+                    }
+                }
+            }
+            $query = Services::with('children')
+                ->where('slug', '!=', 'all')
+                ->when(isset($where) && count($where) > 0, fn($q) => $q->where($where));
+                /* if(count($where)){
+                    $query->where($where);
+                } */
+                $services = $query->get();
+                $mergedServices = [];
+                foreach ($services as $service) {
+                    if($service->parent_id == "0"){
+                        $children = Services::where(['parent_id' => $service->id])->where($where)->orderBy('name')->get()->toArray();
+                        $mergedServices[] = $service->toArray();
+                        foreach ($children as $child) {
+                            $mergedServices[] = $child;
+                        }
+                    } else {
+                        $mergedServices[] = $service->toArray();
+                    }
+                }
+            return $mergedServices;
+        }
+    }
+
     public static function ServicesTree($request = null, $total = 0)
     {
         $where = [];
