eb7d3066cb06b747043dcdbc940c99519bb46848
gulraizazam <gulraiz@redsignal.biz>
2023-06-14 18:05:58 +0500

    resolved

--- app/Reports/Finanaces.php
+++ app/Reports/Finanaces.php
@@ -2909,6 +2909,7 @@ class Finanaces
         ->map(function ($appointments_info) {
             return $appointments_info->sum('conversion_spend');
         });
+        dd($conversionsByPatient);
         $avg_C_val = 0;
         if(count($conversionsByPatient) > 0){
             $avg_cxlient_value = $avg_C_val/count($conversionsByPatient);
