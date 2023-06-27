1408a6f58497b954a15f2b6a6565968371f4a792
gulraizazam <gulraiz@redsignal.biz>
2023-06-27 10:47:14 +0500

    merged with live

--- app/Http/Controllers/DashboardReportsController.php
+++ app/Http/Controllers/DashboardReportsController.php
@@ -2533,7 +2533,7 @@ class DashboardReportsController extends Controller
 
     public function DoctoreWiseConversion(Request $request)
     {
-        dd($request->all());
+        
         $where = [];
         $total_apts = [];
         $converted_apts = [];
