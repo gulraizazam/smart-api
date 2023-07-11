1b140472d775377bbd5d1a704af1e2ecc2a35d2b
gulraizazam <gulraiz@redsignal.biz>
2023-07-11 17:19:42 +0500

    reoslved conflicts:

--- app/Http/Controllers/PatientFollowupController.php
+++ app/Http/Controllers/PatientFollowupController.php
@@ -86,7 +86,7 @@ class PatientFollowupController extends Controller
             ->whereIn('package_advances.location_id', $center_id)
             ->groupBy('package_advances.patient_id')
             ->orderBy('package_advances.patient_id', 'DESC')
-            ->limit(50)
+            ->limit(20)
             ->get();
         $plans_check_array = json_decode(json_encode($plans_check), true);
         $not_treatment = [];
