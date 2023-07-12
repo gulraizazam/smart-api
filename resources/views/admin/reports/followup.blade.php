d1a4df9d0e80d7345e9efc30d6b98e3f6b37865f
gulraizazam <gulraiz@redsignal.biz>
2023-07-12 11:58:03 +0500

    reoslved pat searcjh iossue

--- resources/views/admin/reports/followup.blade.php
+++ resources/views/admin/reports/followup.blade.php
@@ -104,5 +104,12 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
         <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
         <script src="{{asset('assets/js/dailyarrival.js')}}"></script>
+        <script>
+            function SetPatient()
+            {
+                $("#filter_patient_id").val($("#appoint_search_patient").val());
+
+            }
+        </script>
     @endpush
 @endsection
