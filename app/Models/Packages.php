88bb20c6d4e9bcdcd5336253996674621d77ca51
gulraizazam <gulraiz@redsignal.biz>
2023-08-07 15:20:50 +0500

    resolved

--- app/Models/Packages.php
+++ app/Models/Packages.php
@@ -299,7 +299,7 @@ class Packages extends BaseModal
 
         [$orderBy, $order] = getSortBy($request, 'id', 'DESC');
         if (\Illuminate\Support\Facades\Gate::allows('view_inactive_plans')) {
-            return self::when(count($where), fn ($query) => $query->where($where))->whereIn('location_id', ACL::getUserCentres())
+            return self::when(count($where), fn ($query) => $query->where($where))->where(['is_refund'=>1])->whereIn('location_id', ACL::getUserCentres())
                 ->limit($iDisplayLength)
                 ->offset($iDisplayStart)
                 ->orderby($orderBy, $order)
