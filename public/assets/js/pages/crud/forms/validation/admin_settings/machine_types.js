2ea75f2bbb38df198ebfb2547f4adf2e07a0867c
Muhammad Atif <matif@redsignal.biz>
2023-08-08 12:04:22 +0500

    error resolved

--- public/assets/js/pages/crud/forms/validation/admin_settings/machine_types.js
+++ public/assets/js/pages/crud/forms/validation/admin_settings/machine_types.js
@@ -100,7 +100,7 @@ var EditUserValidation = function () {
                 if (response.status) {
                     toastr.success(response.message);
                     closePopup(modal_id);
-                    reInitTable();
+                    reInitTable('machineTypes');
                 } else {
                     toastr.error(response.message);
                 }
