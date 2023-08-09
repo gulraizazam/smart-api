fdac6ce58be3afaba291a241451ff55e850f973f
gulraizazam <gulraiz@redsignal.biz>
2023-08-09 13:04:48 +0500

    resolved

--- app/Http/Controllers/Admin/RefundsController.php
+++ app/Http/Controllers/Admin/RefundsController.php
@@ -122,7 +122,7 @@ class RefundsController extends Controller
                             'total' => number_format($package->total_price),
                             'cash_receive' => number_format($cash_receive),
                             'refunded' =>$refunded_amount,
-                            'case_setteled' => $is_case_setteled->is_setteled == 1 ? 'Yes' : 'No',
+                            'case_setteled' => isset($is_case_setteled) && $is_case_setteled->is_setteled == 1 ? 'Yes' : 'No',
                             'created_at' => $refunded_latest_date ? Carbon::parse($refunded_latest_date->created_at)->format('F j,Y h:i A') : Carbon::parse($package->created_at)->format('F j,Y h:i A'),
                         ];
                     } else {
