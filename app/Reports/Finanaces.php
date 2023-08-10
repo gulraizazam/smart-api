d981d80993c7217c92a85927f74150fe84d13f15
gulraizazam <gulraiz@redsignal.biz>
2023-08-10 13:07:31 +0500

    resolved

--- app/Reports/Finanaces.php
+++ app/Reports/Finanaces.php
@@ -2761,13 +2761,22 @@ class Finanaces
                 }
             }
         }
-
-        $total_appointments = Appointments::whereBetween('scheduled_date', [$start_date, $end_date])
+        if(isset($data['doctor_id'])){
+            $total_appointments = Appointments::whereBetween('scheduled_date', [$start_date, $end_date])
+            ->where(['appointment_type_id' => 1, 'base_appointment_status_id' => 2])
+            ->whereIn('appointments.doctor_id', $consultants)
+            ->whereIn('appointments.location_id', $locations)
+            ->where($where)
+            ->count();
+        }else{
+            $total_appointments = Appointments::whereBetween('scheduled_date', [$start_date, $end_date])
             ->where(['appointment_type_id' => 1, 'base_appointment_status_id' => 2])
             //->whereIn('appointments.doctor_id', $consultants)
             ->whereIn('appointments.location_id', $locations)
             ->where($where)
             ->count();
+        }
+        
         array_push($converted_apts, collect($appointments_info)->whereIn('appointment_id', $converted_appointments->pluck('id')->toArray())->where('conversion_spend', '!=', "")->count());
         array_push($total_apts, $total_appointments);
 
@@ -2802,23 +2811,39 @@ class Finanaces
                 $sum_conversion_total = $new_array[$arrive_category['name']]['total_conversion'];
                 $avg_valu = $new_array[$arrive_category['name']]['avg'];
                 $sum_valu = $new_array[$arrive_category['name']]['sum'];
-
-                $category_total_records = Appointments::where(['service_id' => $arrive_category['service_id'], 'base_appointment_status_id' => 2, 'appointment_type_id' => 1])
+                if(isset($data['doctor_id'])){
+                    $category_total_records = Appointments::where(['service_id' => $arrive_category['service_id'], 'base_appointment_status_id' => 2, 'appointment_type_id' => 1])
+                    ->whereIn('doctor_id', $consultants)
+                    ->whereIn('appointments.location_id', $locations)
+                    ->whereBetween('scheduled_date', [$start_date, $end_date])
+                    ->count();
+                }else{
+                    $category_total_records = Appointments::where(['service_id' => $arrive_category['service_id'], 'base_appointment_status_id' => 2, 'appointment_type_id' => 1])
                     //->whereIn('doctor_id', $consultants)
                     ->whereIn('appointments.location_id', $locations)
                     ->whereBetween('scheduled_date', [$start_date, $end_date])
                     ->count();
+                }
+               
             } else {
                 $name = [$arrive_category['name']][0];
                 $sum_conversion_total = 0;
                 $avg_valu = 0;
                 $sum_valu = 0;
 
-                $category_total_records = Appointments::where(['service_id' => $arrive_category['service_id'], 'base_appointment_status_id' => 2, 'appointment_type_id' => 1])
+                if(isset($data['doctor_id'])){
+                    $category_total_records = Appointments::where(['service_id' => $arrive_category['service_id'], 'base_appointment_status_id' => 2, 'appointment_type_id' => 1])
+                    ->whereIn('doctor_id', $consultants)
+                    ->whereIn('appointments.location_id', $locations)
+                    ->whereBetween('scheduled_date', [$start_date, $end_date])
+                    ->count();
+                }else{
+                    $category_total_records = Appointments::where(['service_id' => $arrive_category['service_id'], 'base_appointment_status_id' => 2, 'appointment_type_id' => 1])
                     //->whereIn('doctor_id', $consultants)
                     ->whereIn('appointments.location_id', $locations)
                     ->whereBetween('scheduled_date', [$start_date, $end_date])
                     ->count();
+                }
             }
 
             $returnCategoryData[$key] = [
