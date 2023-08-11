e175f3be0c959e29953054ffa84fed58f7ad636f
gulraizazam <gulraiz@redsignal.biz>
2023-08-11 18:52:32 +0500

    resolved setteled issue[A

--- public/assets/js/dailyarrival.js
+++ public/assets/js/dailyarrival.js
@@ -9,13 +9,13 @@ $('#date_range_arrival').daterangepicker({
         'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
         'Last 7 Days': [moment().subtract(6, 'days'), moment()],
         'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-        'This Month': [moment().startOf('month'), moment().subtract(1, 'days')],
+        'This Month': [moment().startOf('month'), moment().endOf('month')],
         'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
         'This Year': [moment().startOf('year'), moment().endOf('year')],
         'Last Year': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')],
     },
     startDate: moment().startOf('month'),
-    endDate: moment().subtract(1, 'days')
+    endDate:moment().endOf('month')
 }).val();
 var loadConvertedReport = function (that) {
     if (typeof that.prop("disabled") !== 'undefined' && that.prop("disabled") === true) {
