a809eeea3478b37f995476f80cc47e3136221523
gulraizazam <gulraiz@redsignal.biz>
2023-08-11 11:07:49 +0500

    resolved

--- app/Exports/ExportConsultancies.php
+++ app/Exports/ExportConsultancies.php
@@ -157,6 +157,7 @@ class ExportConsultancies implements FromCollection, WithHeadings, WithMapping, 
                 $phone,
             ];
         }
+        dd($where);
         $results = Appointments::join('users', 'users.id', '=', 'appointments.patient_id')
             ->where(['users.user_type_id' => config('constants.patient_id')])
             ->whereIn('appointments.city_id', ACL::getUserCities())
@@ -167,7 +168,7 @@ class ExportConsultancies implements FromCollection, WithHeadings, WithMapping, 
             })
             ->orderBy('scheduled_time','asc')
             ->get();
-            dd($results);
+           
         return $results;
     }
 
