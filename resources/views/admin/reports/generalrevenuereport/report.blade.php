ff73a8442c6c638bdb095cb03d1fb515a057fbd3
gulraizazam <gulraiz@redsignal.biz>
2023-08-11 18:25:34 +0500

    resolved setteled issue

--- resources/views/admin/reports/generalrevenuereport/report.blade.php
+++ resources/views/admin/reports/generalrevenuereport/report.blade.php
@@ -181,9 +181,9 @@
                                 $t_cash = $total_revenue_cash_location1 - $total_refund_cash_location1;
                                 $t_card = $total_revenue_card_location1 - $total_refund_card_location1;
                                 $t_bank = $total_revenue_bank_location1 - $total_refund_bank_location1;
-                                $total_revenue_cash_location +=$total_revenue_cash_location1;
-                                $total_revenue_card_location +=$total_revenue_card_location1;
-                                $total_revenue_bank_location +=$total_revenue_bank_location1;
+                                $total_revenue_cash_location +=$t_cash;
+                                $total_revenue_card_location +=$t_card ;
+                                $total_revenue_bank_location +=$t_bank;
                                 
                                 @endphp    
                                 <tr style="background:#364150;color: #fff;">
