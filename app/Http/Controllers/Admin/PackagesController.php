a70af9d31671e8c377676b5c89731125b18980e8
gulraizazam <gulraiz@redsignal.biz>
2023-08-11 12:35:05 +0500

    resolved setteled issue

--- app/Http/Controllers/Admin/PackagesController.php
+++ app/Http/Controllers/Admin/PackagesController.php
@@ -2193,6 +2193,9 @@ class PackagesController extends Controller
                $dataInvoiceDetail['service_id'] =$services->id;
                $dataInvoiceDetail['invoice_id'] = $create_invoice->id;
                InvoiceDetails::create($dataInvoiceDetail);
+        }else{
+            $latest_refund->where('id',$request['record_id'])->update(['is_setteled' => 1]);
+       
         }
     }
         $latest_refund->where('id',$request['record_id'])->update(['created_at' => $request['created_at'] , 'cash_amount' => $request['refund_amount'],'payment_mode_id' => $request['payment_mode_id']]);
