e9d0f02f1566d06ef72d48aec3932af796be2fe7
gulraizazam <gulraiz@redsignal.biz>
2023-08-17 17:36:58 +0500

    resolved

--- resources/views/admin/reports/accountsalesreport/index.blade.php
+++ resources/views/admin/reports/accountsalesreport/index.blade.php
@@ -327,13 +327,13 @@
                 }
                 var date_ranges;
                 if($("#date_range_fdm").val()!=""){
-                    console.log("here");
+                  
                     date_ranges = $("#date_range_fdm").val();
                 }else{
-                    console.log("mmhere");
+                   
                     date_ranges = $("#date_range").val();
                 }
-                console.log(date_ranges);
+                console.log($("#date_range_fdm").val());
                 showSpinner();
                 $.ajax({
                     headers: {
