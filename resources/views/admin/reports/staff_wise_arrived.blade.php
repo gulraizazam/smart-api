56561f9dc71559f34e283be122a771827940b903
Gulraiz_azam <gulraizazam@technerdslab.com>
2023-08-20 09:51:55 +0500

    resolved

--- resources/views/admin/reports/staff_wise_arrived.blade.php
+++ resources/views/admin/reports/staff_wise_arrived.blade.php
@@ -29,31 +29,31 @@
                     <table class="table border">
                         <thead>
                             <tr class="">
-                                <td class="bg-light">Total Scheduled Appointments</td>
-                                <td class="bg-light" style="text-align:right;">{{count($Appointments) > 0 ?count($Appointments) -$walkin_customers  : 0}}</td>
+                                <td class="bg-light">Total Scheduled Appointments (Walkin included)</td>
+                                <td class="bg-light" style="text-align:right;">{{count($Appointments) ?? 0}}</td>
                             </tr>
                             <tr class="">
-                                <td class="border-top bg-light"> Arrived</td>
-                                <td class="border-top bg-light" style="text-align:right;">{{$arrived > 0 ?$arrived - $walkin_customers  : 0}}</td>
+                                <td class="border-top bg-light"> Arrived (Walkin included)</td>
+                                <td class="border-top bg-light" style="text-align:right;">{{$arrived ?? 0}}</td>
                             </tr>
-                            <!-- @if(isset($walkin_customers))
+                            @if(isset($walkin_customers))
                             <tr class="">
                                 <td class="border-top bg-light"> Walk-In Customers</td>
                                 <td class="border-top bg-light" style="text-align:right;">{{$walkin_customers ?? 0}}</td>
                             </tr>
-                            @endif -->
-                                <!-- <tr class="">
+                            @endif
+                                <tr class="">
                                     <td class="border-top bg-light" >Arrival Ratio (Walkin included)</td>
                                     <td class="border-top bg-light" style="text-align:right;">
                                         <?php
-                                        // if (isset($arrived) && isset($Appointments)) {
-                                        //     echo number_format(($arrived / count($Appointments)) * 100, 2) . '%';
-                                        // } else {
-                                        //     echo '00.00 %';
-                                        // }
+                                        if (isset($arrived) && isset($Appointments)) {
+                                            echo number_format(($arrived / count($Appointments)) * 100, 2) . '%';
+                                        } else {
+                                            echo '00.00 %';
+                                        }
                                         ?>
                                     </td>
-                                </tr> -->
+                                </tr>
 
                                 @if(isset($walkin_customers))
                                 <tr class="">
