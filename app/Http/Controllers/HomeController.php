566f4581c216e83883fa329d4c338ab0f1020af1
gulraizazam <gulraiz@redsignal.biz>
2023-08-23 15:54:59 +0500

    remove

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -586,6 +586,7 @@ class HomeController extends Controller
                 $todayRecords = $todayRecords->select('invoices.id', 'invoice_details.service_id', DB::raw('SUM(invoices.total_price) AS total_price'))
                     ->groupBy('invoice_details.service_id')
                     ->get();
+                    dd($todayRecords->toArray());
                 $prepareData = [];
 
                 foreach ($todayRecords as $key => $todayRecord) {
