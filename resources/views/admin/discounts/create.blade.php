8ad3d312a36841acb182de9a97d45159fc69f9f2
gulraizazam <gulraiz@redsignal.biz>
2023-09-05 19:41:18 +0500

    resolved

--- resources/views/admin/discounts/create.blade.php
+++ resources/views/admin/discounts/create.blade.php
@@ -86,37 +86,40 @@
                                     
                                 </select>
                             </div>
-                            <div class="fv-row col-12 discount_type_wrap get_discount_type  d-flex mt-3">    
-                                <a class="btn p btn-primary px-3 mr-4 py-0  d-flex justify-content-center align-items-center add_new_discount_field add_new_discount"><i class="la la-plus p-0 m-0"></i></a>
-                                <label class="fw-bold fs-6 pl-0 pr-4 pt-2 mb-0">Get</label>
-                                <select class="form-control form-control-solid mb-0" name="sessions[]">
-                                    <option value="">Select Session</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                </select>
-                                <label class="fw-bold fs-6 px-5 text-nowrap pt-2 mb-0">Sessions of</label>
-                                <select class="form-control form-control-solid mb-0" name="services_name[]" id="services_sessions">
-                                </select>
-                                <div class="d-flex align-items-center ml-5">
-                                    <div class="radio-inline tax-radios mb-0 mr-3">
-                                        <label class="radio">
-                                            <input type="radio" class="group_slug" value="complimentory" name="disc_type[]">
-                                            <span class="mr-2"></span>
-                                            Complimentory
-                                        </label>
-                                    </div>
+                            <div class="fv-row col-12 discount_type_wrap get_discount_type mt-3">
+                                <div class="d-flex">
+                                    <a class="btn p btn-primary px-3 mr-4 py-0  d-flex justify-content-center align-items-center add_new_discount_field add_new_discount"><i class="la la-plus p-0 m-0"></i></a>
+                                    <label class="fw-bold fs-6 pl-0 pr-4 pt-2 mb-0">Get</label>
+                                    <select class="form-control form-control-solid mb-0" name="sessions[]">
+                                        <option value="">Select Session</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                    </select>
+                                    <label class="fw-bold fs-6 px-5 text-nowrap pt-2 mb-0">Sessions of</label>
+                                    <select class="form-control form-control-solid mb-0" name="services_name[]" id="services_sessions">
+                                    </select>
+                                    <div class="d-flex align-items-center ml-5">
+                                        <div class="radio-inline tax-radios mb-0 mr-3">
+                                            <label class="radio">
+                                                <input type="radio" class="group_slug" value="complimentory" name="disc_type[]">
+                                                <span class="mr-2"></span>
+                                                Complimentory
+                                            </label>
+                                        </div>
 
-                                    <div class="radio-inline tax-radios mb-0" id="custom">
-                                        <label class="radio">
-                                            <input type="radio" class="group_slug" value="custom" name="disc_type[]">
-                                            <span class="mr-2"></span>
-                                            Percentage
-                                        </label>
+                                        <div class="radio-inline tax-radios mb-0" id="custom">
+                                            <label class="radio">
+                                                <input type="radio" class="group_slug percentage" value="custom" name="disc_type[]">
+                                                <span class="mr-2"></span>
+                                                Percentage
+                                            </label>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        
                         <div class="fv-row col-md-12 mt-5" id="amount">
                             <label class="required fw-bold fs-6 mb-2 pl-0">Amount <span class="text text-danger">*</span></label>
                             <input min="0" id="add_amount" class="form-control" type="number" name="amount">
