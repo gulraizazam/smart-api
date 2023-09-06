016f2d183c619c663254091f9102b1fc726507ce
gulraizazam <gulraiz@redsignal.biz>
2023-09-06 18:46:13 +0500

    resolved

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -431,6 +431,7 @@ class ProductsController extends Controller
                 return abort(401);
             }
             $products_logs = Activity::where(['log_name' => 'product', 'subject_id' => $id])->orderBy('id', 'DESC')->get();
+            dd($products_logs);
             $users = User::where(['account_id' => Auth::User()->account_id])->get()->getDictionary();
             $brands = Brand::getAllRecordsDictionary(Auth::User()->account_id);
             $centres = Locations::getAllRecordsDictionary(Auth::user()->account_id, 'custom', 'id', 'desc', ACL::getUserCentres());
