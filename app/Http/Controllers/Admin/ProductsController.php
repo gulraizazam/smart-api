ddc24934bac9338fccd470f08874caf48cb1a967
gulraizazam <gulraiz@redsignal.biz>
2023-09-06 18:58:12 +0500

    resolved

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -433,7 +433,7 @@ class ProductsController extends Controller
             $products_logs = Activity::where(['log_name' => 'product', 'subject_id' => $id])->orderBy('id', 'DESC')->get();
           
 
-            //$users = User::where(['account_id' => Auth::User()->account_id])->get()->getDictionary();
+            $users = User::all()->getDictionary();
             
             $brands = Brand::getAllRecordsDictionary(Auth::User()->account_id);
             dd( $brands);
