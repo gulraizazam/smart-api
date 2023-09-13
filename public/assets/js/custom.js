bc1ecc253454df0b3f385042f7bcc8efb1eff5bd
Muhammad Atif <matif@redsignal.biz>
2023-09-13 13:19:55 +0500

    error esolved

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -34,6 +34,7 @@ $(document).ready(function () {
         $("#modal_allocate_discounts_form").find("#services").empty();
         $("#modal_create_order_form").find('form').trigger('reset');
         $("#modal_edit_transfer_products_form").find('form').trigger('reset');
+        $("#modal_add_product_stock").find('form').trigger('reset');
     });
     $('.select2').select2();
     $('.to-from-datepicker').datepicker({
