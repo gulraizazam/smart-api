f8aa116bdb7ff7b8c95c35d570fd72e3ccc2382d
gulraizazam <gulraiz@redsignal.biz>
2023-09-18 10:21:16 +0500

    resoved

--- app/Http/Controllers/Admin/AppointmentsController.php
+++ app/Http/Controllers/Admin/AppointmentsController.php
@@ -721,6 +721,7 @@ class AppointmentsController extends Controller
      */
     private function getDefaultListing(Request $request)
     {
+       
 
         $where = [];
         /*
@@ -819,10 +820,11 @@ class AppointmentsController extends Controller
             $where[] = [['appointments.consultancy_type' => $filters['consultancy_type']]];
             Filters::put(Auth::User()->id, $filename, 'consultancy_type', $filters['consultancy_type']);
         }
-        if (hasFilter($filters, 'created_at')) {
-            $where[] = ['appointments.created_at', '>=', $start_date_time];
-            $where[] = ['appointments.created_at', '<=', $end_date_time];
-            Filters::put(Auth::User()->id, $filename, 'created_at', $filters['created_at']);
+        if (hasFilter($filters, 'created_from')) {
+            $where[] = ['appointments.created_at', '>=', $request->created_from.' 00:00:00'];
+            $where[] = ['appointments.created_at', '<=',  $request->created_to.' 23:59:00'];
+            Filters::put(Auth::User()->id, $filename, 'created_from', $filters['created_from']);
+            Filters::put(Auth::User()->id, $filename, 'created_to', $filters['created_to']);
         }
         if (hasFilter($filters, 'phone')) {
             $phone = substr($filters['phone'], 1);
