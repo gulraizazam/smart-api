a7e60f11265de41f7faf8ad64b356eaadc6e7b4d
Gulraiz_azam <gulraizazam@technerdslab.com>
2023-09-19 16:40:10 +0500

    resolved

--- resources/views/admin/packages/create.blade.php
+++ resources/views/admin/packages/create.blade.php
@@ -129,7 +129,7 @@
                             <th>Discount Value</th>
                             <th>Amount</th>
                             <th>Tax </th>
-                            <th>Total Amount</th>
+                            <th>Total</th>
                             <!-- <th>Is Consumed</th> -->
                             <th>Action</th>
                         </tr>
