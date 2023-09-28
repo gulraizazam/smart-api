1fe5367ba36bf4bc76bdc44c44572da0450d5ac5
Muhammad Atif <matif@redsignal.biz>
2023-09-28 12:26:45 +0500

    error resolved

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -222,7 +222,7 @@ class Product extends BaseModal
         if (!$record) {
             return null;
         }
-       
+
         $record->update($data);
 
         $subjectModel = self::find($id);
@@ -247,10 +247,8 @@ class Product extends BaseModal
         if (self::isChildExists($id, Auth::User()->account_id)) {
             return collect(['status' => false, 'message' => 'Child records exist, unable to delete resource']);
         }
-
-        ProductDetail::where('product_id', $id)->delete();
         Stock::where(['product_id' => $product->id])->delete();
-
+        ProductDetail::where('product_id', $id)->delete();
         $record = $product->delete();
 
         $subjectModel = $product;
