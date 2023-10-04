842b6cc980a46f8845b283197fbdb0e8ee1284bb
gulraizazam <gulraiz@redsignal.biz>
2023-10-04 15:21:54 +0500

    resolved

--- resources/views/admin/invoices/invoice_pdf.blade.php
+++ resources/views/admin/invoices/invoice_pdf.blade.php
@@ -192,7 +192,7 @@
     <table>
         <tr>
             <td>
-                <img class="img-responsive logo" src="{{asset('assets/media/new_logo.png')}}" style="width:235px;" alt=""/>
+                <img class="img-responsive logo" src="https://crm2.cutera.pk/public/assets/media/new_logo.png" style="width:235px;" alt=""/>
                 <p class="logo_caption">{{$location_info->address}}.</p>
                 <p class="logo_caption logo_caption2">Phone. {{$location_info->fdo_phone}}  &nbsp; |  &nbsp; Email. {{$account->email}}  &nbsp; | &nbsp;  www.cuteraesthetics.com  &nbsp; | &nbsp; NTN. {{$location_info->ntn}} &nbsp; | &nbsp; STN. {{$location_info->stn}}</p>
             </td>
