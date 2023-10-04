fc574f06d7a10b9193047cc6c17a60158a704e17
gulraizazam <gulraiz@redsignal.biz>
2023-10-04 15:39:40 +0500

    resolved

--- resources/views/admin/reports/operations/walkinreport/reportpdf.blade.php
+++ resources/views/admin/reports/operations/walkinreport/reportpdf.blade.php
@@ -13,7 +13,7 @@
                 <table>
                     <tr>
                         <td>
-                            <img  style="width:235px" class="logo" src="{{ asset('assets/media/new_logo.png') }}"
+                            <img  style="width:235px" class="logo" src="https://crm2.cutera.pk/assets/media/new_logo.png"
                                  class="img-responsive" alt=""/>
                         </td>
                     </tr>
