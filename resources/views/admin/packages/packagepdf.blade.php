04ccce7ed217a287a86209d889d2b740b1902679
gulraizazam <gulraiz@redsignal.biz>
2023-10-04 15:21:54 +0500

    resolved

--- resources/views/admin/packages/packagepdf.blade.php
+++ resources/views/admin/packages/packagepdf.blade.php
@@ -161,7 +161,7 @@
     <table>
         <tr>
             <td style="float:left">
-                <img style="margin-bottom: 10px;" class="logo" src="{{asset('assets/media/new_logo.png')}}" class="img-responsive" alt=""/>
+                <img style="margin-bottom: 10px;" class="logo" src="https://crm2.cutera.pk/public/assets/media/new_logo.png" class="img-responsive" alt=""/>
                 <p class="logo_caption">{{$location_info->address}}.</p>
                 <p class="logo_caption logo_caption2">Phone. {{$location_info->fdo_phone}} &nbsp;| &nbsp; Email. {{$account_info->email}}  &nbsp; | &nbsp;  www.cuteraesthetics.com  &nbsp; | &nbsp; NTN. {{$location_info->ntn}} &nbsp; | &nbsp; STN. {{$location_info->stn}}</p>
             </td>
