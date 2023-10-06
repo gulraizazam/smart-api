2a8d26c21c15e545e9b3f23fdb5c94ef3731ab1b
gulraizazam <gulraiz@redsignal.biz>
2023-10-06 14:58:54 +0500

    reoslved

--- app/Http/Controllers/DashboardReportsController.php
+++ app/Http/Controllers/DashboardReportsController.php
@@ -3052,6 +3052,7 @@ class DashboardReportsController extends Controller
     }
     public function loadFollowupReport(Request $request)
     {
+        dd($request->all());
         if (isset($request->date_range) && $request->date_range) {
             $date_range = explode(' - ', $request->date_range);
             $start_date = date('Y-m-d', strtotime($date_range[0]));
