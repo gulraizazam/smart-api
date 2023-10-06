f3c607d3d84426d7530fa4d4befe11d3ad112229
gulraizazam <gulraiz@redsignal.biz>
2023-10-06 15:01:08 +0500

    reoslved

--- app/Helpers/GeneralFunctions.php
+++ app/Helpers/GeneralFunctions.php
@@ -1123,8 +1123,10 @@ class GeneralFunctions
             $item->settle_tax_amount = $settle_tax_amounts[$item->patient_id] ?? null;
             return $item;
         });
+        dd($plans_check);
         $patient_data = [];
-        $plan_check_amount = collect($plans_check)->where('cash_receive', '>', 0)->where('created_at', '<', Carbon::now()->subDays(7))->pluck('patient_id')->toArray();
+        $plan_check_amount = collect($plans_check)->where('cash_receive', '>', 0)
+        ->where('created_at', '<', Carbon::now()->subDays(7))->pluck('patient_id')->toArray();
 
         foreach ($plans_check as $data) {
             $treatments = Appointments::where([
