d72cc41305d00b792d24abd7de969407f4a127ef
gulraizazam <gulraiz@redsignal.biz>
2023-10-18 16:01:38 +0500

    resolved

--- app/Console/Commands/SecondMessageOfAppointment.php
+++ app/Console/Commands/SecondMessageOfAppointment.php
@@ -140,7 +140,7 @@ class SecondMessageOfAppointment extends Command
             $setting = Settings::whereSlug('sys-current-sms-operator')->first();
 
             $UserOperatorSettings = UserOperatorSettings::getRecord($account->id, $setting->data);
-            dd($setting);
+           
             if ($setting->data == 1) {
                 $SMSObj = [
                     'username' => $UserOperatorSettings->username, // Setting ID 1 for Username
