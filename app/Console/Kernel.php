9a667e4fcb859a4301c8e43253a9c148253b6344
gulraizazam <gulraiz@redsignal.biz>
2023-10-23 12:00:45 +0500

    resolcved

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -60,7 +60,7 @@ class Kernel extends ConsoleKernel
           * 2nd message one day before appointment at 8PM
           */
         $schedule->command('appointment:2nd-message-on-appointment-day')
-            ->dailyAt('19:55')->timezone($timeZone);
+            ->dailyAt('12:02')->timezone($timeZone);
 
         /*
          * 	3rd message 2 hours before appointment
