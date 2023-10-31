abc132cb9c70b2fed56d63ee2c73acdf36129586
Muhammad Atif <matif@redsignal.biz>
2023-10-31 12:29:39 +0000

    CUE-465 Pie Chart Dashboard

--- app/Http/Controllers/DashboardReportsController.php
+++ app/Http/Controllers/DashboardReportsController.php
@@ -131,7 +131,7 @@ class DashboardReportsController extends Controller
 
           // Step 2 and 3: Calculate the percentage for each slice
           for ($i = 1; $i < count($dataArray); $i++) {
-            $percentage = ($dataArray[$i][1] / $totalValue) * 100;
+            $percentage = $totalValue != 0 ? ($dataArray[$i][1] / $totalValue) * 100 : 0;
 
             $dataArray[$i][0] = $dataArray[$i][0] . " (" . number_format($percentage ?? 0, 1) . "%)";
           }
@@ -1049,7 +1049,7 @@ class DashboardReportsController extends Controller
 
           // Step 2 and 3: Calculate the percentage for each slice
           for ($i = 1; $i < count($dataArray); $i++) {
-            $percentage = ($dataArray[$i][1] / $totalValue) * 100;
+            $percentage = $totalValue != 0 ? ($dataArray[$i][1] / $totalValue) * 100 : 0;
 
             $dataArray[$i][0] = $dataArray[$i][0] . " (" . number_format($percentage ?? 0, 1) . "%)";
           }
@@ -1186,7 +1186,7 @@ class DashboardReportsController extends Controller
 
           // Step 2 and 3: Calculate the percentage for each slice
           for ($i = 1; $i < count($dataArray); $i++) {
-            $percentage = ($dataArray[$i][1] / $totalValue) * 100;
+            $percentage = $totalValue != 0 ? ($dataArray[$i][1] / $totalValue) * 100 : 0;
 
             $dataArray[$i][0] = $dataArray[$i][0] . " (" . number_format($percentage ?? 0, 1) . "%)";
           }
@@ -1564,7 +1564,7 @@ for ($i = 1; $i < count($dataArray); $i++) {
 
 // Step 2 and 3: Calculate the percentage for each slice
 for ($i = 1; $i < count($dataArray); $i++) {
-  $percentage = ($dataArray[$i][1] / $totalValue) * 100;
+    $percentage = $totalValue != 0 ? ($dataArray[$i][1] / $totalValue) * 100 : 0;
 
   $dataArray[$i][0] = $dataArray[$i][0] . " (" . number_format($percentage ?? 0, 1) . "%)";
 }
@@ -2176,7 +2176,7 @@ $day = $request->period == null ? "today" : $request->period;
 
           // Step 2 and 3: Calculate the percentage for each slice
           for ($i = 1; $i < count($dataArray); $i++) {
-            $percentage = ($dataArray[$i][1] / $totalValue) * 100;
+            $percentage = $totalValue != 0 ? ($dataArray[$i][1] / $totalValue) * 100 : 0;
 
             $dataArray[$i][0] = $dataArray[$i][0] . " (" . number_format($percentage ?? 0, 1) . "%)";
           }
