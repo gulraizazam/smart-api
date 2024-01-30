0cf3ffbe98165f6553c79cd5507e8123228300ab
Gulraiz Azam <gulraiz@mail.redsignal.net>
2024-01-30 14:30:52 +0000

    CUE-525 - Membership

--- app/Models/MembershipType.php
+++ app/Models/MembershipType.php
@@ -2,20 +2,13 @@
 
 namespace App\Models;
 
-use App\Helpers\ACL;
-use App\Helpers\Filters;
-use DateTime;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 
 class MembershipType extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
     protected $fillable = ['name', 'status', 'period', 'amount', 'created_by', 'updated_by', 'deleted_by', 'active'];
     protected $table = 'membership_types';
 
