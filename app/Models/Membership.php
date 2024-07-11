bf5ce3a019096b558f2dfa5e2d4bd918afa814b8
Gulraiz Azam <gulraiz@mail.redsignal.net>
2024-07-11 08:04:10 +0000

    Membership Report

--- app/Models/Membership.php
+++ app/Models/Membership.php
@@ -13,24 +13,27 @@ use Illuminate\Support\Facades\DB;
 class Membership extends Model
 {
     use HasFactory;
-    protected $fillable = ['code', 'membership_type_id', 'start_date','end_date','patient_id','created_by', 'updated_by', 'deleted_by','active'];
+    protected $fillable = ['code', 'membership_type_id', 'start_date', 'end_date', 'patient_id', 'created_by', 'updated_by', 'deleted_by', 'active', 'assigned_at'];
     protected $table = 'memberships';
 
     public function createdBy()
     {
-        return $this->belongsTo(User::class,'created_by');
+        return $this->belongsTo(User::class, 'created_by');
     }
     public function updatedBy()
     {
-        return $this->belongsTo(User::class,'updated_by');
+        return $this->belongsTo(User::class, 'updated_by');
     }
     public function deletedBy()
     {
-        return $this->belongsTo(User::class,'deleted_by');
+        return $this->belongsTo(User::class, 'deleted_by');
     }
     public function membershipType()
     {
-        return $this->belongsTo(MembershipType::class,'membership_type_id');
+        return $this->belongsTo(MembershipType::class, 'membership_type_id');
     }
-    
+    public function patient()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
