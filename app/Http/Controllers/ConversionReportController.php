a2ee7ad10c7de277659cfef1d4a8e7d18cd9aa4e
Ali Haroon <ali.haroon@mail.redsignal.net>
2024-07-15 10:21:38 +0000

    Test to Production

--- app/Http/Controllers/ConversionReportController.php
+++ app/Http/Controllers/ConversionReportController.php
@@ -21,16 +21,16 @@ class ConversionReportController extends Controller
         $users = ($select_All + $employees->toArray() + $operators->toArray());
         $operators->prepend('All', '');
         $locations = Locations::getActiveSorted(ACL::getUserCentres());
-        if(Auth::user()->hasRole('FDM')){
-        }else{
+        if (Auth::user()->hasRole('FDM')) {
+        } else {
             $locations->prepend('All', '');
         }
         $locations_com = Locations::getActiveSorted(ACL::getUserCentres());
 
         return view('admin.reports.conversion', get_defined_vars());
     }
-    
-   
+
+
 
     public function LoadConversionReport(Request $request)
     {
---
+++ public/assets/js/memberhsipsreport.js
@@ -0,0 +1,56 @@
+
+$('#date_range_membership').daterangepicker({
+    locale: {},
+    ranges: {
+        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Last 7 Days': [moment().subtract(6, 'days'), moment().subtract(1, 'days')],
+        'Last 30 Days': [moment().subtract(29, 'days'), moment().subtract(1, 'days')],
+        'This Month': [moment().startOf('month'), moment().endOf('month')],
+        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+        'This Year': [moment().startOf('year'), moment().endOf('year')],
+        'Last Year': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')],
+    },
+    autoUpdateInput: false
+}, function (start, end, label) {
+    $('#date_range_membership').val(start.format('MM/DD/YYYY') + ' - ' + end.format('MM/DD/YYYY'));
+});
+
+// Clear any default value
+$('#date_range_membership').val('');
+var loadMembershipReport = function (that) {
+    if (typeof that.prop("disabled") !== 'undefined' && that.prop("disabled") === true) {
+        return false;
+    }
+    showSpinner();
+    $.ajax({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        url: route('admin.reports.load_membership_report'),
+        type: "POST",
+        data: {
+            location_id: $('#location_id').val(),
+            membership_type_id: $('#membership_type').val(),
+            date_range: $('#date_range_membership').val(),
+        },
+        success: function (response) {
+            $('#membership_report_content').html('');
+            $('#membership_report_content').html(response);
+            $("#memberships_table").DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'excelHtml5',
+                    'csvHtml5',
+                    'pdfHtml5',
+                ],
+                "ordering": false
+            });
+            hideSpinner();
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            hideSpinner();
+            return false;
+        }
+    });
+};
+
