34381b37fef51d45f0ac25ff78edfaf52a8b06b5
gulraizazam <gulraiz@mail.redsignal.net>
2024-11-01 13:04:46 +0500

    resolved

---
+++ app/Http/Middleware/CheckIpRestriction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckIpRestriction
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+            $userRole = $user->role; // Assume the role is stored in the 'role' column
+            $allowedIps = config("ip_restrictions.{$userRole}", []);
+
+            // Check if the user's IP is in the allowed list
+            if (in_array($request->ip(), $allowedIps)) {
+                return $next($request);
+            }
+
+            // If the IP is not allowed, return an error response
+            return response()->json(['error' => 'Unauthorized IP address.'], 403);
+        }
+        return $next($request);
+    }
+}
