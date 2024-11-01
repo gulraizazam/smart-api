a7b9f33b1be6c9f3009427df33a6dd2d56321f6b
gulraizazam <gulraiz@mail.redsignal.net>
2024-11-01 15:09:08 +0500

    resolved

--- app/Http/Middleware/CheckIpRestriction.php
+++ app/Http/Middleware/CheckIpRestriction.php
@@ -27,10 +27,10 @@ class CheckIpRestriction
                 $userRole = $user->role; // Assuming 'role' is the column name
             }
                
-            $restrictedIps = ['103.8.112.42','103.8.112.43','103.8.112.107','203.215.176.205','203.215.176.206','203.215.181.201','203.215.181.206','202.69.38.28'];
+            $restrictedIps = ['103.8.112.42','103.8.112.43','103.8.112.107','203.215.176.205','203.215.176.206','203.215.181.201','203.215.181.206','202.69.38.28','202.166.167.242'];
 
             // Check if the user's IP is in the allowed list
-            if (($userRole == 'CSR' || $userRole == 'CSR Supervisor') && in_array($userIp, $restrictedIps)) {
+            if (($userRole == 'CSR' || $userRole == 'CSR Supervisor' || $userRole == 'HRM') && in_array($userIp, $restrictedIps)) {
                 // Redirect to unauthorized page if the IP is restricted
                 return redirect('unauthorized'); // Ensure this route exists
             }
