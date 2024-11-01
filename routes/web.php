036551ba9817d997f8fc21befe90e9e7bb7831ba
gulraizazam <gulraiz@mail.redsignal.net>
2024-11-01 13:04:46 +0500

    resolved

--- routes/web.php
+++ routes/web.php
@@ -85,9 +85,11 @@ Route::get('/unathorized', function () {
 })->name('unauthorized');
 Auth::routes();
 // Authentication Routes...
-Route::get('login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
-Route::post('login', [App\Http\Controllers\Auth\LoginController::class, 'login'])->name('auth.admin.login');
+Route::middleware(['auth', 'check.ip.restriction'])->group(function () {
+    Route::get('login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
 
+    Route::post('login', [App\Http\Controllers\Auth\LoginController::class, 'login'])->name('auth.admin.login');
+});
 Route::get('/deliver-on-appointment-book', function () {
     \Artisan::call('appointment:deliver-on-appointment-book');
 });
@@ -143,18 +145,7 @@ Route::post('password/reset', [App\Http\Controllers\Auth\ResetPasswordController
 /*After authentication*/
 Route::group(['middleware' => ['auth.common', 'checkAccount'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
 
-    Route::get('attchrole', function () {
-        dd(date_default_timezone_get());
-        $user = \App\Models\User::whereEmail('amjad@redsignal.biz')->first();
-        dd($user->assignRole(1));
-    });
-    Route::get('getrecords', function () {
-        $rr = Leads::join('users', 'users.id', '=', 'leads.patient_id')
-            ->select('leads.lead_status_id', 'leads.active', 'leads.city_id', 'leads.service_id', 'leads.active', 'leads.created_by as lead_created_by', 'leads.id as lead_id', 'leads.created_at as lead_created_at', 'users.id as PatientId', 'users.*')
-            ->get();
-
-        return view('admin.records', compact('rr'));
-    });
+    
     Route::get('error-logs', [LogViewerController::class, 'index']);
     Route::get('updateleads', [LeadsController::class, 'leadupdate']);
     Route::get('updatestatusleads', [LeadsController::class, 'leadstatusupdate']);
