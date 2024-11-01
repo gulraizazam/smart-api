a4a86b5eb9fbc5dd3275a9232514b1ee38dbff06
gulraizazam <gulraiz@mail.redsignal.net>
2024-11-01 15:56:49 +0500

    resolved

--- resources/views/unathorized.blade.php
+++ resources/views/unathorized.blade.php
@@ -98,6 +98,7 @@ h1 {
 <div class="message">
   <h1>Access to this page is restricted</h1>
   <p>Please check with the site admin if you believe this is a mistake.</p>
+  <a href="{{route('logout')}}">Logout</a>
 </div>
 <!-- partial -->
   
