d8b05ec1c135c3ba49f1447c68aff47b40a23ce2
gulraizazam <gulraiz@mail.redsignal.net>
2024-11-01 15:03:53 +0500

    resolved

--- app/Http/Middleware/CheckIpRestriction.php
+++ app/Http/Middleware/CheckIpRestriction.php
@@ -26,8 +26,8 @@ class CheckIpRestriction
             } else {
                 $userRole = $user->role; // Assuming 'role' is the column name
             }
-                
-            $restrictedIps = config("ip_restrictions.restricted_ips.{$userRole}", []);
+                dd($userRole);
+            $restrictedIps = ['103.8.112.42','103.8.112.43','103.8.112.107','203.215.176.205','203.215.176.206','203.215.181.201','203.215.181.206','202.69.38.28','39.34.184.159'];
 
             // Check if the user's IP is in the allowed list
             if (($userRole == 'CSR' || $userRole == 'CSR Supervisor') && in_array($userIp, $restrictedIps)) {
