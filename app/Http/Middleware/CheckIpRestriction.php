5229e16219ae73cb6aaf06d32f39926469b6124d
gulraizazam <gulraiz@mail.redsignal.net>
2024-11-01 16:00:15 +0500

    resolved

--- app/Http/Middleware/CheckIpRestriction.php
+++ app/Http/Middleware/CheckIpRestriction.php
@@ -34,7 +34,7 @@ class CheckIpRestriction
                 return $next($request);
                  // Ensure this route exists
             }
-            return view('unauthorized');
+            return redirect('unathorized');
             
         }
         
