9180dcc4affc82050b39defdcba1b773ccb8a06c
gulraizazam <gulraiz@mail.redsignal.net>
2024-11-01 13:40:01 +0500

    resolved

--- app/Http/Middleware/CheckIpRestriction.php
+++ app/Http/Middleware/CheckIpRestriction.php
@@ -21,20 +21,20 @@ class CheckIpRestriction
             $user = Auth::user();
             if (method_exists($user, 'hasRole')) {
                 $userRole = $user->getRoleNames()->first(); // Get the first role if multiple
-                dd($request->ip());
+                $userIp=$request->ip();
             } else {
                 $userRole = $user->role; // Assuming 'role' is the column name
             }
                 
-            $allowedIps = config("ip_restrictions.{$userRole}", []);
+            $restrictedIps = config("ip_restrictions.restricted_ips.{$userRole}", []);
 
             // Check if the user's IP is in the allowed list
-            if (in_array($request->ip(), $allowedIps)) {
-                return $next($request);
+            if ($userRole=='CSR' || $userRole=='CSR Supervisor' && in_array($userIp, $restrictedIps)) {
+                // Redirect to unauthorized page if the IP is restricted
+                return redirect('unauthorized'); // Ensure this route exists
             }
 
-            // If the IP is not allowed, return an error response
-            return redirect('unathorized');
+            
         }
         return $next($request);
     }
