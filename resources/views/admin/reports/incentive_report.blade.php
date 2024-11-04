56a9b0f7e256d13b4ad375a052737a911f69f43a
gulraizazam <gulraiz@mail.redsignal.net>
2024-11-05 00:03:38 +0500

    resolved

---
+++ resources/views/admin/reports/incentive_report.blade.php
@@ -0,0 +1,26 @@
+<div id="incentive_content">
+    <table id="incentive_table" class="table table-striped">
+        <thead>
+            <tr>
+                <th>Appointment Date</th>
+                <th>Cash Amount</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($incentives as $appointment)
+                @foreach($appointment->packageAdvances as $advance)
+                    <tr>
+                        <td>{{ $appointment->appointment_date }}</td>
+                        <td>{{ $advance->cash_amount }}</td>
+                    </tr>
+                @endforeach
+            @endforeach
+        </tbody>
+        <tfoot>
+            <tr>
+                <td><strong>Total Incentive</strong></td>
+                <td>{{ $totalIncentive }}</td>
+            </tr>
+        </tfoot>
+    </table>
+</div>
