424666571f3faed12e1e9ccf012c7f180c9409b6
gulraizazam <gulraiz@mail.redsignal.net>
2024-11-05 22:45:00 +0500

    resolved

--- app/Http/Controllers/Admin/Reports/FinanceReportController.php
+++ app/Http/Controllers/Admin/Reports/FinanceReportController.php
@@ -2897,7 +2897,7 @@ class FinanceReportController extends Controller
                         ->whereBetween('created_at', [$startDate, $endDate])
                         ->where('cash_flow', 'in')
                         ->where('cash_amount', '>', 0)
-                        ->where('delete_at', null)
+                        ->where('deleted_at', null)
                         ->sum('cash_amount');
 
         // Step 2: Calculate Month-wise Revenue
