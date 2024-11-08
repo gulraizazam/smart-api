43e21020eed46b56ab0cd37d4b5d324f4b4a9f94
gulraizazam <gulraiz@mail.redsignal.net>
2024-11-08 21:58:05 +0500

    resolved

--- resources/views/admin/reports/incentive_report.blade.php
+++ resources/views/admin/reports/incentive_report.blade.php
@@ -1,5 +1,7 @@
-<div id="incentive_content">
-    <table id="incentive_table" class="table table-striped">
+<div id="revenue_report">
+    <h4>Total Revenue for Selected Range: {{ number_format($totalRevenue, 2) }}</h4>
+
+    <table class="table table-striped">
         <thead>
             <tr>
                 <th>Month</th>
@@ -9,16 +11,10 @@
         <tbody>
             @foreach($monthWiseRevenue as $month => $revenue)
                 <tr>
-                    <td>{{ \Carbon\Carbon::parse($month . '-01')->format('F Y') }}</td> <!-- Month -->
-                    <td>{{ number_format($revenue, 2) }}</td> <!-- Monthly Revenue -->
+                    <td>{{ \Carbon\Carbon::parse($month . '-01')->format('F Y') }}</td>
+                    <td>{{ number_format($revenue, 2) }}</td>
                 </tr>
             @endforeach
         </tbody>
-        <tfoot>
-            <tr>
-                <td><strong>Total Amount for Selected Range</strong></td>
-                <td>{{ number_format($currentRangeTotal, 2) }}</td> <!-- Total cash amount for selected range -->
-            </tr>
-        </tfoot>
     </table>
 </div>
