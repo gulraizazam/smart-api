9b10c9001797e2f179b88813646b3ce2be41e01e
gulraizazam <gulraiz@mail.redsignal.net>
2024-11-10 12:00:41 +0500

    resolved

--- resources/views/admin/reports/doctor_incentive_report.blade.php
+++ resources/views/admin/reports/doctor_incentive_report.blade.php
@@ -1,12 +1,24 @@
-<div class="card mt-4">
-            <div class="card-body">
-                <h4>Total Cash Amount (Within Date Range and Criteria):</h4>
-                <p>${{ number_format($totalCashAmount, 2) }}</p>
+
+        <div id="revenue_report">
+    <h4>Total Revenue for Selected Range: {{ number_format($totalRevenue, 2) }}</h4>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
                 
-                <h4>Total Revenue Earned by Doctor (Within Date Range):</h4>
-                <p>${{ number_format($totalDoctorRevenue, 2) }}</p>
-                
-                <h4>Difference:</h4>
-                <p>${{ number_format($diff, 2) }}</p>
-            </div>
-        </div>
+                <th>Total Amount In Given Range</th>
+                <th>Total Conversion Amount</th>
+                <th>Difference</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+                <tr>
+                    <td>{{ number_format($totalCashAmount, 2) }}</td>
+                    <td>{{ number_format($totalDoctorRevenue, 2) }}</td>
+                    <td>{{ number_format($diff, 2) }}</td>
+                </tr>
+            
+        </tbody>
+    </table>
+</div>
