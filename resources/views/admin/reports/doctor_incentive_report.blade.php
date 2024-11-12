34b6c2fb7db3dbdfc345485e77465e53a21d3354
gulraizazam <gulraiz@mail.redsignal.net>
2024-11-12 22:28:16 +0500

    resolved

--- resources/views/admin/reports/doctor_incentive_report.blade.php
+++ resources/views/admin/reports/doctor_incentive_report.blade.php
@@ -1,25 +1,40 @@
-
-        <div id="revenue_report">
-   
-
+<div id="revenue_report">
     <table class="table table-striped">
         <thead>
             <tr>
-                
                 <th>Total Amount In Given Range</th>
                 <th>Total Conversion Amount</th>
                 <th>Difference</th>
             </tr>
         </thead>
         <tbody>
-            
-                <tr>
+            <tr>
                 <td>{{ number_format($totalDoctorRevenue, 2) }}</td>
-                    <td>{{ number_format($totalCashAmount, 2) }}</td>
-                    
-                    <td>{{ number_format($diff, 2) }}</td>
+                <td>{{ number_format($totalCashAmount, 2) }}</td>
+                <td>{{ number_format($diff, 2) }}</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <h3>Patient Payments</h3>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Patient ID</th>
+                <th>Patient Name</th>
+                <th>Payment Date</th>
+                <th>Amount</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($patients as $patient)
+                <tr>
+                    <td>{{ $patient->patient_id }}</td>
+                    <td>{{ $patient->patient_name }}</td>
+                    <td>{{ $patient->payment_date->format('d-m-Y') }}</td>
+                    <td>{{ number_format($patient->cash_amount, 2) }}</td>
                 </tr>
-            
+            @endforeach
         </tbody>
     </table>
 </div>
