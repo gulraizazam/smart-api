5e3d8aa1e5d994273145b9635093ea489b13ddd3
gulraizazam <gulraiz@mail.redsignal.net>
2024-12-29 19:42:18 +0500

    Resolved

--- app/Http/Controllers/Admin/Reports/FinanceReportController.php
+++ app/Http/Controllers/Admin/Reports/FinanceReportController.php
@@ -2997,6 +2997,7 @@ class FinanceReportController extends Controller
         $endDate = date('Y-m-d 23:59:59', strtotime($dates[1]));
     
         $centerId = $request->input('centre_id');
+        $createdBy = $request->input('created_by');
         $appointments = Appointments::with(['patient','location','user', 'hasInvoices' => function ($query) {
             $query->orderBy('created_at', 'asc'); // Order invoices by creation time
         }])
@@ -3009,6 +3010,10 @@ class FinanceReportController extends Controller
                 // Apply the centre_id condition if it's present
                 return $query->where('location_id', $centerId);
             })
+            ->when($createdBy, function ($query, $createdBy) {
+                // Apply the centre_id condition if it's present
+                return $query->where('created_by', $createdBy);
+            })
             ->whereBetween('created_at', [$startDate, $endDate])
             ->get();
         return view('admin.reports.appointmentsReports',get_defined_vars());
@@ -3017,7 +3022,7 @@ class FinanceReportController extends Controller
     public function appointmentsReport()
     {
        
-       // $Users = User::getAllRecords(Auth::User()->account_id)->where('user_type_id', 5)->where('active', 1)->getDictionary();
+        $Users = User::getAllRecords(Auth::User()->account_id)->where('user_type_id', 5)->where('active', 1)->getDictionary();
         $locations = Locations::getActiveRecordsByCity('', ACL::getUserCentres(), Auth::User()->account_id);
         $locations = Locations::getActiveRecordsByCity('', ACL::getUserCentres(), Auth::User()->account_id);
         return view('admin.reports.appointments_report', get_defined_vars());
