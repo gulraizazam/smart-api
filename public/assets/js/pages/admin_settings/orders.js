825f6ff2f584385c7e86f44e88810764f34adaf8
gulraizazam <gulraiz@mail.redsignal.net>
2025-01-19 18:56:59 +0500

    resolved inv

--- public/assets/js/pages/admin_settings/orders.js
+++ public/assets/js/pages/admin_settings/orders.js
@@ -239,6 +239,7 @@ function calculateTotal(data) {
             $('#total_product_price strong').text(discountedTotal.toFixed(2)); // Update total with discount
             $("#grand_total").val(discountedTotal.toFixed(2));
             $('#refund_total_product_price').text(discountedTotal.toFixed(2)); // Update refund price
+            $('#discount').val(discount); // Update refund price
         },
         error: function (error) {
             console.error("Error checking membership:", error);
