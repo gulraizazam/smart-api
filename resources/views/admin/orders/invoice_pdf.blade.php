f96ef843a25211588e328fb4f1e9747c73cf8b03
gulraizazam <gulraiz@mail.redsignal.net>
2025-01-19 19:04:59 +0500

    resolved inv

--- resources/views/admin/orders/invoice_pdf.blade.php
+++ resources/views/admin/orders/invoice_pdf.blade.php
@@ -380,7 +380,7 @@
                 <th>Product Price</th>
                 <th>Quantity</th>
                 <th>Sub Total</th>
-                <th>Dicount</th>
+                <th>Discount(%)</th>
             </tr>
             @foreach ($invoice_info->orderDetail as $product)
                 <tr>
@@ -389,7 +389,7 @@
                     <td>{{ $product->product->sale_price }}</td>
                     <td>{{ $product->quantity }}</td>
                     <td>{{ $product->product->sale_price * $product->quantity }}</td>
-                    <td>{{ $product->discount ?? 0% }}</td>
+                    <td>{{ $invoice_info->discount }}</td>
                 </tr>
             @endforeach
         </table>
