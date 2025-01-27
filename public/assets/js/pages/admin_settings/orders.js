9a18aa312eec1b9c5445c771ae1071b9b1d788e5
gulraizazam <gulraiz@mail.redsignal.net>
2025-01-28 00:19:37 +0500

    Resolved

--- public/assets/js/pages/admin_settings/orders.js
+++ public/assets/js/pages/admin_settings/orders.js
@@ -507,7 +507,7 @@ function SelectEmployee(){
     $("#product_list").html("");
     $("#discount").val("");
     $("#total_product_price").text("");
-    $("#add_order_product").change();
+    $("#add_order_product").select2().change();
     $("#product_discount").text("");
     $("#prescribedBy").hide();
     let url = route('admin.get-employees');
@@ -543,7 +543,7 @@ function SelectEmployee(){
    }else{
     $("#product_list").html("");
     $("#discount").val("");
-    $("#add_order_product").change();
+    $("#add_order_product").select2().change();
     $("#total_product_price").text("");
     $("#product_discount").text("");
         $("#prescribedBy").show();
