f2ea8ce30a043558d38144ddd5a564b81aa7caa4
gulraizazam <gulraiz@mail.redsignal.net>
2025-01-29 19:55:22 +0500

    Resolved

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -835,7 +835,7 @@
                         </ul>
                     </div>
                     @endcan
-                    @can('transfer_product_manage')
+                    @can('product_manage')
                     <div class="menu-submenu">
                         <i class="menu-arrow"></i>
                         <ul class="menu-subnav">
