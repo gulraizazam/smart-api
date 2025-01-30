e023335db2cbced65b5ae83dbb0a58a5abb506cf
gulraizazam <gulraiz@mail.redsignal.net>
2025-01-30 23:32:27 +0500

    Resolved

--- resources/views/admin/products/filters.blade.php
+++ resources/views/admin/products/filters.blade.php
@@ -32,46 +32,16 @@
             </select>
         </div> -->
 
-        @if (\Illuminate\Support\Facades\Gate::allows('view_inactive_centres'))
+       
             <div class="col-lg-3 mb-lg-0 mb-6">
                 <label>Status:</label>
                 <select class="form-control filter-field select2" name="status" id="search_status">
                 </select>
             </div>
-        @else
-            <div class="col-lg-3 mb-lg-0 mb-6">
-                <label>Centre:</label>
-                <select class="form-control filter-field select2" name="centre_id" id="search_centre_id">
-                </select>
-            </div>
-        @endif
+       
     </div>
 
-    <div class="row mb-8 advance-filters" style="display: none;">
-        @if (\Illuminate\Support\Facades\Gate::allows('view_inactive_centres'))
-            <div class="col-lg-3 mb-lg-0 mb-6">
-                <label>Centre:</label>
-                <select class="form-control filter-field select2" name="centre_id" id="search_centre_id">
-                </select>
-            </div>
-        @endif
-        <div class="col-lg-3 mb-lg-0 mb-6">
-            <label>Warehouse:</label>
-            <select class="form-control filter-field select2" name="warehouse_id" id="search_warehouse_id">
-            </select>
-        </div>
-        <div class="col-lg-3 mb-lg-0 mb-6 @if ($errors->has('date_range')) has-error @endif">
-            {!! Form::label('date_range', 'Created at:', ['class' => 'control-label']) !!}
-            <div class="input-group">
-                {!! Form::text('date_range', null, [
-                    'id' => 'date_range',
-                    'class' => 'form-control',
-                    'autocomplete' => 'off',
-                    'placeholder' => 'Select Date Range',
-                ]) !!}
-            </div>
-        </div>
-    </div>
+    
 
     <div class="row">
         <div class="col-md-10">
