6094c1000939ba38028b8f23ee4dbd9df33e9827
gulraizazam <gulraiz@mail.redsignal.net>
2025-02-09 15:56:05 +0500

    Resolved

--- app/Http/Controllers/Admin/MembershipsController.php
+++ app/Http/Controllers/Admin/MembershipsController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Exports\ExportMembership;
 use App\HelperModule\ApiHelper;
 use App\Helpers\Filters;
 use App\Http\Controllers\Controller;
+use Barryvdh\DomPDF\Facade\Pdf;
 use App\Models\Membership;
 use App\Models\MembershipType;
 use App\Models\User;
@@ -15,6 +17,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
+use Maatwebsite\Excel\Facades\Excel;
 use Rap2hpoutre\FastExcel\FastExcel;
 
 class MembershipsController extends Controller
@@ -415,17 +418,140 @@ class MembershipsController extends Controller
         ini_set('memory_limit', '-1');
         set_time_limit(0);
         $where = [];
+        $filename = 'memberships';
+        $filters = getFilters($request->all());
+        $apply_filter = checkFilters($filters, $filename);
 
-        $where = self::membershiptype_filters($request, $account_id, $apply_filter);
+        if (hasFilter($filters, 'code')) {
+            $where[] = [
+                'memberships.code',
+                'like',
+                '%' . $filters['code'] . '%',
+            ];
+            Filters::put(Auth::User()->id, 'memberships', 'code', $filters['code']);
+        } else {
+            if ($apply_filter) {
+                Filters::forget(Auth::User()->id, 'memberships', 'code');
+            } else {
+                if (Filters::get(Auth::User()->id, 'memberships', 'code')) {
+                    $where[] = [
+                        'memberships.code',
+                        'like',
+                        '%' . Filters::get(Auth::User()->id, 'memberships', 'code') . '%',
+                    ];
+                }
+            }
+        }
+
+
+        if (hasFilter($filters, 'membership_type_id')) {
+            $where[] = [
+                'memberships.membership_type_id',
+                '=',
+                $filters['membership_type_id'],
+            ];
+            Filters::put(Auth::user()->id, 'memberships', 'membership_type_id', $filters['membership_type_id']);
+        } else {
+            if ($apply_filter) {
+                Filters::forget(Auth::user()->id, 'memberships', 'membership_type_id');
+            } else {
+                if (Filters::get(Auth::user()->id, 'membershipss', 'membership_type_id')) {
+                    if (Filters::get(Auth::user()->id, 'memberships', 'membership_type_id') != null) {
+                        $where[] = [
+                            'memberships.membership_type_id',
+                            '=',
+                            Filters::get(Auth::user()->id, 'memberships', 'membership_type_id'),
+                        ];
+                    }
+                }
+            }
+        }
+        if (hasFilter($filters, 'created_by')) {
+            $where[] = [
+                'memberships.created_by',
+                '=',
+                $filters['created_by'],
+            ];
+            Filters::put(Auth::user()->id, 'memberships', 'created_by', $filters['created_by']);
+        } else {
+            if ($apply_filter) {
+                Filters::forget(Auth::user()->id, 'memberships', 'created_by');
+            } else {
+                if (Filters::get(Auth::user()->id, 'membershipss', 'created_by')) {
+                    if (Filters::get(Auth::user()->id, 'memberships', 'created_by') != null) {
+                        $where[] = [
+                            'memberships.created_by',
+                            '=',
+                            Filters::get(Auth::user()->id, 'memberships', 'created_by'),
+                        ];
+                    }
+                }
+            }
+        }
+        if (hasFilter($filters, 'assigned')) {
+            if ($filters['assigned'] == 1) {
+                // patient_id is not null
+                $where[] = ['memberships.patient_id', '<>', null];
+            } elseif ($filters['assigned'] == 0) {
+                // patient_id is null
+                $where[] = ['memberships.patient_id', '=', null];
+            }
+            Filters::put(Auth::user()->id, 'memberships', 'assigned', $filters['assigned']);
+        } else {
+            if ($apply_filter) {
+                Filters::forget(Auth::user()->id, 'memberships', 'assigned');
+            } else {
+                if (Filters::get(Auth::user()->id, 'memberships', 'assigned') !== null) {
+                    $assignedFilter = Filters::get(Auth::user()->id, 'memberships', 'assigned');
+                    if ($assignedFilter == 1) {
+                        $where[] = ['memberships.patient_id', '<>', null];
+                    } elseif ($assignedFilter == 0) {
+                        $where[] = ['memberships.patient_id', '=', null];
+                    }
+                }
+            }
+        }
+        if (hasFilter($filters, 'created_at')) {
+            $date_range = explode(' - ', $filters['created_at']);
+            $start_date_time = date('Y-m-d H:i:s', strtotime($date_range[0]));
+            $end_date_string = new DateTime($date_range[1]);
+            $end_date_string->setTime(23, 59, 0);
+            $end_date_time = $end_date_string->format('Y-m-d H:i:s');
+        } else {
+            $start_date_time = null;
+            $end_date_time = null;
+        }
+        if (hasFilter($filters, 'created_at')) {
+            $where[] = ['memberships.created_at', '>=', $start_date_time];
+            $where[] = ['memberships.created_at', '<=', $end_date_time];
+            Filters::put(Auth::User()->id, 'memberships', 'created_at', $filters['created_at']);
+        } else {
+            if ($apply_filter) {
+                Filters::forget(Auth::User()->id, 'memberships', 'created_at');
+            } else {
+                if (Filters::get(Auth::User()->id, 'memberships', 'created_at')) {
+                    $where[] = [
+                        'memberships.created_at',
+                        '>=',
+                        Filters::get(Auth::User()->id, 'memberships', 'created_at'),
+                    ];
+                }
+            }
+        }
         $memberships =  Membership::with('membershiptype')->where($where)->where('membership_types.active', 1)
-        ->limit($iDisplayLength)
-        ->offset($iDisplayStart)
-        ->orderby($orderBy, $order)
+        
         ->get();
         $customPaper = [0, 0, 720, 1440];
         $pdf = PDF::loadView('admin.memberships.membership-pdf', compact('memberships'))->setPaper($customPaper, 'portrait');
 
         return $pdf->download('memberships.pdf');
+    }
+    public function exportDocs(Request $request)
+    {
+        
+        set_time_limit(0);
+        ini_set('memory_limit', '-1');
+        return Excel::download(new ExportMembership($request), 'memberships.' . $request->ext);
     }
     public static function getRecords(Request $request, $iDisplayStart, $iDisplayLength, $account_id = false, $apply_filter = false)
     {
