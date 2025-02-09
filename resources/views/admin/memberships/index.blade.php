94f6c8ad35aad4c1441ef908898277b46c7f1281
gulraizazam <gulraiz@mail.redsignal.net>
2025-02-09 15:48:16 +0500

    Resolved

--- resources/views/admin/memberships/index.blade.php
+++ resources/views/admin/memberships/index.blade.php
@@ -53,7 +53,30 @@
                                     Add New
                                 </a>
                             @endif
-
+                            <div class="btn-group">
+                                    <a class="btn  btn-primary" href="javascript:void(0);" data-toggle="dropdown">
+                                        <i class="fa fa-download"></i>
+                                        <span class="hidden-xs"> Export </span>
+                                        <i class="fa fa-angle-down"></i>
+                                    </a>
+                                    <ul class="dropdown-menu pull-right export_leads" id="datatable_ajax_tools">
+                                        <li>
+                                            <a href="#" title="Max pdf export limit is 100 records" id="export-pdf-leads" data-href="{{route('admin.memberships.export.pdf')}}" data-action="0" class="tool-action"><i class="la la-file-pdf"></i>
+                                                PDF
+                                                <!-- <span class="export-pdf-limit">(1 to {{config('constants.export-lead-pdf-limit')}})</span></a> -->
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" title="Max export limit is 1000 records" id="export-leads" data-href="{{route('admin.memberships.export.excel')}}" data-action="1" class="tool-action"><i class="la la-file-excel"></i>
+                                                Excel
+                                                <!-- <span class="export-excel-limit">(1 to {{config('constants.export-lead-excel-limit')}})</span> -->
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#" data-href="{{route('admin.leads.export.excel')}}" id="csv-leads" data-action="2" class="tool-action"><i class="la la-file-csv"></i> CSV</a>
+                                        </li>
+                                    </ul>
+                                </div>
                         <!--end::Button-->
                         </div>
 
