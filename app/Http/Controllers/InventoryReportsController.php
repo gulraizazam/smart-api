a4c03154653f26c1e7030dac24f4dd3040916207
gulraizazam <gulraiz@mail.redsignal.net>
2025-02-21 23:33:29 +0500

    resolved

--- app/Http/Controllers/InventoryReportsController.php
+++ app/Http/Controllers/InventoryReportsController.php
@@ -144,7 +144,7 @@ class InventoryReportsController extends Controller
                     ->whereIn('location_id', $locationId)
                     ->pluck('user_id');
             }
-            dd($doctorIds);
+           
 
             // Fetch orders based on doctor IDs and the date range (if provided)
             $ordersQuery = Order::with(['doctor', 'orderDetail.product'])
