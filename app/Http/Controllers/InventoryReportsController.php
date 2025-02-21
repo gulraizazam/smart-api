87e229f10822816f5bba5cc2497ed95e9c9fdb5e
gulraizazam <gulraiz@mail.redsignal.net>
2025-02-21 23:11:36 +0500

    resolved

--- app/Http/Controllers/InventoryReportsController.php
+++ app/Http/Controllers/InventoryReportsController.php
@@ -264,7 +264,7 @@ class InventoryReportsController extends Controller
             }
         
             $stocks = $query->get();
-            
+            dd(DB::getQueryLog());
             return view('admin.reports.addition_report',get_defined_vars());
         }
             
