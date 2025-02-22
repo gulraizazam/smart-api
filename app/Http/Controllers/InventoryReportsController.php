8353bcfd6f2377f33f8b228bc0511cf14250f9c9
gulraizazam <gulraiz@mail.redsignal.net>
2025-02-22 16:29:17 +0500

    resolved

--- app/Http/Controllers/InventoryReportsController.php
+++ app/Http/Controllers/InventoryReportsController.php
@@ -153,33 +153,31 @@ class InventoryReportsController extends Controller
                     // Fetch active doctors as an associative array
                     $users = User::whereIn('id', $doctors)
                         ->where('active', 1)
-                        ->pluck( 'id') // Preserve user IDs
-                        ->toArray();
+                        ->pluck( 'id');
                 
                     // Ensure 'from_id' is an array
                     $locationIds = is_array($locationId) ? $locationId : [$locationId];
                 
                     // Fetch FDM users by getting the user_ids associated with the center (location_id)
-                    $findFDM = UserHasLocations::whereIn('location_id', $locationIds)->pluck('user_id')->toArray();
+                    $findFDM = UserHasLocations::whereIn('location_id', $locationIds)->pluck('user_id');
                 
                     // Fetch the 'FDM' role and get its user ids
                     $findRole = DB::table('roles')->where('name', 'FDM')->first();
                     $roleId = $findRole->id;
                 
                     // Get users who have the FDM role
-                    $roleHasUser = RoleHasUsers::where('role_id', $roleId)->pluck('user_id')->toArray();
+                    $roleHasUser = RoleHasUsers::where('role_id', $roleId)->pluck('user_id');
                 
                     // Get the intersection of users who are both FDM and belong to the center
                     $fdmUsers = array_intersect($findFDM, $roleHasUser);
                 
                     // Fetch FDM user details (id and name) from the users table
                     $FDMUsers = User::whereIn('id', $fdmUsers)
-                        ->pluck('id') // Preserve user IDs
-                        ->toArray();
+                        ->pluck('id');
                 
                     // Merge the arrays while preserving keys
                     $doctorIds = $users + $FDMUsers;
-                    dd($doctorIds);
+                    
             }
         
             // Fetch orders based on doctor IDs and the date range (if provided)
