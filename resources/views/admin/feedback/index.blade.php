bcd4ee3970994ae64e7d4557eb062e4497a8b9dd
gulraizazam <gulraiz@mail.redsignal.net>
2025-04-14 12:52:39 +0500

    resolved

--- resources/views/admin/feedback/index.blade.php
+++ resources/views/admin/feedback/index.blade.php
@@ -38,9 +38,9 @@
                         <div class="card-toolbar">
                           
                            
-                            <!-- @if(Gate::allows('feedback_create')) -->
+                            
                             <a href="" class="btn btn-primary " style="height: 37px;" data-toggle="modal" data-target="#modal_add_feedback"><i class="la la-plus"></i>  Add Feedback</a>
-                            <!-- @endif -->
+                           
 
                         <!--end::Button-->
                         </div>
