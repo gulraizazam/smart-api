4a830b12af4e14e1b6bc3b5afc1393f4f0197ba3
tomfoodery <foodplatform@outlook.com>
2025-04-24 12:38:15 +0500

    resolved

--- public/assets/js/dailyarrival.js
+++ public/assets/js/dailyarrival.js
@@ -65,11 +65,11 @@ $('#date_range_inv').daterangepicker({
     locale: {
     },
     ranges: {
-        
-    
+
+
         'This Month': [moment().startOf('month'), moment().endOf('month')],
         'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
-        
+
     },
     startDate: moment().startOf('month'),
     endDate: moment().endOf('month')
@@ -167,11 +167,11 @@ var loadIncentiveReport  = function (that) {
         url: route('admin.reports.incentive_report'),
         type: "POST",
         data: {
-           
+
             doctor_id: $('#doctor_id').val(),
             date_range: $('#date_range_incentive').val(),
             centre_id: $('#centre_id').val(),
-           
+
         },
         success: function(response){
             $('#incentive_content').html('');
@@ -205,12 +205,12 @@ var loadAppointmentsReport  = function (that) {
         url: route('admin.reports.appointments_report'),
         type: "POST",
         data: {
-           
+
             time: $('#time_id').val(),
             date_range: $('#date_range_appointments').val(),
             centre_id: $('#centre_id').val(),
             created_by:$("#created_by_id").val(),
-           
+
         },
         success: function(response){
             $('#apt_content').html('');
@@ -247,7 +247,7 @@ var loadInventoryReport  = function (that) {
         return false;
     }
     showSpinner();
-  
+
     $.ajax({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -255,14 +255,14 @@ var loadInventoryReport  = function (that) {
         url: route('admin.reports.load_inventory_report'),
         type: "POST",
         data: {
-           
+
            report_type:$("#report_type").val(),
             date_range: $('#date_range_inv').val(),
             centre_id: $('#centre_id').val(),
             doctor_id:$("#doctor_id_filter").val(),
             brand_id:$("#brand_id").val(),
-           
-           
+
+
         },
         success: function(response){
             $('#inv_content').html('');
@@ -308,8 +308,12 @@ var loadFeedbackReport  = function (that) {
     if (typeof that.prop("disabled") !== 'undefined' && that.prop("disabled") === true) {
         return false;
     }
+    if($("#centre_id").val() == ""){
+        alert("Please select a centre");
+        return false;
+    }
     showSpinner();
-  
+
     $.ajax({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -317,14 +321,14 @@ var loadFeedbackReport  = function (that) {
         url: route('admin.reports.load_feedback_report'),
         type: "POST",
         data: {
-           
-           
+
+
             date_range: $('#date_range_inv').val(),
             centre_id: $('#centre_id').val(),
             doctor_id:$("#feedback_doctor_id_filter").val(),
             service_id:$("#service_id_filter").val(),
-           
-           
+
+
         },
         success: function(response){
             $('#feedback_content').html('');
@@ -439,9 +443,9 @@ var loadPatientFollowUpMonthReport = function (that) {
     });
 };
 function getEmployees(locationId){
-   
+
     let url = route('admin.get-doctors');
- 
+
     $.ajax({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -451,11 +455,11 @@ function getEmployees(locationId){
         cache: false,
         data: {location_id: locationId},
         success: function (response) {
-          
+
             if(response.status==false){
                 toastr.error(response.message);
             }else{
-                        
+
                  let employees = response.users;
                 let emp_options = '<option value="">Select Doctor</option>';
                 Object.entries(employees).forEach(function (value, index) {
@@ -463,18 +467,18 @@ function getEmployees(locationId){
                 });
                 $("#doctor_id_filter").html(emp_options);
             }
-            
+
         },
         error: function (xhr, ajaxOptions, thrownError) {
             errorMessage(xhr);
         }
     });
-  
+
 }
 function getCentreDoctors(locationId){
-   
+
     let url = route('admin.get-centre-doctors');
- 
+
     $.ajax({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -484,11 +488,11 @@ function getCentreDoctors(locationId){
         cache: false,
         data: {location_id: locationId},
         success: function (response) {
-          
+
             if(response.status==false){
                 toastr.error(response.message);
             }else{
-                        
+
                  let employees = response.users;
                 let emp_options = '<option value="">Select Doctor</option>';
                 Object.entries(employees).forEach(function (value, index) {
@@ -496,16 +500,16 @@ function getCentreDoctors(locationId){
                 });
                 $("#feedback_doctor_id_filter").html(emp_options);
             }
-            
+
         },
         error: function (xhr, ajaxOptions, thrownError) {
             errorMessage(xhr);
         }
     });
-  
+
 }
 function patientSearch(search_id = 'patient_id',flag=1) {
-    
+
     $("." + search_id).on("keyup",function() {
         $(".suggestion-list").html('<li>Searching...</li>');
         $(".suggesstion-box").show();
