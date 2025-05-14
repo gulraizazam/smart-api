f918caa89786c909c16101d5b0868eb221813d64
gulraizazam <gulraiz@mail.redsignal.net>
2025-05-14 22:17:40 +0500

    resolved

--- resources/views/admin/reports/accountsalesreport/index.blade.php
+++ resources/views/admin/reports/accountsalesreport/index.blade.php
@@ -290,6 +290,11 @@
 
     @push('js')
 
+   <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+   <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
         <script>
             $("#location_id_com").on('change',function(){
                 $("#location_id_com-report").val($("#location_id_com").val());
