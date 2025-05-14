448bac3f49e4d5e6873d49efd01c78c97ba48d05
gulraizazam <gulraiz@mail.redsignal.net>
2025-05-14 22:19:20 +0500

    resolved

--- resources/views/admin/reports/accountsalesreport/index.blade.php
+++ resources/views/admin/reports/accountsalesreport/index.blade.php
@@ -289,12 +289,25 @@
     @include('admin.settings.edit')
 
     @push('js')
-
+    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+   
    <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
+   <script>
+    $("#servicesSoldTable").DataTable({
+           dom: 'Bfrtip',
+           buttons: [
+               'excelHtml5',
+               'csvHtml5',
+               'pdfHtml5',
+           ],
+           "ordering": false,
+           "pageLength": 50
+       });
+</script>
         <script>
             $("#location_id_com").on('change',function(){
                 $("#location_id_com-report").val($("#location_id_com").val());
@@ -533,18 +546,7 @@
 
         </script>
 
-<script>
-    $("#servicesSoldTable").DataTable({
-           dom: 'Bfrtip',
-           buttons: [
-               'excelHtml5',
-               'csvHtml5',
-               'pdfHtml5',
-           ],
-           "ordering": false,
-           "pageLength": 50
-       });
-</script>
+
     @endpush
 
 @endsection
