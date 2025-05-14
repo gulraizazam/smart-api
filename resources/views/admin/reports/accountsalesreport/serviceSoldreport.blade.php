f8d50bf4c2a2fe38e3e8d88dbef1bd31540d338f
gulraizazam <gulraiz@mail.redsignal.net>
2025-05-14 22:11:45 +0500

    resolved

--- resources/views/admin/reports/accountsalesreport/serviceSoldreport.blade.php
+++ resources/views/admin/reports/accountsalesreport/serviceSoldreport.blade.php
@@ -104,22 +104,27 @@
     </div>
     <div class="clear clearfix"></div>
     <script src="{{ url('js/admin/scrollbar/scrollbardev.js') }}" type="text/javascript"></script>
+    @if($request->get('medium_type') == 'web')
+    <!-- jQuery (required for DataTables) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- DataTables core and extensions -->
     <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
+
     <script>
-         $("#servicesSoldTable").DataTable({
+        $(document).ready(function () {
+            $("#servicesSoldTable").DataTable({
                 dom: 'Bfrtip',
-                buttons: [
-                    'excelHtml5',
-                    'csvHtml5',
-                    'pdfHtml5',
-                ],
-                "ordering": false,
-                "pageLength": 50
+                buttons: ['excelHtml5', 'csvHtml5', 'pdfHtml5'],
+                ordering: false,
+                pageLength: 50
             });
+        });
     </script>
+@endif
 </div>
