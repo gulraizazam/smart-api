eafaf43d2881988b52ee8343d52e78a82901d728
gulraizazam <gulraiz@mail.redsignal.net>
2025-05-14 22:03:45 +0500

    resolved

--- resources/views/admin/reports/accountsalesreport/serviceSoldreport.blade.php
+++ resources/views/admin/reports/accountsalesreport/serviceSoldreport.blade.php
@@ -5,6 +5,8 @@
     @else
         @include('partials.head')
     @endif
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css">
     <style type="text/css">
         @page {
             margin: 10px 20px;
@@ -102,6 +104,12 @@
     </div>
     <div class="clear clearfix"></div>
     <script src="{{ url('js/admin/scrollbar/scrollbardev.js') }}" type="text/javascript"></script>
+    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+        <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
     <script>
          $("#servicesSoldTable").DataTable({
                 dom: 'Bfrtip',
