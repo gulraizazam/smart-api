40471cde4d0b034de81561da40a1fe902ba5a53e
gulraizazam <gulraiz@mail.redsignal.net>
2025-05-15 22:29:50 +0500

    resolved

--- resources/views/admin/reports/feedback_report.blade.php
+++ resources/views/admin/reports/feedback_report.blade.php
@@ -71,7 +71,7 @@
                                         <div class="col-md-3 form-group sn-select @if($errors->has('date_range')) has-error @endif">
                                             {!! Form::label('date_range', 'Date*', ['class' => 'control-label']) !!}
                                             <div class="input-group">
-                                                {!! Form::text('date_range', null, ['id' => 'date_range_inv', 'class' => 'form-control']) !!}
+                                                {!! Form::text('date_range', null, ['id' => 'date_range_ratings', 'class' => 'form-control']) !!}
                                             </div>
                                         </div>
 
