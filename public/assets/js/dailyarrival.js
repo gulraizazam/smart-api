f0e8bab6d2f28d2fb644202c0bc60a7bfd492c7e
tomfoodery <foodplatform@outlook.com>
2025-05-20 10:32:16 +0500

    Resolved

--- public/assets/js/dailyarrival.js
+++ public/assets/js/dailyarrival.js
@@ -412,7 +412,7 @@ var loadFutureTreatmentsReport  = function (that) {
 
             date_range: $('#date_range_patients').val(),
             centre_id: $('#centre_id').val(),
-            patient_id:$("#patient_id_filter").val(),
+            patient_id:$("#create_order_patient_search").val(),
             membership_id:$("#membership_id_filter").val(),
 
 
