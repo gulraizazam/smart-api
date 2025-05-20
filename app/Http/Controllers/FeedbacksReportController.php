0576a033223e963f61e06ffdbca888b670274f5c
tomfoodery <foodplatform@outlook.com>
2025-05-20 09:50:39 +0500

    Resolved

--- app/Http/Controllers/FeedbacksReportController.php
+++ app/Http/Controllers/FeedbacksReportController.php
@@ -184,7 +184,7 @@ class FeedbacksReportController extends Controller
             },
             'membership.membershipType'
         ])
-        ->orderBy('scheduled_date', 'asc')->get();
+        ->orderBy('appointmentsPatient.scheduled_date', 'asc')->get();
 
     return view('admin.reports.futureTreatmentsReport', compact('patients'));
     }
