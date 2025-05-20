d1893cecd33b7ce534f3ae61406621c3ec2904fd
tomfoodery <foodplatform@outlook.com>
2025-05-20 10:10:28 +0500

    Resolved

--- resources/views/admin/reports/future_treatments_report.blade.php
+++ resources/views/admin/reports/future_treatments_report.blade.php
@@ -115,6 +115,9 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
         <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
         <script src="{{asset('assets/js/dailyarrival.js')}}"></script>
+
+        <script src="{{ asset('assets/js/pages/admin_settings/orders.js') }}"></script>
+
         <script>
             $("#ratings_table").DataTable();
         </script>
