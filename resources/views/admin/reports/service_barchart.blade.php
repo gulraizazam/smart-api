fd28a95ce301bcf983474005673fc7d2fae48620
tomfoodery <foodplatform@outlook.com>
2025-05-21 12:55:23 +0500

    Reoslved

--- resources/views/admin/reports/service_barchart.blade.php
+++ resources/views/admin/reports/service_barchart.blade.php
@@ -67,8 +67,8 @@
                 datasets: [{
                     label: 'Total Sold',
                     data: @json($values),
-                    backgroundColor: 'rgba(54, 162, 235, 0.6)',
-                    borderColor: 'rgba(54, 162, 235, 1)',
+                  backgroundColor: 'rgba(105, 147, 255, 0.85)',
+                    borderColor: 'rgba(105, 147, 255, 1)',
                     borderWidth: 1
                 }]
             },
