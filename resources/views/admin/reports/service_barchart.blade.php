86ed492d89ad403371d18c84aea93b000de371a6
tomfoodery <foodplatform@outlook.com>
2025-05-21 12:47:32 +0500

    Reoslved

--- resources/views/admin/reports/service_barchart.blade.php
+++ resources/views/admin/reports/service_barchart.blade.php
@@ -31,13 +31,14 @@
                             </span>
                         </span>
                         <h2>Service Sold by Centre: {{ $service->name }}</h2>
-@if($start_date && $end_date)
-    <p>From: {{ \Carbon\Carbon::parse($start_date)->format('d M Y') }} To: {{ \Carbon\Carbon::parse($end_date)->format('d M Y') }}</p>
-@endif
+
                     </div>
                 </div>
                 <div class="card-body">
                     <div class="mt-2 mb-7">
+                                    @if($start_date && $end_date)
+                                    <p>From: {{ \Carbon\Carbon::parse($start_date)->format('d M Y') }} To: {{ \Carbon\Carbon::parse($end_date)->format('d M Y') }}</p>
+                                @endif
                         <canvas id="barChart" style="width: 100%;  height: 400px;"></canvas>
                         @if(count($labels) === 0)
                             <p>No sales data found for this service.</p>
