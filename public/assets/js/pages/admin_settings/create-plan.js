a37aff94c025cf1175aeb928b0a8b3ef806ca7f4
gulraizazam <gulraiz@mail.redsignal.net>
2025-05-29 09:28:30 +0500

    resolved

--- public/assets/js/pages/admin_settings/create-plan.js
+++ public/assets/js/pages/admin_settings/create-plan.js
@@ -1332,7 +1332,7 @@ function getServices() {
             $("#modal_edit_regions").modal("show");
 
             setServices(response);
-            setSoldBy(response);
+           
 
         },
         error: function (xhr, ajaxOptions, thrownError) {
@@ -1365,10 +1365,12 @@ function setServices(response) {
 function setSoldBy(response) {
     try {
         let users = response.data.users;
+        let selectedUserId = response.data.selected_doctor_id; // Get selected user ID from response
         let user_options = '<option value=""> Select </option>';
 
         Object.entries(users).forEach(function ([id, name]) {
-            user_options += '<option value="' + id + '"> ' + name + ' </option>';
+            let selected = (parseInt(id) === parseInt(selectedUserId)) ? 'selected' : '';
+            user_options += '<option value="' + id + '" ' + selected + '> ' + name + ' </option>';
         });
 
         $("#add_sold_by").html(user_options);
@@ -1398,7 +1400,7 @@ function getAppointments(patient) {
             cache: false,
             success: function (response) {
                 setAppointments(response);
-
+                    setSoldBy(response);
             },
             error: function (xhr, ajaxOptions, thrownError) {
                 errorMessage(xhr);
