edb5a5fe3bb964c15dc17ebf3ef6d6d7cda0f536
tomfoodery <foodplatform@outlook.com>
2025-06-17 10:09:49 +0500

    Resolved

--- resources/views/admin/reports/accountsalesreport/index.blade.php
+++ resources/views/admin/reports/accountsalesreport/index.blade.php
@@ -296,18 +296,7 @@
    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
-   <script>
-    $("#servicesSoldTable").DataTable({
-           dom: 'Bfrtip',
-           buttons: [
-               'excelHtml5',
-               'csvHtml5',
-               'pdfHtml5',
-           ],
-           "ordering": false,
-           "pageLength": 50
-       });
-</script>
+
         <script>
             $("#location_id_com").on('change',function(){
                 $("#location_id_com-report").val($("#location_id_com").val());
