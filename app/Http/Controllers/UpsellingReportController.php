966ebd151ff0ed8bdf704fe3ce59b5791b474fd1
Gulraiz Azam <gulraizazam0@gmail.com>
2025-07-04 22:20:15 +0500

    resolved

--- app/Http/Controllers/UpsellingReportController.php
+++ app/Http/Controllers/UpsellingReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Helpers\ACL;
 use App\Models\Locations;
 use App\Models\PackageService;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
