1c790e853c2bfc55261d75d26904b2fa8e8a6d46
gulraizazam <gulraiz@mail.redsignal.net>
2025-07-24 23:48:48 +0500

    Resolved

--- app/Http/Controllers/Admin/PackagesController.php
+++ app/Http/Controllers/Admin/PackagesController.php
@@ -1124,7 +1124,7 @@ class PackagesController extends Controller
             $vouchers = Voucher::whereIn('id', $voucherIds)->where([
                 ['active', '=', '1'],
             ])->whereDate('start', '<=', $today)->whereDate('end', '>=', $today)->get();
-            dd($voucherIds);
+            dd($vouchers);
         } else {
            
             if ($bundle && $bundle->apply_discount == '1') {
