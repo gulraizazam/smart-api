8f027f822d98976433f6f36a3a905de928b84bbf
gulraizazam <gulraiz@mail.redsignal.net>
2025-07-24 23:48:13 +0500

    Resolved

--- app/Helpers/Widgets/DiscountWidget.php
+++ app/Helpers/Widgets/DiscountWidget.php
@@ -440,7 +440,7 @@ class DiscountWidget
         ])
             ->whereIn('service_id', $serviceWithParents)
             ->get();
-            dd($centreservicediscounts);
+          
         if ($centreservicediscounts->count()) {
             foreach ($centreservicediscounts as $centreservicediscount) {
                 if (! in_array($centreservicediscount->discount_id, $discount_array)) {
@@ -448,7 +448,7 @@ class DiscountWidget
                 }
             }
         }
-
+        dd($discount_array);
         return $discount_array;
     }
     /*
