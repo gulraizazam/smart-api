962d3082118e24b325b2b094df64e3b02a3a571c
gulraizazam <gulraiz@mail.redsignal.net>
2025-07-26 11:43:31 +0500

    Resolved

--- app/Http/Controllers/Admin/PackagesController.php
+++ app/Http/Controllers/Admin/PackagesController.php
@@ -187,13 +187,9 @@ class PackagesController extends Controller
                 }else if ($discount_data->discount_type == "voucher") {
                     $patientVoucher = UserVouchers::where("user_id", $patient_id)->where("voucher_id", $discount_id)->first();
                     if ($patientVoucher) {
-                        $discount_type = "Fixed";
-                        $discount_price = (int) $patientVoucher->amount;
-                        
-                        $discount_price_cal = $service_data->price * (($discount_price) / 100);
-                        dd((($discount_price) / 100));
-                        $net_amount = ($service_data->price) - ($discount_price_cal);
-                      
+                        $discount_type = Config::get('constants.Fixed');
+                        $discount_price = $patientVoucher->amount;
+                        $net_amount = ($service_data->price) - ($discount_price);
                     }else{
                         $discount_type = "";
                         $discount_price = 0;
