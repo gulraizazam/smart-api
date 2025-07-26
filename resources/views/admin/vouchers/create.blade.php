1e490158f472f6853c0d31b2702cca71145d888d
gulraizazam <gulraiz@mail.redsignal.net>
2025-07-26 12:51:30 +0500

    Resolved

--- resources/views/admin/vouchers/create.blade.php
+++ resources/views/admin/vouchers/create.blade.php
@@ -29,7 +29,7 @@
 
                 <div class="form-group">
                     <div class="row">
-                        <div class="fv-row col-md-6 mt-5">
+                        <div class="fv-row col-md-12 mt-5">
                             <label class="required fw-bold fs-6 mb-2 pl-0">Voucher Name <span class="text text-danger">*</span></label>
                             <input id="add_name" class="form-control" type="text" name="name" placeholder="Name">
                         </div>
