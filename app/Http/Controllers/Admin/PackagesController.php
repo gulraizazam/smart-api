7f284280f52abb1445d5204c9e5c74f4bf53e970
gulraizazam <gulraiz@mail.redsignal.net>
2025-07-26 11:42:31 +0500

    Resolved

--- app/Http/Controllers/Admin/PackagesController.php
+++ app/Http/Controllers/Admin/PackagesController.php
@@ -191,7 +191,7 @@ class PackagesController extends Controller
                         $discount_price = (int) $patientVoucher->amount;
                         
                         $discount_price_cal = $service_data->price * (($discount_price) / 100);
-                        dd($service_data->price);
+                        dd((($discount_price) / 100));
                         $net_amount = ($service_data->price) - ($discount_price_cal);
                       
                     }else{
