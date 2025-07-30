ffe79555003700bbc720937e752218fa31f1e904
gulraizazam <gulraiz@mail.redsignal.net>
2025-07-30 22:57:51 +0500

    Resolved

--- app/Http/Controllers/Admin/PackagesController.php
+++ app/Http/Controllers/Admin/PackagesController.php
@@ -743,6 +743,7 @@ class PackagesController extends Controller
         $packageBundleData['id'] = $generateRandomId;
         if($discount){
             $userVoucher = UserVouchers::where('voucher_id', $discount->id)->where('user_id', $request->user_id)->first();
+            dd($userVoucher);
             if($userVoucher){
                 $amountLeft = $userVoucher->amount -  $request->discount_price;
                 if($amountLeft < 0){
