7d77fbf5e369892636c200e7b3810ada90bf8927
gulraizazam <gulraiz@mail.redsignal.net>
2025-07-31 23:38:49 +0500

    Resolved

--- resources/views/admin/packages/create.blade.php
+++ resources/views/admin/packages/create.blade.php
@@ -1,12 +1,15 @@
 <div class="modal-content">
     <div class="modal-header" id="kt_modal_password_header">
         <h2 class="fw-bolder">Create</h2>
-        <div class="btn btn-icon btn-sm btn-active-icon-primary popup-close" data-kt-users-modal-action="close">
-            <span class="svg-icon svg-icon-1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="black" />
-                    <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black" />
-                </svg>
+        <div class="btn btn-icon btn-sm btn-active-icon-primary popup-close" 
+     data-kt-users-modal-action="close" 
+     onclick="resetVoucherAdd(event); return false;">
+    <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
+    <span class="svg-icon svg-icon-1">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="black" />
+            <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black" />
+        </svg>
             </span>
         </div>
     </div>
