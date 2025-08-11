fe8ba219c5c2ba3af14a2362ec46fcfe5a9a7536
gulraizazam <gulraiz@mail.redsignal.net>
2025-08-11 22:41:36 +0500

    removed image validation

---
+++ resources/views/admin/reports/upsellingReport.blade.php
@@ -0,0 +1,31 @@
+<div id="doctor_sales_report">
+    <h4>Doctor Sales Report for Selected Location</h4>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Doctor</th>
+                <th>Total Sold Amount</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($reportData as $report)
+                <tr>
+                    <td>{{ $report->doctor_name ?? 'Unknown' }}</td>
+                    <td>{{ number_format($report->total_sold_amount, 2) }}</td>
+                    <td>
+                        <a href="{{ route('admin.doctor.upselling.detail', $report->doctor_id) }}"
+                           class="btn btn-primary btn-sm">
+                            View Details
+                        </a>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="3" class="text-center">No data available for this location.</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+</div>
