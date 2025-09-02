3eb7b0172d0eea09beccc4536fdadb8861d919d3
gulraizazam <gulraiz@mail.redsignal.net>
2025-09-02 23:00:09 +0500

    resolved

--- app/Http/Controllers/UpsellingReportController.php
+++ app/Http/Controllers/UpsellingReportController.php
@@ -8,6 +8,7 @@ use App\Models\PackageService;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Carbon;
 use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Role;
 
