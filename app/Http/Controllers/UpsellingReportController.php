78128c7dbce7fef9aef18ae239da20983119a3d2
gulraizazam <gulraiz@mail.redsignal.net>
2025-09-02 22:06:18 +0500

    resolved

--- app/Http/Controllers/UpsellingReportController.php
+++ app/Http/Controllers/UpsellingReportController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 use Spatie\Permission\Models\Role;
 
 class UpsellingReportController extends Controller
