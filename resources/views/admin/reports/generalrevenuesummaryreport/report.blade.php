a33b668d18a4b74a7ceb08dccb067eb7f227a48a
gulraizazam <gulraiz@mail.redsignal.net>
2025-09-10 20:57:02 +0500

    resolved

--- resources/views/admin/reports/generalrevenuesummaryreport/report.blade.php
+++ resources/views/admin/reports/generalrevenuesummaryreport/report.blade.php
@@ -85,12 +85,13 @@
                 <table class="table">
                     <thead>
                     <th>Centre</th>
-                    <th>City</th>
-                    <th>Region</th>
+                   
                     <th>Cash </th>
                     <th>Card </th>
                     <th>Bank/Wire Transfer</th>
                     <th>Refund/Out</th>
+                    <th>Collection By Gender (Male)</th>
+                    <th>Collection By Gender (Female)</th>
                     <th>In Hand</th>
                     </thead>
                     <tbody>
@@ -98,23 +99,25 @@
                         @foreach($report_data as $reportRow)
                             <tr>
                                 <td>{{$reportRow['name']}}</td>
-                                <td>{{$reportRow['city']}}</td>
-                                <td>{{$reportRow['region']}}</td>
+                               
                                 <td>{{number_format($reportRow['revenue_cash_in'],2)}}</td>
                                 <td>{{number_format($reportRow['revenue_card_in'],2)}}</td>
                                 <td>{{number_format($reportRow['revenue_bank_in'],2)}}</td>
                                 <td>{{number_format($reportRow['refund_out'],2)}}</td>
+                                <td>{{number_format($reportRow['male_revenue'],2)}}</td>
+                                <td>{{number_format($reportRow['female_revenue'],2)}}</td>
                                 <td>{{number_format($reportRow['in_hand'],2)}}</td>
                             </tr>
                         @endforeach
                         <tr style="background: #364150; color: #fff;">
                             <td style="font-weight: bold;color: #fff;">Total</td>
-                            <td></td>
-                            <td></td>
+                            
                             <td style="font-weight: bold;color: #fff;">{{number_format($total_revenue_cash_in,2)}}</td>
                             <td style="font-weight: bold;color: #fff;">{{number_format($total_revenue_card_in,2)}}</td>
                             <td style="font-weight: bold;color: #fff;">{{number_format($total_revenue_bank_in,2)}}</td>
                             <td style="font-weight: bold;color: #fff;">{{number_format($total_refund,2)}}</td>
+                            <td style="font-weight: bold;color: #fff;">{{number_format($total_revenue_male_in,2)}}</td>
+                            <td style="font-weight: bold;color: #fff;">{{number_format($total_revenue_female_in,2)}}</td>
                             <td style="font-weight: bold;color: #fff;">{{number_format(($total_revenue_cash_in+$total_revenue_card_in+$total_revenue_bank_in)-$total_refund,2)}}</td>
 
                         </tr>
@@ -145,6 +148,14 @@
                     </tr>
                     <tr>
                         <th>Refund</th>
+                        <td>{{number_format($total_refund,2)}}</td>
+                    </tr>
+                    <tr>
+                        <th>Collection By Gender (Male)</th>
+                        <td>{{number_format($total_refund,2)}}</td>
+                    </tr>
+                    <tr>
+                        <th>Collection By Gender (Female)</th>
                         <td>{{number_format($total_refund,2)}}</td>
                     </tr>
                     <tr>
