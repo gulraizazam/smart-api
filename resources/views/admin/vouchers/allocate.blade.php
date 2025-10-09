ea25cef96dbf2a94c0e6f19e57581e09eec6c8fd
gulraizazam <gulraiz@mail.redsignal.net>
2025-10-09 13:03:57 +0500

    Reoslbved

---
+++ resources/views/admin/vouchers/allocate.blade.php
@@ -0,0 +1,96 @@
+<!--begin::Modal content-->
+<div class="modal-content">
+    <!--begin::Modal header-->
+    <div class="modal-header" id="kt_modal_password_header">
+        <!--begin::Modal title-->
+        <h2 class="fw-bolder">Allocate</h2>
+        <!--end::Modal title-->
+        <!--begin::Close-->
+        <div class="btn btn-icon btn-sm btn-active-icon-primary popup-close" data-kt-users-modal-action="close">
+            <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
+            <span class="svg-icon svg-icon-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="black" />
+                    <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black" />
+                </svg>
+            </span>
+            <!--end::Svg Icon-->
+        </div>
+        <!--end::Close-->
+    </div>
+    <!--end::Modal header-->
+    <!--begin::Modal body-->
+    <div class="modal-body scroll-y mx-5 mx-xl-15 my-7">
+        <!--begin::Form-->
+        <form id="modal_allocate_vouchers_form" method="post" action="">
+            <!--begin::Scroll-->
+
+            <input type="hidden" name="id" id="voucher_id">
+
+            <div class="d-flex flex-column scroll-y me-n7 pe-7" id="kt_modal_vouchers_scroll" data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-max-height="auto" data-kt-scroll-dependencies="#kt_modal_add_user_header" data-kt-scroll-wrappers="#kt_modal_add_user_scroll" data-kt-scroll-offset="300px">
+
+                <div class="form-group">
+                    <div class="row">
+
+                        <div class="fv-row col-md-5 mt-5">
+                            <label class="required fw-bold fs-6 mb-2 pl-0">Centre <span class="text text-danger">*</span></label>
+                            <select id="locations" onchange="getDesrvice($(this));" class="form-control form-control-solid mb-3 mb-lg-0 select2" name="location_id">
+                                <option value="">Select Centre</option>
+                            </select>
+                        </div>
+
+                        <div class="fv-row col-md-5 mt-5">
+                            <label class="required fw-bold fs-6 mb-2 pl-0">Services <span class="text text-danger">*</span></label>
+                            <select id="services" class="form-control form-control-solid mb-3 mb-lg-0 select2" name="service_id">
+                                <option value="">Select Services</option>
+                            </select>
+                        </div>
+
+                        <div class="fv-row col-md-2 mt-5">
+                            <label class="required fw-bold fs-6 mb-2 pl-0" style="opacity: 0;">Add <span class="text text-danger">*</span></label>
+                        <button type="submit" class="btn btn-primary spinner-button">
+                            <span class="indicator-label">Add</span>
+                        </button>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <!--end::Scroll-->
+            <!--begin::Actions-->
+        {{--<hr>
+        {{-- class="text-center">
+            <button type="reset" class="btn btn-light me-3 popup-close" data-kt-users-modal-action="cancel">Cancel</button>
+            <button type="submit" class="btn btn-primary spinner-button">
+                <span class="indicator-label">Submit</span>
+            </button>
+        </div>--}}
+            <!--end::Actions-->
+        </form>
+
+
+        <div class="table-responsive">
+            <table id="allocate_services" class="table table-striped table-bordered table-advance table-hover">
+
+                <thead>
+                <tr>
+                    <th>Location</th>
+                    <th>Service</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+
+            </table>
+        </div>
+
+
+        <!--end::Form-->
+    </div>
+    <!--end::Modal body-->
+</div>
+<!--end::Modal content-->
+
+
+
