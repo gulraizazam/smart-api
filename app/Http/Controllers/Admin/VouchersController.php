bd7354057ad9ee6781910a1a6fa186764da31478
gulraizazam <gulraiz@mail.redsignal.net>
2025-10-11 13:17:54 +0500

    Resolved

--- app/Http/Controllers/Admin/VouchersController.php
+++ app/Http/Controllers/Admin/VouchersController.php
@@ -671,7 +671,7 @@ class VouchersController extends Controller
     }
     public function getListing()
     {
-        $vouchers = Discounts::where('discount_type', 'voucher')->pluck('id','name')->toArray();
+        $vouchers = Discounts::where('discount_type', 'voucher')->pluck('name','id')->toArray();
         return response()->json(['data'=> $vouchers]);
 
     }
