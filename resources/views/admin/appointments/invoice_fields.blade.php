41f912a1ce4809f4f32c446eff4d98d6c2581dd5
gulraizazam <gulraiz@mail.redsignal.net>
2025-10-14 21:52:29 +0500

    Resolved

--- resources/views/admin/appointments/invoice_fields.blade.php
+++ resources/views/admin/appointments/invoice_fields.blade.php
@@ -255,10 +255,10 @@
                            class="form-control float-right custom_field" id="created_at" readonly>
                 <!-- </div> -->
 
-                <!-- <div class="col-md-10 mt-5 mb-10" id="pay_section">
+                <div class="col-md-10 mt-5 mb-10" id="pay_section">
                     <strong class="mt-5">Pay</strong>
                     <input style="width: 50%;" type="text" name="cash_create" id="cash_create" value="0" class="form-control float-right" min="0" oninput="this.value = !!this.value && Math.abs(this.value) >= 0 ? Math.abs(this.value) : null;">
-                </div> -->
+                </div>
 
                 <div class="col-md-10 mt-5" id="paymentmode" style="display: none;">
                     <strong>Payment Mode</strong>
