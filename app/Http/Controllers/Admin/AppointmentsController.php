0f3d2867c44bd887a3feff5baeb87678ad86a118
gulraizazam <gulraiz@mail.redsignal.net>
2025-10-15 10:28:42 +0500

    deployed voucher module

--- app/Http/Controllers/Admin/AppointmentsController.php
+++ app/Http/Controllers/Admin/AppointmentsController.php
@@ -842,7 +842,8 @@ class AppointmentsController extends Controller
             'appointment_status',
             'appointment_status.parent',
             'hasInvoices', // 👈 eager load invoice
-        ])
+            'invoice'
+            ])
         ->where('appointments.appointment_type_id', $consultancyslug->id)
         ->whereIn('appointments.location_id', ACL::getUserCentres());
         }
