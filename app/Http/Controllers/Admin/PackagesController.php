c95c72fdf033b5dee0a2cc492705ee3db5d069de
gulraizazam <gulraiz@mail.redsignal.net>
2025-10-24 18:30:43 +0500

    Resolved plan issue

--- app/Http/Controllers/Admin/PackagesController.php
+++ app/Http/Controllers/Admin/PackagesController.php
@@ -1217,7 +1217,8 @@ class PackagesController extends Controller
             $location_id = $request->location_id;
 
             $discountIds = DiscountWidget::loadPlanDsicountByLocationService($location_id, $service_id, Auth::User()->account_id);
-             $generalDiscounts = Discounts::whereIn('id', $discountIds)
+            dd( $discountIds) ;
+            $generalDiscounts = Discounts::whereIn('id', $discountIds)
             ->where('discount_type', '!=', 'voucher')
             ->where('active', '=', '1')
             ->whereDate('start', '<=', $today)
