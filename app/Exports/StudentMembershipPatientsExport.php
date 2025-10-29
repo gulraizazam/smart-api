9867f7235e154ffd53eac4bc27d11ab046c81dee
gulraizazam <gulraiz@mail.redsignal.net>
2025-10-29 20:11:48 +0500

    resolved

--- app/Exports/StudentMembershipPatientsExport.php
+++ app/Exports/StudentMembershipPatientsExport.php
@@ -15,9 +15,10 @@ class StudentMembershipPatientsExport implements FromCollection, WithHeadings, W
             ->join('package_services', 'packages.id', '=', 'package_services.package_id')
             ->join('services', 'package_services.service_id', '=', 'services.id')
             ->leftJoin('memberships', 'packages.patient_id', '=', 'memberships.patient_id')
+            ->leftJoin('locations', 'packages.location_id', '=', 'locations.id')
             ->where('services.name', 'Student Membership Card') // Adjust field name if needed
             ->whereNull('memberships.patient_id') // Not in memberships table
-            ->select('packages.patient_id')
+            ->select('packages.patient_id', 'locations.name as location_name')
             ->distinct()
             ->get();
     }
@@ -25,14 +26,16 @@ class StudentMembershipPatientsExport implements FromCollection, WithHeadings, W
     public function headings(): array
     {
         return [
-            'Patient ID'
+            'Patient ID',
+            'Location Name'
         ];
     }
 
     public function map($row): array
     {
         return [
-            $row->patient_id
+            $row->patient_id,
+            $row->location_name
         ];
     }
 }
