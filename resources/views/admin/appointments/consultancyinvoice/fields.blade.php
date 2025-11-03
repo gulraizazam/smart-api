6b76906b3e77df71c726f4cc3d4a4e28019fd6d6
gulraizazam <gulraiz@mail.redsignal.net>
2025-11-03 12:40:54 +0500

    resolved chart isssue on home screen

--- resources/views/admin/appointments/consultancyinvoice/fields.blade.php
+++ resources/views/admin/appointments/consultancyinvoice/fields.blade.php
@@ -62,12 +62,12 @@
             {{--left section--}}
             <div class="col-md-10">
 
-                <div class="col-md-12 mt-5">
-                    <strong class="mt-5">Date</strong>
+                <!-- <div class="col-md-12 mt-5">
+                    <strong class="mt-5">Date</strong> -->
                     <!-- <span class="d-none"><i  onclick="triggerDate('custom_field');" style="color: #cc8600; font-size: large; cursor: pointer;" class="la la-pencil float-right"></i></span> -->
-                    <input type="text" name="created_at" value="{{\Carbon\Carbon::now()->format('Y-m-d')}}"
-                           class="form-control float-right custom_field pr-0 text-right" id="created_at" readonly>
-                </div>
+                    <!-- <input type="text" name="created_at" value="{{\Carbon\Carbon::now()->format('Y-m-d')}}"
+                           class="form-control float-right custom_field pr-0 text-right" id="created_at" readonly> -->
+                <!-- </div> -->
 
                 <div class="col-md-12 mt-5 mb-10">
                     <strong class="mt-5">Pay</strong>
