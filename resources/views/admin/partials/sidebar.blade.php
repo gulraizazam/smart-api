b1613b4661a1235774f1095bd13a5dc1ca5ba6eb
gulraizazam <gulraiz@mail.redsignal.net>
2025-12-11 20:11:03 +0500

    Resolved namespace issue

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -975,7 +975,7 @@
                     </div>
                     @endcan
 
-                    @can('tax_calculation_report_manage')
+                    @can('finance_general_revenue_reports_manage')
                     <div class="menu-submenu">
                         <i class="menu-arrow"></i>
                         <ul class="menu-subnav">
