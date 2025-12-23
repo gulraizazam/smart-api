2e44a54e10a95900dfef5076745e2716fbf63322
gulraizazam <gulraiz@mail.redsignal.net>
2025-12-23 13:52:25 +0500

    changes made in statuses

--- resources/views/admin/lead_statuses/create.blade.php
+++ resources/views/admin/lead_statuses/create.blade.php
@@ -47,14 +47,21 @@
                             </div>
                         </div>
                         <div class="fv-row col-md-6">
+                            <label class="required fw-bold fs-6 mb-2 pl-0">Default for Booked Leads</label>
+                            <div class="radio-inline">
+                                <label class="radio"><input name="is_booked" value="1" type="radio"/><span></span>Yes</label>
+                                <label class="radio"><input name="is_booked" value="0" checked type="radio"><span></span>No</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="fv-row col-md-6">
                             <label class="required fw-bold fs-6 mb-2 pl-0">Default for Arrived Leads</label>
                             <div class="radio-inline">
                                 <label class="radio"><input name="is_arrived" value="1" type="radio"/><span></span>Yes</label>
                                 <label class="radio"><input name="is_arrived" value="0" checked type="radio"><span></span>No</label>
                             </div>
                         </div>
-                    </div>
-                    <div class="row mt-5">
                         <div class="fv-row col-md-6">
                             <label class="required fw-bold fs-6 mb-2 pl-0">Default for Converted Leads</label>
                             <div class="radio-inline">
@@ -62,6 +69,8 @@
                                 <label class="radio"><input name="is_converted" value="0" checked type="radio"><span></span>No</label>
                             </div>
                         </div>
+                    </div>
+                    <div class="row mt-5">
                         <div class="fv-row col-md-6">
                             <label class="required fw-bold fs-6 mb-2 pl-0">Default for Junk Leads</label>
                             <div class="radio-inline">
--- resources/views/admin/lead_statuses/edit.blade.php
+++ resources/views/admin/lead_statuses/edit.blade.php
@@ -49,14 +49,21 @@
                             </div>
                         </div>
                         <div class="fv-row col-md-6">
+                            <label class="required fw-bold fs-6 mb-2 pl-0">Default for Booked Leads</label>
+                            <div class="radio-inline">
+                                <label class="radio"><input name="is_booked" value="1" type="radio"/><span></span>Yes</label>
+                                <label class="radio"><input name="is_booked" value="0" type="radio"><span></span>No</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="fv-row col-md-6">
                             <label class="required fw-bold fs-6 mb-2 pl-0">Default for Arrived Leads</label>
                             <div class="radio-inline">
                                 <label class="radio"><input name="is_arrived" value="1" type="radio"/><span></span>Yes</label>
                                 <label class="radio"><input name="is_arrived" value="0" type="radio"><span></span>No</label>
                             </div>
                         </div>
-                    </div>
-                    <div class="row mt-5">
                         <div class="fv-row col-md-6">
                             <label class="required fw-bold fs-6 mb-2 pl-0">Default for Converted Leads</label>
                             <div class="radio-inline">
@@ -64,6 +71,8 @@
                                 <label class="radio"><input name="is_converted" value="0" type="radio"><span></span>No</label>
                             </div>
                         </div>
+                    </div>
+                    <div class="row mt-5">
                         <div class="fv-row col-md-6">
                             <label class="required fw-bold fs-6 mb-2 pl-0">Default for Junk Leads</label>
                             <div class="radio-inline">
