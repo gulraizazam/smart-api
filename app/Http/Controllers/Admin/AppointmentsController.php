b48790352b69e8729cbf6433e1456987ce6b86c4
gulraizazam <gulraiz@mail.redsignal.net>
2025-12-24 16:21:41 +0500

    Resolved

--- app/Http/Controllers/Admin/AppointmentsController.php
+++ app/Http/Controllers/Admin/AppointmentsController.php
@@ -821,7 +821,22 @@ class AppointmentsController extends Controller
             Filters::put(Auth::User()->id, $filename, 'updated_by', $filters['updated_by']);
         }
         if (hasFilter($filters, 'appointment_status_id')) {
-            $where[] = [['appointments.base_appointment_status_id' => $filters['appointment_status_id']]];
+            // Check if the selected status is "arrived" - if so, include both arrived and converted
+            $selectedStatus = AppointmentStatuses::find($filters['appointment_status_id']);
+            if ($selectedStatus && $selectedStatus->is_arrived == 1) {
+                // Get converted status as well
+                $convertedStatus = AppointmentStatuses::where([
+                    'account_id' => Auth::User()->account_id,
+                    'is_converted' => 1
+                ])->first();
+                if ($convertedStatus) {
+                    $where[] = ['appointments.base_appointment_status_id', 'IN', [$filters['appointment_status_id'], $convertedStatus->id]];
+                } else {
+                    $where[] = [['appointments.base_appointment_status_id' => $filters['appointment_status_id']]];
+                }
+            } else {
+                $where[] = [['appointments.base_appointment_status_id' => $filters['appointment_status_id']]];
+            }
             Filters::put(Auth::User()->id, $filename, 'appointment_status_id', $filters['appointment_status_id']);
         }
         if (hasFilter($filters, 'appointment_type_id')) {
@@ -1185,7 +1200,22 @@ class AppointmentsController extends Controller
             Filters::put(Auth::User()->id, $filename, 'updated_by', $filters['updated_by']);
         }
         if (hasFilter($filters, 'appointment_status_id')) {
-            $where[] = [['appointments.base_appointment_status_id' => $filters['appointment_status_id']]];
+            // Check if the selected status is "arrived" - if so, include both arrived and converted
+            $selectedStatusTreatment = AppointmentStatuses::find($filters['appointment_status_id']);
+            if ($selectedStatusTreatment && $selectedStatusTreatment->is_arrived == 1) {
+                // Get converted status as well
+                $convertedStatusTreatment = AppointmentStatuses::where([
+                    'account_id' => Auth::User()->account_id,
+                    'is_converted' => 1
+                ])->first();
+                if ($convertedStatusTreatment) {
+                    $where[] = ['appointments.base_appointment_status_id', 'IN', [$filters['appointment_status_id'], $convertedStatusTreatment->id]];
+                } else {
+                    $where[] = [['appointments.base_appointment_status_id' => $filters['appointment_status_id']]];
+                }
+            } else {
+                $where[] = [['appointments.base_appointment_status_id' => $filters['appointment_status_id']]];
+            }
             Filters::put(Auth::User()->id, $filename, 'appointment_status_id', $filters['appointment_status_id']);
         }
         if (hasFilter($filters, 'appointment_type_id')) {
