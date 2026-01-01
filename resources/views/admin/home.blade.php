ca165acc16257d835ed6fc8b7250ccf2bf29fa8f
gulraizazam <gulraiz@mail.redsignal.net>
2026-01-01 21:02:56 +0500

    optimize dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -655,6 +655,7 @@
                                                 <option value="last7days" {{ request('type')=='last7days' ? 'selected' : '' }}>Last 7 Days</option>
                                                 <option value="week" {{ request('type')=='week' ? 'selected' : '' }}>This Week</option>
                                                 <option value="thismonth" {{ request('type')=='thismonth' ? 'selected' : '' }}>This Month</option>
+                                                <option value="lastmonth" {{ request('type')=='lastmonth' ? 'selected' : '' }}>Last Month</option>
                                                 <option value="all" {{ request('type')=='all' ? 'selected' : '' }}>Life Time</option>
                                             </select>
                                         </div>
