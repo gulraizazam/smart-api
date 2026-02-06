dfb8763ae2af407e16fd71ce9fe377c65f86844f
gulraizazam <gulraiz@mail.redsignal.net>
2026-02-06 11:40:25 +0500

    Resolved

--- resources/views/admin/packages/index.blade.php
+++ resources/views/admin/packages/index.blade.php
@@ -113,7 +113,7 @@
 
     @push('js')
         <script src="{{ asset('assets/js/pages/appointments/referred-by-patient-search.js') }}"></script>
-        <script src="{{ asset('assets/js/pages/admin_settings/create-plan.js') }}"></script>
+        <script src="{{ asset('assets/js/pages/admin_settings/create-plan.js') }}?v={{ time() }}"></script>
         <script src="{{ asset('assets/js/pages/admin_settings/create-bundle.js') }}"></script>
         <script src="{{ asset('assets/js/pages/admin_settings/edit-bundle.js') }}"></script>
         <script src="{{ asset('assets/js/pages/crud/forms/validation/admin_settings/refunds.js') }}"></script>
