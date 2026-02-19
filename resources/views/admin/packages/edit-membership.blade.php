e7ebe9dfefe96ef9ecf14465765c16f47cdd3cc9
gulraizazam <gulraiz@mail.redsignal.net>
2026-02-19 13:01:40 +0500

    resolved

--- resources/views/admin/packages/edit-membership.blade.php
+++ resources/views/admin/packages/edit-membership.blade.php
@@ -162,7 +162,7 @@
                         <div id="edit_document_upload_container">
                             <!-- Document Upload Item 1 (Default) -->
                             <div class="edit-document-upload-item mb-2" data-index="0">
-                                <div class="d-flex align-items-center gap-2">
+                                <div class="d-flex align-items-start gap-2">
                                     <div class="flex-grow-1">
                                         <input type="file" 
                                                name="edit_student_documents[]" 
