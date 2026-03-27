b42eeacd2e06ae61e602f387fa4f197c6bc24fe1
gulraizazam <gulraiz@mail.redsignal.net>
2026-03-27 18:03:37 +0500

    resolved

--- resources/views/admin/invoices/invoice_pdf.blade.php
+++ resources/views/admin/invoices/invoice_pdf.blade.php
@@ -192,7 +192,7 @@
     <table>
         <tr>
             <td>
-                <img class="img-responsive logo" src="{{ public_path('assets/media/new_logo.png') }}" style="width:235px;" alt=""/>
+                <img class="img-responsive logo" src="{{ !empty($download) ? public_path('assets/media/new_logo.png') : asset('assets/media/new_logo.png') }}" style="width:235px;" alt=""/>
                 <p class="logo_caption">{{$location_info->address}}.</p>
                 <p class="logo_caption logo_caption2">Phone. {{$location_info->fdo_phone}}  &nbsp; |  &nbsp; Email. {{$account->email}}  &nbsp; | &nbsp;  www.cuteraesthetics.com  &nbsp; | &nbsp; NTN. {{$location_info->ntn}} &nbsp; | &nbsp; STN. {{$location_info->stn}}</p>
             </td>
