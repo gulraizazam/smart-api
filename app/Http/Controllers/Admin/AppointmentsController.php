80766e2518097d5a6c71893d918de7a351179943
gulraiz azam <gulraizazam00@gmail.com>
2026-04-17 11:45:37 +0500

    rsolved

--- app/Http/Controllers/Admin/AppointmentsController.php
+++ app/Http/Controllers/Admin/AppointmentsController.php
@@ -823,9 +823,6 @@ class AppointmentsController extends Controller
 
     public function createTreatmentAppointment(Request $request): \Illuminate\Http\JsonResponse
     {
-        if (! Gate::allows('appointments_manage')) {
-            return $this->errorResponse('You are not authorized to access this resource.', 401);
-        }
         if (
             $request->location_id &&
             $request->doctor_id
