f3a61c9f9a45ec06d38116442a2c7cd3587d84c6
gulraiz azam <gulraizazam00@gmail.com>
2026-05-19 12:15:02 +0500

    fixed treatment invoice consume bug

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PermissionSeeder::class,
+            DashboardTabPermissionSeeder::class,
             AuditTrailActionSeeder::class,
             AuditTrailTableSeeder::class,
             AccountsSeeder::class,
